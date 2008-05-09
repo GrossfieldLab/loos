f28134242df57b236cab1d41176de923ab7dba4f
Tod Romo <drtromo@gmail.com>
2008-05-09 21:00:08 +0000

    Basic tokenizer for the atom selection parser
    svn path=/software/LOOS/trunk/; revision=399

---
+++ Tokenizer.cpp
@@ -0,0 +1,107 @@
+/*
+  KernelValue.hpp
+  (c) 2008 Tod D. Romo
+
+
+  Grossfield Lab
+  Department of Biochemistry and Biophysics
+  University of Rochester Medical School
+
+*/
+
+#include <ctype.h>
+
+
+
+#include "Tokenizer.hpp"
+
+
+namespace loos {
+
+  void Tokenizer::tokenize(void) {
+    int i = 0;
+    int state = 0;
+    char c;
+    string t;
+    Token token;
+
+    while (i < text.size()) {
+      c = text[i];
+
+      if (state == 0) {
+
+	if (isalpha(c)) {
+	  state = 1;
+	  t = c;
+	} else if (c == '-' || c == '+' || isdigit(c)) {
+	  state = 2;
+	  t = c;
+	} else if (c == '\'') {
+	  state = 3;
+	  t = string("");
+	} else if (c == '&' || c == '|' || c == '!' || c == '=' || c == '<' || c == '>') {
+	  state = 4;
+	  t = c;
+	}
+
+      } else if (state == 1) {
+
+	if (!isalpha(c)) {
+	  state = 0;
+	  token.setId(t);
+	  _tokens.push_back(token);
+	  --i;
+	} else 
+	  t += c;
+
+      } else if (state == 2) {
+
+	if (!isdigit(c)) {
+	  state = 0;
+	  token.setNumeric(t);
+	  _tokens.push_back(token);
+	  --i;
+	} else 
+	  t += c;
+
+      } else if (state == 3) {
+
+	if (c == '\\') {
+	  c = text[++i];
+	  t += c;
+	} else if (c == '\'') {
+	  state = 0;
+	  token.setString(t);
+	  _tokens.push_back(token);
+	} else
+	  t += c;
+
+      } else if (state == 4) {
+
+	if (!(c == '&' || c == '|' || c == '!' || c == '=' || c == '<' || c == '>')) {
+	  state = 0;
+	  token.setOperator(t);
+	  _tokens.push_back(token);
+	  --i;
+	} else
+	  t += c;
+
+      }
+
+
+      ++i;
+    }
+
+    switch(state) {
+    case 0: break;
+    case 1: token.setId(t); break;
+    case 2: token.setNumeric(t); break;
+    case 3: token.setString(t); break;
+    case 4: token.setOperator(t); break;
+    default:
+      throw(logic_error("Invalid state in tokenization"));
+    }
+
+  }
+
+};
---
+++ Tokenizer.hpp
@@ -0,0 +1,94 @@
+/*
+  KernelValue.hpp
+  (c) 2008 Tod D. Romo
+
+
+  Grossfield Lab
+  Department of Biochemistry and Biophysics
+  University of Rochester Medical School
+
+*/
+
+
+#if !defined(TOKENIZER_HPP)
+#define TOKENIZER_HPP
+
+
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <deque>
+
+#include <string.h>
+
+
+using namespace std;
+
+
+namespace loos {
+
+
+  struct Token {
+    enum TokenType { ID, NUMERIC, STRING, OPERATOR };
+
+    TokenType type;
+    string datum;
+
+
+    void setId(const string s) { datum = s; type = ID; }
+    void setNumeric(const string s) { datum = s; type = NUMERIC; }
+    void setString(const string s) { datum = s; type = STRING; }
+    void setOperator(const string s) { datum = s; type = OPERATOR; }
+
+    friend ostream& operator<<(ostream& os, const Token& t) {
+      os << "<TOKEN TYPE='";
+      switch(t.type) {
+      case ID: os << "ID"; break;
+      case NUMERIC: os << "NUMERIC" ; break;
+      case STRING: os << "STRING" ; break;
+      case OPERATOR: os << "OPERATOR" ; break;
+      default: throw(logic_error("Should never be here"));
+      }
+
+      os << "'>" << t.datum << "</TOKEN>";
+
+      return(os);
+    }
+
+
+  };
+
+
+  typedef deque<Token> Tokens;
+
+
+  class Tokenizer {
+    Tokens _tokens;
+    string text;
+
+  public:
+    Tokenizer(const string s) : text(s) { tokenize(); }
+    Tokenizer() { }
+
+    void tokenize(void);
+    void tokenize(const string s) { text = s; tokenize(); }
+
+    Tokens& tokens(void) { return(_tokens); }
+
+    friend ostream& operator<<(ostream& os, const Tokenizer& t) {
+      Tokens::const_iterator i;
+
+      for (i=t._tokens.begin(); i != t._tokens.end(); i++) 
+	os << *i << endl;
+
+      return(os);
+    }
+  };
+
+
+};
+
+
+
+#endif
