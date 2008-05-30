55accdffaa44db2c69f230834b82f05838ec72eb
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2008-05-30 21:56:33 +0000

    LOOSened order_params.
    Note: I haven't even tried to run it, let alone verified that it's not bug 
    ridden. 
    
    
    svn path=/software/LOOS/trunk/; revision=506

--- Tools/order_params.cpp
+++ Tools/order_params.cpp
@@ -7,13 +7,18 @@
 using namespace std;
 
 #include <iostream>
-#include "PDB.hpp"
 #include <math.h>
+
+#include <loos.hpp>
+#include <psf.hpp>
+#include <dcd.hpp>
+#include <Parser.hpp>
+#include <Selectors.hpp>
 
 void Usage()
     {
-    cerr << "Usage: order_params pdb_filelist skip "
-         << "selection_file1 [selection_file2...] "
+    cerr << "Usage: order_params psf dcd skip "
+         << "selection1 [selection2 ...]"
          << endl;
     }
 
@@ -21,117 +26,115 @@ int main (int argc, char *argv[])
 {
 if ( (argc <= 1) || 
      ( (argc >= 2) && (strncmp(argv[1], "-h", 2) == 0) ) ||
-     (argc < 4)
+     (argc < 5)
    )
     {
     Usage();
     exit(-1);
     }
 
-// Echo the command line to stdout
-cout << "# ";
-for (int i =0; i<argc; i++)
-    {
-    cout << argv[i] << " ";
-    }
-cout << endl;
+cout << "# " << invocationHeader(argc, argv) << endl;
 
 // copy the command line variables to real variable names
 
-char *pdb_filelist = argv[1];
-int skip = atoi(argv[2]);
+char *psf_filename = argv[1];
+char *dcd_filename = argv[2];
+int skip = atoi(argv[3]);
 
-vector<Selection> selections;
+// Create the data structures for the system and dcd
+PSF psf(psf_filename);
+DCD dcd(dcd_filename);
+
+// NOTE: We assume each selection is a list of carbon atoms, and we'll figure
+//       out the identities of the bound hydrogens ourselves
+vector<AtomicGroup> selections;
 for (int i =3; i<argc; i++)
     {
-    selections.push_back(Selection(argv[i]));
+    Parser p(argv[i]);
+    KernelSelector parsed(p.kernel());
+    selections.push_back(psf.select(parsed));
     }
 
-// Read the list of pdb files
-vector<string> file_list;
-ifstream pdb_files(pdb_filelist);
-if (pdb_files.fail())
+// Now, figure out which hydrogens go with each carbon selected
+// hydrogen_list mimics the structure of selections, so the hydrogens
+// bound to the jth carbon of the ith selection will be found at
+// hydrogen_list[i][j].
+vector< vector<AtomicGroup> > hydrogen_list(selections.size());
+HydrogenSelector hyd_sel;
+for (unsigned int i=0; i<selections.size(); i++) 
     {
-    cerr << "couldn't read " << pdb_filelist << ": exiting..." << endl;
-    exit(-1);
+    AtomicGroup *s = &(selections[i]);
+    AtomicGroup::Iterator iter(*s);
+    pAtom p;
+    while (p = iter()) 
+        {
+        vector<int> atom_ids = p->getBonds();
+        AtomicGroup bonded = psf.groupFromID(atom_ids);
+        AtomicGroup bonded_hydrogens = bonded.select(hyd_sel);
+        hydrogen_list[i].push_back(bonded_hydrogens);
+        }
     }
-string name;
-while ( getline(pdb_files, name) )
-    {
-    file_list.push_back(name);
-    }
 
-// remove the number of frames specified by skip
-file_list.erase(file_list.begin(), file_list.begin()+skip);
 
-int num_pdbfiles = file_list.size();
+// skip the equilibration frames
+dcd.readFrame(skip);
 
-// read the first file
-PDBFile file(file_list[0].c_str());
-
-// make the selections
-vector<Group *> carbons;
-for (vector<Selection>::iterator s =selections.begin();
-                                 s!=selections.end();
-                                 s++)
-    {
-    Group *g = new Group;
-    file.select(*s, *g);
-    carbons.push_back(g);
-    }
 
 // set up storage to accumulate the averages
+// we're going to dump all of the data from a given selection into one
+// big lump, so the dimension of sums and counts should match the number of 
+// selections specified 
 vector<float> sums;
+vector<float> sums2;
 vector<int> counts;
-sums.insert(sums.begin(), carbons.size(), 0.0);
-counts.insert(counts.begin(), carbons.size(), 0);
+sums.insert(sums.begin(), selections.size(), 0.0);
+sums2.insert(sums2.begin(), selections.size(), 0.0);
+counts.insert(counts.begin(), selections.size(), 0);
 
 // loop over pdb files
-for (int i = 0; i < num_pdbfiles; i++)
+while (dcd.readFrame())
     {
-    // get the new coordinates
-    file.update_coor(file_list[i].c_str());
+    dcd.updateGroupCoords(psf);
 
-    // loop over carbon groups
-    for (int j=0; j<carbons.size(); j++)
+    // loop over sets of selected carbons
+    for (unsigned int i=0; i<selections.size(); i++)
         {
-        Group *g = carbons[j];
-        for (vector<Atom*>::iterator c =g->atoms.begin();
-                                     c!=g->atoms.end();
-                                     c++)
+        AtomicGroup *g = &(selections[i]);
+        for (int j=0; j<g->size(); j++)
             {
-            // identify the two hydrogens attached
-            // a->index = the index of a in file + 1
-            // so the index of the 2 hydrogens will by a->index and a->index+1
-            int i = (*c)->index;
-            Coor *h1 = &(file.atoms[i].coor);
-            Coor *h2 = &(file.atoms[i+1].coor);
-
-            Coor v = (*c)->coor - h1;
-            float cos_val = v.z / v.length();
-            sums[j] += 0.5 - 1.5*cos_val*cos_val;
-
-            v = (*c)->coor - h2;
-            cos_val = v.z / v.length();
-            sums[j] += 0.5 - 1.5*cos_val*cos_val;
-
-            counts[j] += 2;
+            // get the carbon
+            pAtom carbon = g->getAtom(j);
+            // get the relevant hydrogens
+            AtomicGroup hyds = hydrogen_list[i][j];
+            
+            AtomicGroup::Iterator iter(hyds);
+            pAtom h;
+            while (h = iter() )
+                {
+                GCoord v = carbon->coords() - h->coords();
+                double cos_val =  v.z() / v.length();
+                double order = 0.5 - 1.5*cos_val*cos_val;
+                sums[i] += order;
+                sums2[i] += order*order;
+                counts[i]++;
+                }
             }
         }
     }
 
-for (int i = 0; i < carbons.size(); i++)
+for (unsigned int i = 0; i < selections.size(); i++)
     {
-    float val = sums[i] / counts[i];
+    double ave = sums[i] / counts[i];
+    double ave2 = sums2[i] / counts[i];
+    double dev = sqrt(ave2 - ave*ave);
+
     // get carbon number
-    string name = carbons[i]->atoms[0]->name;
+    pAtom pa = selections[i].getAtom(0);
+    string name = pa->name();
     name.erase(0,1); // delete the C
     int index = atoi(name.c_str());
 
-    //cout << i << "\t" 
-    //     << val << "\t"
-    //     << carbons[i]->atoms[0]->print() << endl;
-    cout << index << "\t" << val << endl;
+    cout << index << "\t" << ave << "\t" << dev << endl;
 
     }
 
