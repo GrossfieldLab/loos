389a48bbed10a97d53d2069ceaec793785d51e36
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2008-06-02 21:39:53 +0000

    Changed the way command line arguments work -- you just select all of the carbons, and it breaks them up by name
    svn path=/software/LOOS/trunk/; revision=544

--- Tools/order_params.cpp
+++ Tools/order_params.cpp
@@ -17,8 +17,8 @@ using namespace std;
 
 void Usage()
     {
-    cerr << "Usage: order_params psf dcd skip "
-         << "selection1 [selection2 ...]"
+    cerr << "Usage: order_params psf dcd skip selection "
+         << "first_carbon last_carbon"
          << endl;
     }
 
@@ -26,7 +26,7 @@ int main (int argc, char *argv[])
 {
 if ( (argc <= 1) || 
      ( (argc >= 2) && (strncmp(argv[1], "-h", 2) == 0) ) ||
-     (argc < 5)
+     (argc < 7)
    )
     {
     Usage();
@@ -40,19 +40,35 @@ cout << "# " << invocationHeader(argc, argv) << endl;
 char *psf_filename = argv[1];
 char *dcd_filename = argv[2];
 int skip = atoi(argv[3]);
+char *sel= argv[4];
+int first_carbon = atoi(argv[5]);
+int last_carbon = atoi(argv[6]);
 
 // Create the data structures for the system and dcd
 PSF psf(psf_filename);
 DCD dcd(dcd_filename);
 
-// NOTE: We assume each selection is a list of carbon atoms, and we'll figure
-//       out the identities of the bound hydrogens ourselves
+// NOTE: We assume the selection is a list of all of the relevant carbon atoms. 
+//       We'll break it into invidual carbons ourselves (assuming the normal
+//       convention of C2, C3, ... 
+//       Afterward, we'll figure out the relevant hydrogens ourselves
+Parser main_p(sel);
+KernelSelector main_parsed(main_p.kernel());
+AtomicGroup main_selection = psf.select(main_parsed);
+
+// Now break into individual carbons
 vector<AtomicGroup> selections;
-for (int i =4; i<argc; i++)
+for (int i =first_carbon; i<=last_carbon; i++)
     {
-    Parser p(argv[i]);
+    string sel_string = string(sel);
+    char carbon_name[4];
+    sprintf(carbon_name, "%d", i);
+    string name = string(" && name == \"C") + string(carbon_name)
+                  + string("\"");
+    sel_string.insert(sel_string.size(), name);
+    Parser p(sel_string.c_str());
     KernelSelector parsed(p.kernel());
-    selections.push_back(psf.select(parsed));
+    selections.push_back(main_selection.select(parsed));
     }
 
 // Now, figure out which hydrogens go with each carbon selected
