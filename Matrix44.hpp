3bc50c790ec2b2857d41bd4f1638d46543ad76e0
Tod Romo <drtromo@gmail.com>
2008-06-03 14:47:29 +0000

    Minor perf optimization in matrix-matrix multiply
    svn path=/software/LOOS/trunk/; revision=550

--- Matrix44.hpp
+++ Matrix44.hpp
@@ -139,39 +139,34 @@ public:
   friend Coord<T> operator*<>(const Matrix44<T>&, const Coord<T>&);
 
   //! Matrix-matrix multiply...
-  //! Uses temporary storage for the product, then copies back into
-  //! the current matrix.
   Matrix44<T>& operator*=(const Matrix44<T>& rhs) {
-    T tmp[16];
-
-    tmp[0] = matrix[0]*rhs.matrix[0] + matrix[1]*rhs.matrix[4] + matrix[2]*rhs.matrix[8] + matrix[3]*rhs.matrix[12];
-    tmp[1] = matrix[0]*rhs.matrix[1] + matrix[1]*rhs.matrix[5] + matrix[2]*rhs.matrix[9] + matrix[3]*rhs.matrix[13];
-    tmp[2] = matrix[0]*rhs.matrix[2] + matrix[1]*rhs.matrix[6] + matrix[2]*rhs.matrix[10] + matrix[3]*rhs.matrix[14];
-    tmp[3] = matrix[0]*rhs.matrix[3] + matrix[1]*rhs.matrix[7] + matrix[2]*rhs.matrix[11] + matrix[3]*rhs.matrix[15];
-
-    tmp[4] = matrix[4]*rhs.matrix[0] + matrix[5]*rhs.matrix[4] + matrix[6]*rhs.matrix[8] + matrix[7]*rhs.matrix[12];
-    tmp[5] = matrix[4]*rhs.matrix[1] + matrix[5]*rhs.matrix[5] + matrix[6]*rhs.matrix[9] + matrix[7]*rhs.matrix[13];
-    tmp[6] = matrix[4]*rhs.matrix[2] + matrix[5]*rhs.matrix[6] + matrix[6]*rhs.matrix[10] + matrix[7]*rhs.matrix[14];
-    tmp[7] = matrix[4]*rhs.matrix[3] + matrix[5]*rhs.matrix[7] + matrix[6]*rhs.matrix[11] + matrix[7]*rhs.matrix[15];
-
-    tmp[8] = matrix[8]*rhs.matrix[0] + matrix[9]*rhs.matrix[4] + matrix[10]*rhs.matrix[8] + matrix[11]*rhs.matrix[12];
-    tmp[9] = matrix[8]*rhs.matrix[1] + matrix[9]*rhs.matrix[5] + matrix[10]*rhs.matrix[9] + matrix[11]*rhs.matrix[13];
-    tmp[10] = matrix[8]*rhs.matrix[2] + matrix[9]*rhs.matrix[6] + matrix[10]*rhs.matrix[10] + matrix[11]*rhs.matrix[14];
-    tmp[11] = matrix[8]*rhs.matrix[3] + matrix[9]*rhs.matrix[7] + matrix[10]*rhs.matrix[11] + matrix[11]*rhs.matrix[15];
-
-    tmp[12] = matrix[12]*rhs.matrix[0] + matrix[13]*rhs.matrix[4] + matrix[14]*rhs.matrix[8] + matrix[15]*rhs.matrix[12];
-    tmp[13] = matrix[12]*rhs.matrix[1] + matrix[13]*rhs.matrix[5] + matrix[14]*rhs.matrix[9] + matrix[15]*rhs.matrix[13];
-    tmp[14] = matrix[12]*rhs.matrix[2] + matrix[13]*rhs.matrix[6] + matrix[14]*rhs.matrix[10] + matrix[15]*rhs.matrix[14];
-    tmp[15] = matrix[12]*rhs.matrix[3] + matrix[13]*rhs.matrix[7] + matrix[14]*rhs.matrix[11] + matrix[15]*rhs.matrix[15];
-
-    memcpy(matrix, tmp, 16*sizeof(T));
+    *this = *this * rhs;
     return(*this);
   }
 
   //! Matrix-matrix multiply
   Matrix44<T> operator*(const Matrix44<T>& rhs) const {
-    Matrix44<T> res(*this);
-    res *= rhs;
+    Matrix44<T> res;
+
+    res.matrix[0] = matrix[0]*rhs.matrix[0] + matrix[1]*rhs.matrix[4] + matrix[2]*rhs.matrix[8] + matrix[3]*rhs.matrix[12];
+    res.matrix[1] = matrix[0]*rhs.matrix[1] + matrix[1]*rhs.matrix[5] + matrix[2]*rhs.matrix[9] + matrix[3]*rhs.matrix[13];
+    res.matrix[2] = matrix[0]*rhs.matrix[2] + matrix[1]*rhs.matrix[6] + matrix[2]*rhs.matrix[10] + matrix[3]*rhs.matrix[14];
+    res.matrix[3] = matrix[0]*rhs.matrix[3] + matrix[1]*rhs.matrix[7] + matrix[2]*rhs.matrix[11] + matrix[3]*rhs.matrix[15];
+
+    res.matrix[4] = matrix[4]*rhs.matrix[0] + matrix[5]*rhs.matrix[4] + matrix[6]*rhs.matrix[8] + matrix[7]*rhs.matrix[12];
+    res.matrix[5] = matrix[4]*rhs.matrix[1] + matrix[5]*rhs.matrix[5] + matrix[6]*rhs.matrix[9] + matrix[7]*rhs.matrix[13];
+    res.matrix[6] = matrix[4]*rhs.matrix[2] + matrix[5]*rhs.matrix[6] + matrix[6]*rhs.matrix[10] + matrix[7]*rhs.matrix[14];
+    res.matrix[7] = matrix[4]*rhs.matrix[3] + matrix[5]*rhs.matrix[7] + matrix[6]*rhs.matrix[11] + matrix[7]*rhs.matrix[15];
+
+    res.matrix[8] = matrix[8]*rhs.matrix[0] + matrix[9]*rhs.matrix[4] + matrix[10]*rhs.matrix[8] + matrix[11]*rhs.matrix[12];
+    res.matrix[9] = matrix[8]*rhs.matrix[1] + matrix[9]*rhs.matrix[5] + matrix[10]*rhs.matrix[9] + matrix[11]*rhs.matrix[13];
+    res.matrix[10] = matrix[8]*rhs.matrix[2] + matrix[9]*rhs.matrix[6] + matrix[10]*rhs.matrix[10] + matrix[11]*rhs.matrix[14];
+    res.matrix[11] = matrix[8]*rhs.matrix[3] + matrix[9]*rhs.matrix[7] + matrix[10]*rhs.matrix[11] + matrix[11]*rhs.matrix[15];
+
+    res.matrix[12] = matrix[12]*rhs.matrix[0] + matrix[13]*rhs.matrix[4] + matrix[14]*rhs.matrix[8] + matrix[15]*rhs.matrix[12];
+    res.matrix[13] = matrix[12]*rhs.matrix[1] + matrix[13]*rhs.matrix[5] + matrix[14]*rhs.matrix[9] + matrix[15]*rhs.matrix[13];
+    res.matrix[14] = matrix[12]*rhs.matrix[2] + matrix[13]*rhs.matrix[6] + matrix[14]*rhs.matrix[10] + matrix[15]*rhs.matrix[14];
+    res.matrix[15] = matrix[12]*rhs.matrix[3] + matrix[13]*rhs.matrix[7] + matrix[14]*rhs.matrix[11] + matrix[15]*rhs.matrix[15];
 
     return(res);
   }
