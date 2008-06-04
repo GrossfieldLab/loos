a567703292d3ab14064daa9a3819292aa00017c1
Tod Romo <drtromo@gmail.com>
2008-06-04 01:14:40 +0000

    Fixed bug in SCons when regenerating test files Revamped alignment tests...
    svn path=/software/LOOS/trunk/; revision=560

--- Tests/alignment-tests.cpp
+++ Tests/alignment-tests.cpp
@@ -24,13 +24,15 @@
 #include <Selectors.hpp>
 #include <ensembles.hpp>
 
+// Configuration options...
+const unsigned int iter_tests = 1000;
+const double iter_perturbation = 2.0;
+const double iter_rmsd_thresh = 2.0;
+const double iter_final_rmsd_thresh = 1e-3;
 
-#if !defined(MAXGRPCNT)
-#define MAXGRPCNT 50
-#endif
-
-
-const unsigned int maxgrpcnt = MAXGRPCNT;
+const unsigned int single_tests = 1000;
+const double single_rmsd_thresh = 1e-10;
+const static bool show_results = false;
 
 
 
@@ -71,44 +73,52 @@ int main(int argc, char *argv[]) {
   cout << "Aligned rmsd = " << cas.rmsd(casb) << endl;
   cout << M << endl;
 
+
+  boost::uniform_real<> anglemap(-180.0, 180.0);
+  boost::variate_generator<loos::base_generator_type&, boost::uniform_real<> > angles(rng, anglemap);
+
+  boost::uniform_real<> transmap(-20.0, 20.0);
+  boost::variate_generator<loos::base_generator_type&, boost::uniform_real<> > translations(rng, transmap);
+
+
   // -------------------------------------------------------------------------------
 
-  cout << "*** Wide Single Alignment Sweep ***\n";
-  greal x, y, z;
+  cout << "*** Single Alignment Tests ***\n";
 
-  for (z = -90.0; z <= 90.0; z += 30.0) {
-    for (y = -90.0; y <= 90.0; y += 30.0) {
-      for (x = -90.0; x <= 90.0; x += 30.0) {
-	AtomicGroup casr = cas.copy();
-	W.identity();
-	W.rotate('x', x);
-	W.rotate('y', y);
-	W.rotate('z', z);
-	casr.applyTransform(W);
-	greal pre_rmsd = cas.rmsd(casr);
-	casr.alignOnto(cas);
-	greal rmsd = cas.rmsd(casr);
-	cout << setw(15) << z << setw(15) << y << setw(15) << x << setw(15) << pre_rmsd << setw(15) << rmsd << endl;
-      }
+  int warnings = 0;
+  for (i=0; i<single_tests; i++) {
+    AtomicGroup casr = cas.copy();
+    W.identity();
+    W.translate(translations(), translations(), translations());
+    W.rotate('x', angles());
+    W.rotate('y', angles());
+    W.rotate('z', angles());
+    casr.applyTransform(W);
+    greal pre_rmsd = cas.rmsd(casr);
+    casr.alignOnto(cas);
+    double rmsd = cas.rmsd(casr);
+    if (rmsd >= single_rmsd_thresh) {
+      cout << "WARNING - Possible mis-alignment - pre = " << pre_rmsd << ", post = " << rmsd << endl;
+      ++warnings;
     }
   }
+
+  if (warnings > 0)
+    cout << "*** There were " << warnings << " possible errors detected.\n";
+  else
+    cout << "All tests passed (threshold = " << single_rmsd_thresh << ")\n";
+
 
 
   // -------------------------------------------------------------------------------
   // Now run iteratative superpositon tests...
 
-  boost::uniform_real<> anglemap(-45.0, 45.0);
-  boost::variate_generator<loos::base_generator_type&, boost::uniform_real<> > angles(rng, anglemap);
-
-  boost::uniform_real<> transmap(-10.0, 10.0);
-  boost::variate_generator<loos::base_generator_type&, boost::uniform_real<> > translations(rng, transmap);
-
   vector<AtomicGroup> mols;
-  for (i=0; i<maxgrpcnt; i++) {
+  for (i=0; i<iter_tests; i++) {
     AtomicGroup subgroup = cas.copy();
     GCoord t(translations(), translations(), translations());
     
-    subgroup.perturbCoords(2.0);
+    subgroup.perturbCoords(iter_perturbation);
 
     W.identity();
     W.translate(t);
@@ -121,15 +131,34 @@ int main(int argc, char *argv[]) {
   }
 
   AtomicGroup avg = averageStructure(mols);
-  cout << "Pre-aligned rmsds:\n";
-  for (i=0; i<maxgrpcnt; i++)
-    cout << "\t" << i << "\t" << avg.rmsd(mols[i]) << endl;
+  if (show_results) {
+    cout << "Pre-aligned rmsds:\n";
+    for (i=0; i<iter_tests; i++)
+      cout << "\t" << i << "\t" << avg.rmsd(mols[i]) << endl;
+  }
 
 
   greal final_rmsd = loos::iterativeAlignment(mols, 2.0);
-  cout << "Final alignment rmsd to avg struct = " << final_rmsd << endl;
+  if (show_results)
+    cout << "Final alignment rmsd to avg struct = " << final_rmsd << endl;
 
+  if (final_rmsd >= iter_final_rmsd_thresh)
+    cout << "WARNING - final rmsd of " << final_rmsd << " is above threshold.\n";
+
+  warnings = 0;
   avg = averageStructure(mols);
-  for (i=0; i<maxgrpcnt; i++)
-    cout << "\t" << i << "\t" << avg.rmsd(mols[i]) << endl;
+  for (i=0; i<iter_tests; i++) {
+    if (show_results)
+      cout << "\t" << i << "\t" << avg.rmsd(mols[i]) << endl;
+    double irmsd = avg.rmsd(mols[i]);
+    if (irmsd >= iter_rmsd_thresh) {
+      ++warnings;
+      cout << "WARNING - possible iterative failure at " << i << " with rmsd of " << irmsd << endl;
+    }
+  }
+
+  if (warnings > 0)
+    cout << "*** There were " << warnings << " possible errors detected.\n";
+  else
+    cout << "All tests passed (threshold = " << iter_final_rmsd_thresh << " : " << iter_rmsd_thresh << ")\n";
 }
