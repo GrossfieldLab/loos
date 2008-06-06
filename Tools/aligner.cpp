3e8f69a8792c3dcf6b547ba1c0c946e093233c62
Tod Romo <drtromo@gmail.com>
2008-06-06 17:07:04 +0000

    Cleaned up majorly ugly option code
    svn path=/software/LOOS/trunk/; revision=588

--- Tools/aligner.cpp
+++ Tools/aligner.cpp
@@ -42,13 +42,29 @@
 
 // Default values...
 
-string alignment_string("name == 'CA'");
-string transform_string("all");
 
-greal alignment_tol = 0.5;
-int maxiter = 5000;
-int show_rmsd = 0;
-int no_rmsd = 0;
+struct Globals {
+  Globals() : alignment_string("name == 'CA'"),
+	      transform_string("name == 'CA'"),
+	      alignment_tol(0.5),
+	      maxiter(5000),
+	      show_rmsd(false),
+	      no_rmsd(false) { }
+
+
+  string alignment_string;
+  string transform_string;
+
+  greal alignment_tol;
+  int maxiter;
+  bool show_rmsd;
+  bool no_rmsd;
+};
+
+
+Globals globals;
+
+
 
 
 static struct option long_options[] = {
@@ -56,63 +72,50 @@ static struct option long_options[] = {
   {"transform", required_argument, 0, 't'},
   {"tolerance", required_argument, 0, 'T'},
   {"max", required_argument, 0, 'm'},
-  {"show", no_argument, &show_rmsd, 1},
-  {"normsd", no_argument, &no_rmsd, 1},
+  {"show", no_argument, 0, 's'},
+  {"normsd", no_argument, 0, 'n'},
   {"help", no_argument, 0, 'H'},
   {0,0,0,0}
 };
 
 
-static const char* short_options = "a:t:T:rn";
+static const char* short_options = "a:t:T:m:snH";
 
 
 void show_help(void) {
+  Globals defaults;
   cout << "Usage- aligner [opts] pdb dcd output-filename-prefix\n";
-  cout << "   --align=string     [" << alignment_string << "]\n";
-  cout << "   --transform=string [" << transform_string << "]\n";
-  cout << "   --tolerance=float  [" << alignment_tol << "]\n";
-  cout << "   --max=int          [" << maxiter << "]\n";
-  cout << "   --show=bool  (rmsd)[" << show_rmsd << "]\n";
-  cout << "   --normsd=boold     [" << no_rmsd << "]\n";
+  cout << "   --align=string     [" << defaults.alignment_string << "]\n";
+  cout << "   --transform=string [" << defaults.transform_string << "]\n";
+  cout << "   --tolerance=float  [" << defaults.alignment_tol << "]\n";
+  cout << "   --max=int          [" << defaults.maxiter << "]\n";
+  cout << "   --show=bool  (rmsd)[" << defaults.show_rmsd << "]\n";
+  cout << "   --normsd=boold     [" << defaults.no_rmsd << "]\n";
   cout << "   --help\n";
 }
 
 
 void parseOptions(int argc, char *argv[]) {
   int opt, idx;
-  string al, tr;
-  greal at = alignment_tol;
-  int mi = maxiter;
-  int sr = show_rmsd;
-  int nr = no_rmsd;
 
   while ((opt = getopt_long(argc, argv, short_options, long_options, &idx)) != -1) {
     switch(opt) {
-    case 'a': al = string(optarg); break;
-    case 't': tr = string(optarg); break;
-    case 'T': at = strtod(optarg, 0); break;
-    case 'm': mi = atoi(optarg); break;
-    case 'r': sr = 1; break;
-    case 'n': nr = 1; break;
+    case 'a': globals.alignment_string = string(optarg); break;
+    case 't': globals.transform_string = string(optarg); break;
+    case 'T': globals.alignment_tol = strtod(optarg, 0); break;
+    case 'm': globals.maxiter = atoi(optarg); break;
+    case 's': globals.show_rmsd = true; break;
+    case 'n': globals.no_rmsd = true; break;
     case 'H': show_help(); exit(-1);
     case 0: break;
-    default: cerr << "Unknown option '" << opt << "'\n";
+    default: cerr << "Unknown option '" << opt << "' - ignored.\n";
     }
   }
 
-
-  // Major uglage here!  Yuck!
-  if (al.size() != 0)
-    alignment_string = al;
-  if (tr.size() != 0)
-    transform_string = tr;
-
-  alignment_tol = at;
-  maxiter = mi;
-  if (!show_rmsd)
-    show_rmsd = sr;
-  if (!no_rmsd)
-    no_rmsd = nr;
+  if (globals.show_rmsd && globals.no_rmsd) {
+    cerr << "Error- you cannot disable RMSD and then show it's results...  That's just daft!\n";
+    exit(-1);
+  }
 
 }
 
@@ -121,19 +124,19 @@ double calcRMSD(const string& octave_tag, vector<AtomicGroup>& grps) {
 
   unsigned int nframes = grps.size();
   double avg_rmsd = 0.0;
-  if (show_rmsd)
+  if (globals.show_rmsd)
     cout << "<OCTAVE>\n";
   AtomicGroup avg = averageStructure(grps);
-  if (show_rmsd)
+  if (globals.show_rmsd)
     cout << octave_tag << " = [\n";
   for (unsigned int i = 0; i<nframes; i++) {
     greal irmsd = avg.rmsd(grps[i]);
-    if (show_rmsd)
+    if (globals.show_rmsd)
       cout << irmsd << " ;\n";
     avg_rmsd += irmsd;
   }
   
-  if (show_rmsd) {
+  if (globals.show_rmsd) {
     cout << "];\n";
     cout << "</OCTAVE>\n";
   }
@@ -190,10 +193,10 @@ int main(int argc, char *argv[]) {
   string prefix(argv[optind]);
 
   // Get the selections (subsets) to operate over
-  Parser alignment_parsed(alignment_string);
+  Parser alignment_parsed(globals.alignment_string);
   KernelSelector align_sel(alignment_parsed.kernel());
   
-  Parser applyto_parsed(transform_string);
+  Parser applyto_parsed(globals.transform_string);
   KernelSelector apply_sel(applyto_parsed.kernel());
 
   AtomicGroup align_sub = pdb.select(align_sel);
@@ -222,14 +225,14 @@ int main(int argc, char *argv[]) {
     frames.push_back(subcopy);
   }
 
-  boost::tuple<vector<XForm>,greal, int> res = iterativeAlignment(frames, alignment_tol, maxiter);
+  boost::tuple<vector<XForm>,greal, int> res = iterativeAlignment(frames, globals.alignment_tol, globals.maxiter);
   greal final_rmsd = boost::get<1>(res);
   cout << "Final RMSD between average structres is " << final_rmsd << endl;
   cout << "Total iters = " << boost::get<2>(res) << endl;
 
   vector<XForm> xforms = boost::get<0>(res);
 
-  if (!no_rmsd) {
+  if (!globals.no_rmsd) {
     double avg_rmsd = calcRMSD("r", frames);
     cout << "Average RMSD vs average for aligned subset = " << avg_rmsd << endl;
   }
@@ -269,12 +272,12 @@ int main(int argc, char *argv[]) {
     dcdout.writeFrame(applyto_sub);
 
     // Track average frame...
-    if (!no_rmsd)
+    if (!globals.no_rmsd)
       addCoords(avg, applyto_sub);
   }
 
 
-  if (!no_rmsd) {
+  if (!globals.no_rmsd) {
     // Second pass to calc rmsds...
 
     cout << "Calculating rmsds...\n";
@@ -282,7 +285,7 @@ int main(int argc, char *argv[]) {
     
   
     double avg_rmsd = 0.0;
-    if (show_rmsd)
+    if (globals.show_rmsd)
       cout << "<OCTAVE>\nrall = [\n";
     
     for (unsigned int i=0; i<nframes; i++) {
@@ -290,12 +293,12 @@ int main(int argc, char *argv[]) {
       dcd.updateGroupCoords(applyto_sub);
       applyto_sub.applyTransform(xforms[i]);
       double rms = applyto_sub.rmsd(avg);
-      if (show_rmsd)
+      if (globals.show_rmsd)
 	cout << rms << " ;\n";
       avg_rmsd += rms;
     }
     
-    if (show_rmsd)
+    if (globals.show_rmsd)
       cout << "];\n</OCTAVE>\n";
 
     avg_rmsd /= nframes;
