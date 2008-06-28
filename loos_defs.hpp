fdeae0403e417d9e6e12b2490558fde54bacde28
Tod Romo <drtromo@gmail.com>
2008-06-28 21:05:17 +0000

    Woops...forgotten file
    svn path=/software/LOOS/trunk/; revision=705

---
+++ loos_defs.hpp
@@ -0,0 +1,52 @@
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2008, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+
+
+#if !defined(LOOSDEFS_HPP)
+#define LOOSDEFS_HPP
+
+
+#include <Coord.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/tuple/tuple.hpp>
+
+typedef double greal;
+typedef long gint;
+
+typedef float dcd_real;
+typedef double dcd_double;
+
+typedef Coord<double> GCoord;
+typedef boost::shared_ptr<GCoord> pGCoord;
+
+
+const uint kilobytes = 1024;
+const uint megabytes = kilobytes * kilobytes;
+const uint gigabytes = megabytes * megabytes;
+
+
+#endif
+
+
