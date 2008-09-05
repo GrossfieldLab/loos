41a4e3611b732a5e4a7089876baf1161e1cc3110
Tod Romo <drtromo@gmail.com>
2008-09-05 20:48:45 +0000

    Moved rmsd2avg to rmsd2ref since it can now take an optional external reference model for computing rmsds against.  The following modes/options are now supported:
    o Aligning with no reference struct
      Computes the iterative alignment to find the average within the
      trajectory, then computes rmsds against that.
    
    o No aligning with no reference struct
      Simple RMSD between the avg and each frame
    
    o Aligning with external reference struct
      Computes alignment between reference and each frame of the
      trajectory, then rmsds.
    
    o No aligning with external reference struct
      Straight RMSD between external file and each frame of the
      trajectory. 
    
    
    svn path=/software/LOOS/trunk/; revision=970

--- Tools/rmsd2ref.cpp
+++ Tools/rmsd2ref.cpp
@@ -1,8 +1,8 @@
 /*
-  rmsd2avg.cpp
+  rmsd2ref.cpp
 
-  Computes rmsds between a selection and its average conformation, optionally
-  aligning the selection.
+  Computes rmsds between a selection and either its average
+  conformation or a reference model, optionally aligning the selection.
 */
 
 
@@ -39,23 +39,26 @@ typedef unsigned int uint;
 
 string align_string;
 string selection_string;
-string pdb_name, dcd_name;
+string model_name, traj_name;
+string target_name;
 
 const double tolerance = 1e-6;
 
 static struct option long_options[] = {
   {"align", required_argument, 0, 'a'},
+  {"target", required_argument, 0, 't'},
   {0,0,0,0}
 };
 
 
-static const char* short_options = "a:";
+static const char* short_options = "a:t:";
 
 
 void show_help(void) {
 
-  cout << "Usage- rmsd2avg [options] selection pdb dcd\n";
+  cout << "Usage- rmsd2ref [options] selection model trajectory\n";
   cout << "       --align=selection_string\n";
+  cout << "       --target=model\n";
 }
 
 
@@ -66,6 +69,7 @@ void parseOptions(int argc, char *argv[]) {
   while ((opt = getopt_long(argc, argv, short_options, long_options, &idx)) != -1)
     switch(opt) {
     case 'a': align_string = string(optarg); break;
+    case 't': target_name = string(optarg); break;
     case 0: break;
     default:
       cerr << "Unknown option '" << (char)opt << "' - ignored.\n";
@@ -77,8 +81,8 @@ void parseOptions(int argc, char *argv[]) {
   }
 
   selection_string = string(argv[optind++]);
-  pdb_name = string(argv[optind++]);
-  dcd_name = string(argv[optind++]);
+  model_name = string(argv[optind++]);
+  traj_name = string(argv[optind++]);
 }
 
 
@@ -87,8 +91,8 @@ int main(int argc, char *argv[]) {
   cout << "# " << hdr << endl;
 
   parseOptions(argc, argv);
-  AtomicGroup molecule = createSystem(pdb_name);
-  pTraj ptraj = createTrajectory(dcd_name, molecule);
+  AtomicGroup molecule = createSystem(model_name);
+  pTraj ptraj = createTrajectory(traj_name, molecule);
 
   cerr << boost::format("Using trajectory \"%s\" with %lu frames.\n") % dcd_name % ptraj->nframes();
 
@@ -96,30 +100,59 @@ int main(int argc, char *argv[]) {
   KernelSelector selector(parsed.kernel());
   AtomicGroup subset = molecule.select(selector);
 
-  cerr << boost::format("Computing RMSD vs avg conformation using %d atoms from \"%s\".\n") % subset.size() % selection_string;
+  if (target_name.empty())
+    cerr << boost::format("Computing RMSD vs avg conformation using %d atoms from \"%s\".\n") % subset.size() % selection_string;
+  else
+    cerr << boost::format("Computing RMSD vs target %s using %d atoms from \"%s\".\n") % target_name % subset.size() % selection_string;
 
   vector<AtomicGroup> frames;
+  AtomicGroup target;
 
+  // handle aligning, if requested...
   if (! align_string.empty()) {
+
+    // First, parse the alignment selection and extract the
+    // appropriate bits from the trajectory model...
     Parser parsed_align(align_string);
     KernelSelector align_selector(parsed_align.kernel());
     AtomicGroup align_subset = molecule.select(align_selector);
 
-    cerr << boost::format("Aligning using %d atoms from \"%s\".\n") % align_subset.size() % align_string;
+    // Iteratively align the trajectory...
+    if (target_name.empty()) {
+      cerr << boost::format("Aligning using %d atoms from \"%s\".\n") % align_subset.size() % align_string;
+      
+      boost::tuple<vector<XForm>, greal, int> res = loos::iterativeAlignment(align_subset, *ptraj, tolerance);
+      vector<XForm> xforms = boost::get<0>(res);
+      
+      uint n = ptraj->nframes();
+      for (uint i=0; i<n; i++) {
+	ptraj->readFrame(i);
+	ptraj->updateGroupCoords(subset);
+	subset.applyTransform(xforms[i]);
+	AtomicGroup frame = subset.copy();
+	frames.push_back(frame);
+      }
 
-    boost::tuple<vector<XForm>, greal, int> res = loos::iterativeAlignment(align_subset, *ptraj, tolerance);
-    vector<XForm> xforms = boost::get<0>(res);
+    } else {   // A target was provided and aligning was requested...
+      
+      target = createSystem(target_name);
+      target_subset = target.select(align_selector);
 
-    uint n = ptraj->nframes();
-    for (uint i=0; i<n; i++) {
-      ptraj->readFrame(i);
-      ptraj->updateGroupCoords(subset);
-      subset.applyTransform(xforms[i]);
-      AtomicGroup frame = subset.copy();
-      frames.push_back(frame);
+      uint n = ptraj->nframes();
+      for (uint i=0; i<n; i++) {
+	ptraj->readFrame(i);
+	ptraj->updateGroupCoords(molecule);
+	GMatrix M = align_subset.alignOnto(target_subset);
+	XForm W(M);
+	subset.applyTransform(W);
+	frames.push_back(frame);
+      }
+
+
     }
 
-  } else {
+  } else {  // No aligning was requested, so simply slurp up the trajectory...
+
     uint n = ptraj->nframes();
     for (uint i=0; i<n; i++) {
       ptraj->readFrame(i);
@@ -130,12 +163,16 @@ int main(int argc, char *argv[]) {
 
   }
 
-  AtomicGroup avg = loos::averageStructure(frames);
+  // If no external reference structure was specified, set the target
+  // to the average of the trajectory...
+  if (target_name.empty())
+    target = loos::averageStructure(frames);
+
   vector<double> rmsds;
   double avg_rmsd = 0.0;
 
   for (uint i=0; i<frames.size(); i++) {
-    double d = avg.rmsd(frames[i]);
+    double d = target.rmsd(frames[i]);
     rmsds.push_back(d);
     avg_rmsd += d;
   }
