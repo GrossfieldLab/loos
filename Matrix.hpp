583512b46a47577a79429bbb2219289301729138
Tod Romo <drtromo@gmail.com>
2008-10-06 21:02:36 +0000

    Updated namespace and comments
    svn path=/software/LOOS/branches/matrix-branch/; revision=1082

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,22 +1,38 @@
 /*
-  smatrix.cpp
-
-  
-  (c) 2008 Tod D. Romo, Grossfield Lab
-      Department of Biochemistry
-      University of Rochster School of Medicine and Dentistry
+  Matrix.hpp
 
   A simple matrix wrapper class.  This is not meant to be a mathematical matrix class!
+*/
 
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2008, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-#if !defined(SMATRIX_HPP)
-#define SMATRIX_HPP
+
+#if !defined(LOOS_MATRIX_HPP)
+#define LOOS_MATRIX_HPP
 
 #include <boost/shared_ptr.hpp>
 
-namespace banal {
+namespace loos {
 
   // These are the policy classes for the Matrix class.  They define
   // how the data is actually stored internally, i.e. lower
