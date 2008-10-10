1a9ee4ae7cf33a3ee92629024e4d7b063b4a99b7
Tod Romo <drtromo@gmail.com>
2008-10-10 14:56:55 +0000

    Rewritten to use new Matrix class and the CRTP...
    svn path=/software/LOOS/branches/matrix-branch/; revision=1093

--- MatrixWriter.hpp
+++ MatrixWriter.hpp
@@ -37,7 +37,9 @@
 #include <cassert>
 
 #include <boost/shared_array.hpp>
+#include <boost/format.hpp>
 
+#include <StreamWrapper.hpp>
 #include <Matrix.hpp>
 
 using namespace std;
@@ -46,236 +48,66 @@ typedef unsigned int uint;
 
 
 //! Class for handling writing of matrices in different formats.
-template<class T>
+template<class Derived>
 class MatrixWriter {
   struct null_deleter {
     void operator()(const void *) const { }
   };
 
 public:
-
-  //! Default constructor sends output to cout
-  MatrixWriter() : prefixname(""), ofs(&cout) { }
-
-  //! If passed a string, this forces output to open files named prefix + tag
-  explicit MatrixWriter(const string& prefix) : prefixname(prefix), ofs(0) { }
+  MatrixWriter(const string& s) : wfs(s) { }
+  MatrixWriter(const char* s) : wfs(s) { }
+  MatrixWriter(fstream& fs) : wfs(fs) { }
   
-  //! If passed a pointer to an ostream, then that is used instead...
-  explicit MatrixWriter(ostream* ofsp) : prefixname(""), ofs(ofsp) { }
   virtual ~MatrixWriter() { }
-  
 
-  string prefix(void) const { return(prefixname); }
-  void prefix(const string& s) { prefixname = s; }
-  ostream* outputStream(void) const { return(ofs); }
-  void outputStream(ostream* ofsp) { ofs = ofsp; }
-
-  string metadata(void) const { return(meta_data); }
-  //! Metadata is written out (optionally) depending on format.
-  void metadata(const string& s) { meta_data = s; }
-  
-
-  //! This handles writing the matrix output.
-  /** Matrix will always be written in row-major format, but the
-   *  actual storage order will depend on the Policy class used...
-   *
-   *  Notable parameters:
-   *  \arg \c tag String used to tag the matrix (i.e. name it)
-   *  \arg \c trans The matrix is transposed on output
-   *  \arg \c maxcol The maximum column to write
-   *  \arg \c maxrow The maxmimum row to write
-   */
-  template<class Policy>
-  void write(const loos::Matrix<T,Policy>& M, const string& tag, const bool trans = false, const uint maxcol = 0, const uint maxrow = 0) {
-    uint i, j, k;
-    uint m = M.rows();
-    uint n = M.cols();
-    uint nn = (maxcol == 0 || maxcol > n) ? n : maxcol;
-    uint mm = (maxrow == 0 || maxrow > n) ? m : maxrow;
-    ofstream *ofsp = 0;
-  
-    // Determine if we need to open a file or not...
-    ostream *po = ofs;
-    if (!po) {
-      string fname = constructFilename(tag);
-      ofsp = new ofstream(fname.c_str());
-      if (!ofsp)
-	throw(runtime_error("Unable to open file " + fname));
-      po = ofsp;
-    }
-
-    OutputPreamble(po, tag, m, n, trans);
-    for (j=0; j<mm; j++) {
-      for (i=0; i<nn; i++) {
-
-	T datum;
-	if (trans)
-	  datum = M(i, j);
-	else
-	  datum = M(j, i);
-	assert(k < m*n && "Matrix index exceeds dimensions");
-	OutputDatum(po, datum);
-      }
-      OutputEOL(po);
-    }
-    OutputCoda(po);
-  
-    delete ofsp;
+  template<class T, class P>
+  void write(const loos::Matrix<T, P>&M, const string& tag, const loos::Duple& start, const loos::Duple& end, const bool transpose = false) {
+    static_cast<Derived *>(this)->write_impl(M, tag, start, end, transpose);
+  }
+    
+  template<class T, class P>
+  void write(const loos::Matrix<T, P>& M, const string& tag) {
+    write(M, tag, Duple(0,0), Duple(M.rows(), M.cols()), false);
   }
 
-  //! Temporarily wraps data in a Matrix object, then writes it...
-  /** The Matrix class will wrap the data block in a shared_array if we
-   * just pass the T* to it, which then means it will be deallocated
-   * when control leaves the write() function.  So, we have to
-   * pre-wrap it in a shared_array that will not delete itself when it
-   * finally goes out of scope.  This is what the null_deleter does
-   * for us.  For more information, see the Boost documentation...
-   */
-  template<class Policy>
-  void write(const T* data, const string& tag, const uint m, const uint n, const bool trans = false, const uint maxcol = 0, const uint maxrow = 0) {
-    T* p = const_cast<T*>(data);
-    boost::shared_array<T> temp(p, null_deleter());
-    loos::Matrix<T, Policy> M(temp, m, n);
-    write(M, tag, m, n, trans, maxcol, maxrow);
+  template<class T, class P>
+  void write(const T* data, const string& tag, const loos::Duple& size, const loos::Duple& start, const loos::Duple& end, const bool transpose = false) {
+    T* p = const_cast<T *>(data);
+    boost::shared_array<T> sp(p, null_deleter());
+    const Matrix<T, P> M(sp, size, size.j, size.i);
+    write(M, tag, start, end, transpose);
   }
 
-
-  //! Temporarily wraps data in a Col-major matrix object, then writes it.
-  /**
-   * This is for compatability with existing code...  The original
-   * interface to write() assumed that the data was stored in
-   * column-major format.
-   */
-
-  void write(const T* data, const string& tag, const uint m, const uint n, const bool trans = false, const uint maxcol = 0, const uint maxrow = 0) {
-    // This is probably not good, but I can't think of a better way
-    // to put data into a const shared_array...
-    T* p = const_cast<T*>(data);
-    boost::shared_array<T> temp(p, null_deleter());
-    const loos::Matrix<T, loos::ColMajor> M(temp, m, n);
-    write(M, tag, trans, maxcol, maxrow);
+  template<class T>
+  void write(const T* data, const string& tag, const int m, const int n) {
+    write<T,RowMajor>(data, tag, Duple(m, n), Duple(0,0), Duple(m, n), false);
   }
-
-  template<class Policy>
-  void writeTriangular(const loos::Matrix<T, Policy>& M, const string tag) {
-    assert(M.rows() == M.cols() && "Error - cannot write a triangular matrix from a non-square one.");
-    ofstream *ofsp = 0;
-
-    // Determine if we need to open a file or not...
-    ostream *po = ofs;
-    if (!po) {
-      string fname = constructFilename(tag);
-      ofsp = new ofstream(fname.c_str());
-      if (!ofsp)
-	throw(runtime_error("Unable to open file " + fname));
-      po = ofsp;
-    }
-
-    OutputPreamble(po, tag, M.rows(), M.cols(), false);
-    for (uint j = 0; j<M.rows(); ++j) {
-      for (uint i = 0; i<j; ++i)
-	OutputDatum(po, M(j, i));
-      OutputEOL(po);
-    }
-    OutputCoda(po);
   
-    delete ofsp;
-  }
-
-  // These are overriden by subclasses to control the output format...
-
-  //! Writes out any pre-matrix text as required by the format.
-  virtual void OutputPreamble(ostream *po, const string& tag, const uint m, const uint n, const bool trans) =0;
-
-  //! Writes out a single element of the matrix
-  virtual void OutputDatum(ostream *po, const T d) =0;
-
-  //! Ends a row (line) of data
-  virtual void OutputEOL(ostream *po) =0;
-
-  //! Anything that needs to come after the matrix data has been written...
-  virtual void OutputCoda(ostream *po) =0;
-
-  //! Constructs format-dependent output filename...
-  virtual string constructFilename(const string& tag) =0;
 
 protected:
-  string prefixname;
-  string meta_data;
-  ostream *ofs;
+  StreamWrapper wfs;
 };
-
-
-//! Class for writing raw ascii matrices
-/** Matrix properties (such as size and transpose flags) are written
- *  in the preamble
- */
-template<class T>
-class RawAsciiWriter : public MatrixWriter<T> {
-public:
-  RawAsciiWriter() : MatrixWriter<T>() { }
-  explicit RawAsciiWriter(const string& s) : MatrixWriter<T>(s) { }
-  explicit RawAsciiWriter(ostream* o) : MatrixWriter<T>(o) { }
-
-  void OutputPreamble(ostream *po, const string& tag, const uint m, const uint n, const bool trans) {
-    if (RawAsciiWriter<T>::meta_data != "")
-      *po << "# " << RawAsciiWriter<T>::meta_data << endl;
-    *po << "# " << m << " " << n << " " << trans << " \"" << tag << "\"" << endl;
-  }
-
-  void OutputDatum(ostream *po, const T d) {
-    *po << d << " ";
-  }
-
-  void OutputEOL(ostream *po) {
-    *po << endl;
-  }
   
-  void OutputCoda(ostream *po) {
-    *po << endl;
- }
-
-  string constructFilename(const string& tag) { return(string(RawAsciiWriter<T>::prefixname + tag + ".asc")); }
-};
 
 
-//! Class for writing ASCII Octave format (as in a .m script)
-template<class T>
-class OctaveAsciiWriter : public MatrixWriter<T> {
+class RawAsciiWriter : public MatrixWriter<RawAsciiWriter> {
 public:
-  OctaveAsciiWriter() : MatrixWriter<T>() { }
-  explicit OctaveAsciiWriter(const string& s) : MatrixWriter<T>(s) { }
-  explicit OctaveAsciiWriter(ostream* o) : MatrixWriter<T>(o) { }
+  RawAsciiWriter(const string& s) : MatrixWriter<RawAsciiWriter>(s) { }
+  RawAsciiWriter(const char* s) : MatrixWriter<RawAsciiWriter>(s) { }
+  RawAsciiWriter(fstream& fs) : MatrixWriter<RawAsciiWriter>(fs) { }
 
-  void OutputPreamble(ostream *po, const string& tag, const uint m, const uint n, const bool trans) {
-    if (OctaveAsciiWriter<T>::meta_data != "") {
-      *po << "% " << OctaveAsciiWriter<T>::meta_data << endl;
-      *po << "% " << m << " x " << n << (trans ? " (transposed) " : "") << endl;
+  template<class T, class P>
+  void write_impl(const loos::Matrix<T, P>& M, const string& tag, const loos::Duple& start, const loos::Duple& end, const bool trans) {
+    *(wfs()) << boost::format("# %s\n") % tag;
+    *(wfs()) << boost::format("# %d x %d (%d)\n") % M.rows() % M.cols() % trans;
+
+    for (int j=0; j<M.rows(); j++) {
+      for (int i=0; i<M.cols(); i++)
+	*(wfs()) << M(j, i) << " ";
+      *(wfs()) << endl;
     }
-    *po << tag << " = [\n";
   }
-
-  void OutputDatum(ostream *po, const T d) {
-    *po << d << " ";
-  }
-
-  void OutputEOL(ostream *po) {
-    *po << " ;\n";
-  }
-
-  void OutputCoda(ostream *po) {
-    *po << "];\n";
-  } 
-
-  string constructFilename(const string& tag) { return(string(OctaveAsciiWriter<T>::prefixname + tag + ".m")); }
-
-private:
-
-  // Disable ability to write triangular matrices in octave format
-  // since it can't handle non-rectangular formats...
-  template<class Policy>
-  void writeTriangular(const Matrix<T, Policy>&, const string);
-
 };
 
 
