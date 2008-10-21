7b8a5d83dbd3e9d56a142876a7fa24669f535853
Tod Romo <drtromo@gmail.com>
2008-10-21 17:18:41 +0000

    Updated to use factories and other new-fangled LOOS niceties...
    svn path=/software/LOOS/trunk/; revision=1159

--- Tools/contacts.cpp
+++ Tools/contacts.cpp
@@ -34,95 +34,92 @@ using namespace std;
 
 void Usage()
     {
-    cerr << "Usage: contacts psf dcd selection1 selection_2 max" 
+    cerr << "Usage: contacts model trajectory selection1 selection_2 max" 
          << endl;
     }
 
 int main (int argc, char *argv[])
 {
-if ( (argc <= 1) || 
-     ( (argc >= 2) && (strncmp(argv[1], "-h", 2) == 0) ) ||
-     (argc < 6)
-   )
+  if ( (argc <= 1) || 
+       ( (argc >= 2) && (strncmp(argv[1], "-h", 2) == 0) ) ||
+       (argc < 6)
+       )
     {
-    Usage();
-    exit(-1);
+      Usage();
+      exit(-1);
     }
 
-cout << "# " << invocationHeader(argc, argv) << endl;
+  cout << "# " << invocationHeader(argc, argv) << endl;
 
-// copy the command line variables to real variable names
-char *psf_filename = argv[1];
-char *dcd_filename = argv[2];
-char *selection1 = argv[3];
-char *selection2 = argv[4];
-double max= atof(argv[5]);
-double max2 = max*max;
+  // copy the command line variables to real variable names
+  string model_filename(argv[1]);
+  string traj_filename(argv[2]);
+  string selection1(argv[3]);
+  string selection2(argv[4]);
+  double max = strtod(argv[5], 0);
+  double max2 = max*max;
 
-PSF psf(psf_filename);
-DCD dcd(dcd_filename);
+  AtomicGroup model = loos::createSystem(model_filename);
+  pTraj traj = loos::createTrajectory(traj_filename, model);
 
-// The assumption here is that selection1 will specify a bunch of molecules,
-// eg, all lipid headgroups of type foo.  g1 will be the group containing all
-// of those atoms.  However, what we'll actually want to do is work with the 
-// individual molecules' centers of mass, so we'll split g1 into a vector of
-// individual segids (corresponding to individual lipids) called group1
-Parser p1(selection1);
-KernelSelector parsed1(p1.kernel());
-AtomicGroup g1 = psf.select(parsed1);
-vector<AtomicGroup> group1 = g1.splitByUniqueSegid();
 
-// g2 / group2 works the same way
-Parser p2(selection2);
-KernelSelector parsed2(p2.kernel());
-AtomicGroup g2 = psf.select(parsed2);
-vector<AtomicGroup> group2 = g2.splitByUniqueSegid();
+  // The assumption here is that selection1 will specify a bunch of molecules,
+  // eg, all lipid headgroups of type foo.  g1 will be the group containing all
+  // of those atoms.  However, what we'll actually want to do is work with the 
+  // individual molecules' centers of mass, so we'll split g1 into a vector of
+  // individual segids (corresponding to individual lipids) called group1
+  AtomicGroup g1 = loos::selectAtoms(model, selection1);
+  vector<AtomicGroup> group1 = g1.splitByUniqueSegid();
+
+  // g2 / group2 works the same way
+  AtomicGroup g2 = loos::selectAtoms(model, selection2);
+  vector<AtomicGroup> group2 = g2.splitByUniqueSegid();
 
 
 
-cout << "#Frame\tPairs\tPerGroup1\tPerGroup2" << endl;
+  cout << "#Frame\tPairs\tPerGroup1\tPerGroup2" << endl;
 
-// loop over the frames of the dcd file
-int frame = 0;
-while (dcd.readFrame())
+  // loop over the frames of the dcd file
+  int frame = 0;
+  while (traj->readFrame())
     {
-    // get the new coordinates
-    dcd.updateGroupCoords(psf);
-    int count = 0;
+      // get the new coordinates
+      traj->updateGroupCoords(model);
+      int count = 0;
 
-    // compute the number of contacts between group1 center of mass 
-    // and group2 center of mass
-    vector<AtomicGroup>::iterator first;
-    for (first=group1.begin(); first!=group1.end(); first++)
+      // compute the number of contacts between group1 center of mass 
+      // and group2 center of mass
+      vector<AtomicGroup>::iterator first;
+      for (first=group1.begin(); first!=group1.end(); first++)
         {
-        GCoord com1 = first->centerOfMass();
+	  GCoord com1 = first->centerOfMass();
 
-        vector<AtomicGroup>::iterator second;
-        for (second=group2.begin(); second!=group2.end(); second++)
+	  vector<AtomicGroup>::iterator second;
+	  for (second=group2.begin(); second!=group2.end(); second++)
             {
-            // exclude self pairs 
-            if (*first == *second)
+	      // exclude self pairs 
+	      if (*first == *second)
                 continue;
 
-            GCoord com2 = second->centerOfMass();
+	      GCoord com2 = second->centerOfMass();
 
-            double d2 = com1.distance2(com2, psf.periodicBox());
-            if ( (d2 <= max2) )
+	      double d2 = com1.distance2(com2, model.periodicBox());
+	      if ( (d2 <= max2) )
                 {
-                count++;
+		  count++;
                 }
             }
         }
     
-    // Output the results
-    double per_g1_atom = (double)count / group1.size();
-    double per_g2_atom = (double)count / group2.size();
-    cout << frame << "\t" 
-         << count << "\t"
-         << per_g1_atom << "\t"
-         << per_g2_atom << endl;
+      // Output the results
+      double per_g1_atom = (double)count / group1.size();
+      double per_g2_atom = (double)count / group2.size();
+      cout << frame << "\t" 
+	   << count << "\t"
+	   << per_g1_atom << "\t"
+	   << per_g2_atom << endl;
 
-    frame++;
+      frame++;
     }
 }
 
