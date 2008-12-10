eaa847b74399b4811c46ae1201a23fab9a28f37c
Tod Romo <drtromo@gmail.com>
2008-12-10 15:20:46 +0000

    Matrix writers can now use a formatting functor...
    svn path=/software/LOOS/trunk/; revision=1467

--- MatrixWrite.hpp
+++ MatrixWrite.hpp
@@ -36,19 +36,48 @@ namespace loos {
   typedef std::pair<int, int> MDuple;
 
   // Forward declaration for matrix writing implementation
-  template<class T, class P, template<typename> class S>
+  template<class T, class P, template<typename> class S, class F>
   struct MatrixWriteImpl;
+
+  namespace internal {
+    template<typename T>
+    struct BasicMatrixFormatter {
+      std::string operator()(const T& t) {
+        std::stringstream ss;
+        ss << t;
+        return(ss.str());
+      }
+    };
+  }
 
   // The following are the templated global functions.  Do not
   // overload/specialize them.  Instead, specialize the
   // MatrixWriteImpl class...
 
   //! Write a submatrix to a stream
+  template<class T, class P, template<typename> class S, class F>
+  std::ostream& writeAsciiMatrix(std::ostream& os, const Math::Matrix<T,P,S>& M,
+                                 const std::string& meta, const MDuple& start,
+                                 const MDuple& end, const bool trans = false, F fmt = F()) {
+    return(MatrixWriteImpl<T,P,S,F>::write(os, M, meta, start, end, trans, fmt));
+  }
+
+
+  //! Write a submatrix to a stream
   template<class T, class P, template<typename> class S>
   std::ostream& writeAsciiMatrix(std::ostream& os, const Math::Matrix<T,P,S>& M,
                                  const std::string& meta, const MDuple& start,
                                  const MDuple& end, const bool trans = false) {
-    return(MatrixWriteImpl<T,P,S>::write(os, M, meta, start, end, trans));
+    return(MatrixWriteImpl<T,P,S,internal::BasicMatrixFormatter<T> >::write(os, M, meta, start, end, trans));
+  }
+
+  //! Write an entire matrix to a stream
+  template<class T, class P, template<typename> class S, class F>
+  std::ostream& writeAsciiMatrix(std::ostream& os, const Math::Matrix<T,P,S>& M,
+                                 const std::string& meta, const bool trans = false, F fmt = F()) {
+    MDuple start(0,0);
+    MDuple end(M.rows(), M.cols());
+    return(MatrixWriteImpl<T,P,S,F>::write(os, M, meta, start, end, trans, fmt));
   }
 
   //! Write an entire matrix to a stream
@@ -57,52 +86,77 @@ namespace loos {
                                  const std::string& meta, const bool trans = false) {
     MDuple start(0,0);
     MDuple end(M.rows(), M.cols());
-    return(MatrixWriteImpl<T,P,S>::write(os, M, meta, start, end, trans));
+    return(MatrixWriteImpl<T,P,S,internal::BasicMatrixFormatter<T> >::write(os, M, meta, start, end, trans));
+  }
+
+  //! Write a submatrix to a file
+  template<class T, class P, template<typename> class S, class F>
+  void writeAsciiMatrix(const std::string& fname, const Math::Matrix<T,P,S>& M,
+                                 const std::string& meta, const MDuple& start,
+                        const MDuple& end, const bool trans = false, F fmt = F()) {
+    std::ofstream ofs(fname.c_str());
+    if (ofs == 0)
+      throw(std::runtime_error("Cannot open " + fname + " for writing."));
+    MatrixWriteImpl<T,P,S,F>::write(ofs, M, meta, start, end, trans, fmt);
   }
 
   //! Write a submatrix to a file
   template<class T, class P, template<typename> class S>
   void writeAsciiMatrix(const std::string& fname, const Math::Matrix<T,P,S>& M,
                                  const std::string& meta, const MDuple& start,
-                                 const MDuple& end, const bool trans = false) {
+                        const MDuple& end, const bool trans = false) {
     std::ofstream ofs(fname.c_str());
     if (ofs == 0)
       throw(std::runtime_error("Cannot open " + fname + " for writing."));
-    MatrixWriteImpl<T,P,S>::write(ofs, M, meta, start, end, trans);
+    MatrixWriteImpl<T,P,S,internal::BasicMatrixFormatter<T> >::write(ofs, M, meta, start, end, trans);
   }
 
 
   //! Write an entire matrix to a file
-  template<class T, class P, template<typename> class S>
+  template<class T, class P, template<typename> class S, class F>
   void writeAsciiMatrix(const std::string& fname, const Math::Matrix<T,P,S>& M,
-                                 const std::string& meta, const bool trans = false) {
+                        const std::string& meta, const bool trans = false, F fmt = F()) {
     MDuple start(0,0);
     MDuple end(M.rows(), M.cols());
 
     std::ofstream ofs(fname.c_str());
     if (ofs == 0)
       throw(std::runtime_error("Cannot open " + fname + " for writing."));
-    MatrixWriteImpl<T,P,S>::write(ofs, M, meta, start, end, trans);
+    MatrixWriteImpl<T,P,S,F>::write(ofs, M, meta, start, end, trans, fmt);
+  }
+
+
+  //! Write an entire matrix to a file
+  template<class T, class P, template<typename> class S>
+  void writeAsciiMatrix(const std::string& fname, const Math::Matrix<T,P,S>& M,
+                        const std::string& meta, const bool trans = false) {
+    MDuple start(0,0);
+    MDuple end(M.rows(), M.cols());
+
+    std::ofstream ofs(fname.c_str());
+    if (ofs == 0)
+      throw(std::runtime_error("Cannot open " + fname + " for writing."));
+    MatrixWriteImpl<T,P,S,internal::BasicMatrixFormatter<T> >::write(ofs, M, meta, start, end, trans);
   }
 
 
   // Writing implementation and specializations...
 
-  template<class T, class P, template<typename> class S>
+  template<class T, class P, template<typename> class S, class F>
   struct MatrixWriteImpl {
     static std::ostream& write(std::ostream& os,
                                const Math::Matrix<T,P,S>& M,
                                const std::string& meta,
                                const MDuple& start, const MDuple& end,
-                               const bool trans) {
+                               const bool trans, F fmt = F()) {
       os << "# " << meta << std::endl;
       os << boost::format("# %d %d (%d)\n") % M.rows() % M.cols() % trans;
       for (int j=start.first; j<end.first; j++) {
         for (int i=start.second; i<end.second; i++)
           if (trans)
-            os << M(i, j) << " ";
+            os << fmt(M(i, j)) << " ";
           else
-            os << M(j, i) << " ";
+            os << fmt(M(j, i)) << " ";
         os << std::endl;
       }
       return(os);
@@ -111,19 +165,19 @@ namespace loos {
 
   //! Write out a sparse matrix.
   /** Ignores \a start, \a end, and \a trans */
-  template<class T, class P>
-  struct MatrixWriteImpl<T, P, Math::SparseArray > {
+  template<class T, class P, class F>
+  struct MatrixWriteImpl<T, P, Math::SparseArray, F> {
     static std::ostream& write(std::ostream& os,
                                const Math::Matrix<T,P,Math::SparseArray>& M,
                                const std::string& meta,
                                const MDuple& start, const MDuple& end,
-                               const bool trans) {
+                               const bool trans, F fmt = F()) {
       os << "# " << meta << std::endl;
       os << boost::format("# %d %d %d SPARSE\n") % M.actualSize() % M.rows() % M.cols();
       typename Math::Matrix<T,P,Math::SparseArray>::const_iterator ci;
       
       for (ci = M.begin(); ci != M.end(); ++ci)
-        os << (*ci).first << "\t" << (*ci).second << std::endl;
+        os << (*ci).first << "\t" << fmt((*ci).second) << std::endl;
       
       return(os);
     }
@@ -131,18 +185,18 @@ namespace loos {
 
   //! Write out a triangular matrix
   /** Ignores \a start, \a end, and \a trans */
-  template<class T, template<typename> class S>
-  struct MatrixWriteImpl<T, Math::Triangular, S> {
+  template<class T, template<typename> class S, class F>
+  struct MatrixWriteImpl<T, Math::Triangular, S, F> {
     static std::ostream& write(std::ostream& os,
                                const Math::Matrix<T,Math::Triangular,S>& M,
                                const std::string& meta,
                                const MDuple& start, const MDuple& end,
-                               const bool trans) {
+                               const bool trans, F fmt = F()) {
       os << "# " << meta << std::endl;
       os << boost::format("# %d TRIANGULAR\n") % M.rows();
       long s = M.size();
       for (long i=0; i<s; i++)
-        os << M[i] << std::endl;
+        os << fmt(M[i]) << std::endl;
       
       return(os);
     }
