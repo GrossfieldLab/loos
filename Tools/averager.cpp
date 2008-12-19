97a85a427248856e833cf675c56c0ef3fca09f89
Tod Romo <drtromo@gmail.com>
2008-12-19 12:39:15 +0000

    Tweaked help formatting
    svn path=/software/LOOS/trunk/; revision=1559

--- Tools/averager.cpp
+++ Tools/averager.cpp
@@ -55,7 +55,7 @@ Globals globals;
 void parseOptions(int argc, char *argv[]) {
 
   try {
-    po::options_description generic("Allowed options");
+    po::options_description generic("Allowed options", 120);
     generic.add_options()
       ("help", "Produce this help message")
       ("align,a", po::value<string>(&globals.align_string)->default_value("name == 'CA'"),"Align using this selection")
