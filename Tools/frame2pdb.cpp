c832e04541fc066327ccb25fd8607d3d0db20f44
Tod Romo <drtromo@gmail.com>
2009-01-23 22:00:03 +0000

    Added ability to pass a selection
    svn path=/software/LOOS/trunk/; revision=1659

--- Tools/frame2pdb.cpp
+++ Tools/frame2pdb.cpp
@@ -32,36 +32,80 @@
 
 
 #include <loos.hpp>
-
+#include <boost/program_options.hpp>
 
 using namespace std;
 using namespace loos;
 
+namespace po = boost::program_options;
 
-int main(int argc, char *argv[]) {
-  if (argc != 4) {
-    cerr << argv[0] << " pdbfile dcdfile frameno\n";
+string model_name, traj_name, selection;
+uint frameno;
+
+
+void parseOptions(int argc, char *argv[]) {
+
+  try {
+    po::options_description generic("Allowed options");
+    generic.add_options()
+      ("help", "Produce this help message")
+      ("selection,s", po::value<string>(&selection)->default_value("all"), "Only write out this subset");
+
+    po::options_description hidden("Hidden options");
+    hidden.add_options()
+      ("model", po::value<string>(&model_name), "Model filename")
+      ("traj", po::value<string>(&traj_name), "Trajectory filename")
+      ("frame", po::value<uint>(&frameno), "Frame Number");
+
+    po::options_description command_line;
+    command_line.add(generic).add(hidden);
+
+    po::positional_options_description p;
+    p.add("model", 1);
+    p.add("traj", 1);
+    p.add("frame", 1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+              options(command_line).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help") || !(vm.count("model") && vm.count("traj") && vm.count("frame"))) {
+      cerr << "Usage- " << argv[0] << " [options] model trajectory frame# >output.pdb\n";
+      cerr << generic;
+      exit(-1);
+    }
+
+  }
+
+  catch(exception& e) {
+    cerr << "Error - " << e.what() << endl;
     exit(-1);
   }
 
+}
+
+
+
+int main(int argc, char *argv[]) {
   string hdr = invocationHeader(argc, argv);
+  parseOptions(argc, argv);
 
-  AtomicGroup model = createSystem(argv[1]);
-  pTraj traj = createTrajectory(argv[2], model);
-  int frame = atoi(argv[3]);
+  AtomicGroup model = createSystem(model_name);
+  pTraj traj = createTrajectory(traj_name, model);
 
 
-  bool b = traj->readFrame(frame);
+  bool b = traj->readFrame(frameno);
   if (!b) {
-    cerr << "Could not read frame " << frame << " from trajectory " << argv[2] << endl;
+    cerr << "Could not read frame " << frameno << " from trajectory " << traj_name << endl;
     exit(-2);
   }
 
-  traj->updateGroupCoords(model);
-  PDB pdb = PDB::fromAtomicGroup(model);
+  AtomicGroup subset = selectAtoms(model, selection);
+  traj->updateGroupCoords(subset);
+  PDB pdb = PDB::fromAtomicGroup(subset);
   pdb.remarks().add(hdr);
   cout << pdb << endl;
-
 }
 
 
