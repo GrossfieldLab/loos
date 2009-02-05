f46cf5afcb5d747c5f6a43ae3ee9453fa5c12630
Tod Romo <drtromo@gmail.com>
2009-02-05 16:22:13 +0000

    Fixed bug in default selection
    svn path=/software/LOOS/trunk/; revision=1689

--- Tools/rmsd2ref.cpp
+++ Tools/rmsd2ref.cpp
@@ -59,7 +59,7 @@ void parseOptions(int argc, char *argv[]) {
     po::options_description generic("Allowed options", 100);
     generic.add_options()
       ("help", "Produce this help message")
-      ("align,a", po::value<string>(&globals.alignment)->default_value("name == 'CA')"), "Align using this selection")
+      ("align,a", po::value<string>(&globals.alignment)->default_value("name == 'CA'"), "Align using this selection")
       ("iterative,i", po::value<bool>(&globals.iterate)->default_value(false),"Use iterative alignment method")
       ("target,t", po::value<string>(&globals.target_name), "Compute RMSD against this reference target")
       ("tolerance,T", po::value<double>(&globals.tol)->default_value(1e-6), "Tolerance to use for iterative alignment")
