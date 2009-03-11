9696aee4a51c07b3c838f8e11d3b3048d1b49383
Tod Romo <drtromo@gmail.com>
2009-03-11 16:21:44 +0000

    Improved...  Added more targets...
    svn path=/software/LOOS/trunk/; revision=1864

--- Tools/drifter.cpp
+++ Tools/drifter.cpp
@@ -30,42 +30,158 @@
 */
 
 
-#include <loos.hpp>
 #include <string>
+#include <sstream>
 #include <vector>
+#include <boost/program_options.hpp>
+
+#include <loos.hpp>
+
 
 using namespace std;
 using namespace loos;
+namespace po = boost::program_options;
 
 
-GCoord averageCentroid(AtomicGroup& model, pTraj traj) {
-  GCoord avg(0,0,0);
-  uint n = traj->nframes();
-  for (uint i=0; i<n; ++i) {
-    traj->readFrame(i);
-    traj->updateGroupCoords(model);
-    GCoord c = model.centroid();
-    avg += c;
+struct Base {
+  virtual ~Base() { }
+
+  virtual double operator()(const GCoord& c, const uint t) =0;
+};
+
+
+
+struct averageCentroid : public Base {
+  averageCentroid(AtomicGroup& model, pTraj traj) : avg(0,0,0) {
+    uint n = traj->nframes();
+    for (uint i=0; i<n; ++i) {
+      traj->readFrame(i);
+      traj->updateGroupCoords(model);
+      GCoord c = model.centroid();
+      avg += c;
+    }
+    
+    avg /= n;
   }
 
-  avg /= n;
-  return(avg);
+  double operator()(const GCoord& c, const uint) {
+    return(c.distance(avg));
+  }
+
+  GCoord avg;
+};
+
+
+// Note: This relies on a little trickery...  First, it assumes that
+// the appropriate frame will always have been read prior to calling.
+// Second, it bypass the prohibition on copying Trajectory objects by
+// binding a reference to the pTraj shared pointer...  It's probably
+// better not to do these sorts of things unless you understand LOOS
+// reasonably well...
+
+struct selectionCentroid : public Base {
+  selectionCentroid(AtomicGroup& subset, pTraj& traj) : ref_subset(subset.copy()), traj_(traj) { }
+
+  double operator()(const GCoord& c, const uint) {
+    traj_->updateGroupCoords(ref_subset);
+    GCoord ctr = ref_subset.centroid();
+    return(c.distance(ctr));
+  }
+
+  AtomicGroup ref_subset;
+  pTraj& traj_;
+};
+
+
+struct fixedPoint : public Base {
+  fixedPoint(GCoord& c) : fixed(c) { }
+
+  double operator()(const GCoord& c, const uint) {
+    return(c.distance(fixed));
+  }
+
+  GCoord fixed;
+};
+
+
+
+string model_name, traj_name, selection;
+Base* compute = 0;
+AtomicGroup model;
+pTraj traj;
+AtomicGroup subset;
+
+
+
+
+void parseOptions(int argc, char *argv[]) {
+
+  try {
+
+    po::options_description generic("Allowed options");
+    generic.add_options()
+      ("help", "Produce this help message")
+      ("selection,s", po::value<string>(&selection)->default_value("name == 'CA'"), "Subset selection")
+      ("average,a", "Calculate distance from selection to the average centroid (default)")
+      ("centroid,c", po::value<string>(), "Calculate distance to the centroid of this selection")
+      ("fixed,f", po::value<string>(), "Calculate distance to a fixed point (x,y,z)");
+
+    po::options_description hidden("Hidden options");
+    hidden.add_options()
+      ("model", po::value<string>(&model_name), "Model filename")
+      ("traj", po::value<string>(&traj_name), "Trajectory filenames");
+
+
+    po::options_description command_line;
+    command_line.add(generic).add(hidden);
+
+    po::positional_options_description p;
+    p.add("model", 1);
+    p.add("traj", 1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+              options(command_line).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help") || !(vm.count("model") && vm.count("traj"))) {
+      cerr << "Usage- " << argv[0] << " [options] model-name trajectory-name\n";
+      cerr << generic;
+      exit(-1);
+    }
+
+    model = createSystem(model_name);
+    traj = createTrajectory(traj_name, model);
+    subset = selectAtoms(model, selection);
+
+    if (vm.count("centroid")) {
+      string sel = vm["centroid"].as<string>();
+      AtomicGroup refsub = selectAtoms(model, sel);
+      compute = new selectionCentroid(refsub, traj);
+    } else if (vm.count("fixed")) {
+      string s = vm["fixed"].as<string>();
+      stringstream ss(s);
+      GCoord c;
+      if (!(ss >> c)) {
+        cerr << "Error - cannot parse '" << s << "' as a coordinate.  It must be (x,y,z).\n";
+        exit(-1);
+      }
+      compute = new fixedPoint(c);
+    } else
+      compute = new averageCentroid(subset, traj);
+
+  }
+  catch(exception& e) {
+    cerr << "Error - " << e.what() << endl;
+    exit(-1);
+  }
 }
 
 
 int main(int argc, char *argv[]) {
+
   string hdr = invocationHeader(argc, argv);
-  if (argc != 4) {
-    cerr << "Usage: drifter selection model trajectory\n";
-    exit(-1);
-  }
-
-  AtomicGroup model = createSystem(argv[2]);
-  pTraj traj = createTrajectory(argv[3], model);
-
-  AtomicGroup subset = selectAtoms(model, argv[1]);
-
-  GCoord avg_center = averageCentroid(subset, traj);
+  parseOptions(argc, argv);
 
   cout << "# " << hdr << endl;
   cout << "# t d\n";
@@ -74,6 +190,6 @@ int main(int argc, char *argv[]) {
     traj->readFrame(i);
     traj->updateGroupCoords(subset);
     GCoord c = subset.centroid();
-    cout << i << " " << avg_center.distance(c) << endl;
+    cout << i << " " << (*compute)(c, i) << endl;
   }
 }
