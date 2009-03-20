2557d84ca878613caefba254d896c4899c49984a
Tod Romo <drtromo@gmail.com>
2009-03-20 15:45:24 +0000

    Added usage notes and full LOOS copyright
    svn path=/software/LOOS/trunk/; revision=1897

--- Tools/porcupine.cpp
+++ Tools/porcupine.cpp
@@ -4,6 +4,50 @@
   (c) 2009 Tod D. Romo, Grossfield Lab
   Department of Biochemistry
   University of Rochester School of Medicine and Dentistry
+
+
+
+  Creates "porcupine" plots by placing atoms at the endpoints of the
+  vectors and adding a bond between them.  This is all written out a
+  PDB file with CONECT records.
+
+  Notes:
+
+    The eigenvectors are taken to be column-vectors from the
+  input matrix.  Since SVD's are often computed over subsets of a
+  structure, the eigenvectors can be mapped back onto the appropriate
+  atoms from a larger model by using a map file.  This is just a list
+  of the eigenvector id's (i.e. column #'s) and the corresponding
+  atomid's.  For example:
+     0     30
+     1     42
+     2     57
+     3     66
+
+*/
+
+
+
+/*
+
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2009, Tod D. Romo
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
