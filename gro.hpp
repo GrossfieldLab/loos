9416faace2764b979fb6e63a4f1260f7c6f6a3c1
Tod Romo <drtromo@gmail.com>
2009-08-27 15:39:18 +0000

    Added copyright to gro.?pp (woops) Added minimal doxygen comment to Gromacs class so it will appear in the docs...
    svn path=/software/LOOS/trunk/; revision=2551

--- gro.hpp
+++ gro.hpp
@@ -1,3 +1,25 @@
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2009, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #if !defined(GRO_HPP)
 #define GRO_HPP
 
@@ -13,6 +35,7 @@
 
 namespace loos {
 
+  //! Implements a GROMACS model file (.gro)
   class Gromacs : public AtomicGroup {
   public:
 
