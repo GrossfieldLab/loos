242ace718b24ec5dc8299fc5d7159e35b2a7c6c2
Tod Romo <drtromo@gmail.com>
2010-07-26 19:54:55 +0000

    Copying large matrix SVD into LOOS
    svn path=/software/LOOS/trunk/; revision=4544

---
+++ Tools/big-svd.cpp
@@ -0,0 +1,140 @@
+#include <loos.hpp>
+#include <boost/format.hpp>
+
+using namespace std;
+using namespace loos;
+
+
+
+
+RealMatrix extractCoordinates(pTraj& traj, AtomicGroup& grp) {
+  uint m = grp.size() * 3;
+  uint n = traj->nframes();
+
+  RealMatrix A(m, n);
+  vector<double> avg(m, 0.0);
+
+  for (uint i=0; i<n; ++i) {
+    traj->readFrame(i);
+    traj->updateGroupCoords(grp);
+    for (uint j=0; j<grp.size(); ++j) {
+      GCoord c = grp[j]->coords();
+      A(3*j, i) = c.x();
+      avg[3*j] += c.x();
+
+      A(3*j+1, i) = c.y();
+      avg[3*j+1] += c.y();
+
+      A(3*j+2, i) = c.z();
+      avg[3*j+2] += c.z();
+    }
+  }
+
+  for (uint j=0; j<m; ++j)
+    avg[j] /= n;
+
+  for (uint i=0; i<n; ++i)
+    for (uint j=0; j<m; ++j)
+      A(j, i) -= avg[j];
+
+  return(A);
+}
+
+
+// Don't want to bother with an in-place transpose...just eat up memory instead!
+
+RealMatrix transpose(const RealMatrix& A) {
+  uint m = A.rows();
+  uint n = A.cols();
+
+  RealMatrix At(A.cols(), A.rows());
+  for (uint j=0; j<m; ++j)
+    for (uint i=0; i<n; ++i)
+      At(i,j) = A(j, i);
+
+  return(At);
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    cerr << "Usage- bigsvd selection model traj prefix\n";
+    exit(0);
+  }
+
+
+  string hdr = invocationHeader(argc, argv);
+  int k = 1;
+  string selection(argv[k++]);
+  string modelname(argv[k++]);
+  string trajname(argv[k++]);
+  string prefix(argv[k++]);
+
+
+  AtomicGroup model = createSystem(modelname);
+  AtomicGroup subset = selectAtoms(model, selection);
+  pTraj traj = createTrajectory(trajname, model);
+
+  // Build AA'
+
+  RealMatrix A = extractCoordinates(traj, subset);
+  writeAsciiMatrix(prefix + "_A.asc", A, hdr);
+
+
+  cerr << "Multiplying transpose...\n";
+  RealMatrix C = MMMultiply(A, A, false, true);
+  cerr << "Done!\n";
+  writeAsciiMatrix(prefix + "_AA.asc", C, hdr);
+
+  // Compute [U,D] = eig(C)
+
+  char jobz = 'V';
+  char uplo = 'L';
+  f77int n = A.rows();
+  f77int lda = n;
+  float dummy;
+  RealMatrix W(n, 1);
+  f77int lwork = -1;
+  f77int info;
+
+  cerr << "Calling ssyev to get work size...\n";
+
+  ssyev_(&jobz, &uplo, &n, C.get(), &lda, W.get(), &dummy, &lwork, &info);
+  if (info != 0) {
+      cerr << boost::format("ssyev failed with info = %d\n") % info;
+      exit(-10);
+  }
+   
+  lwork = static_cast<f77int>(dummy);
+  cerr << boost::format("Allocating space %f MB\n") % (dummy / (1024.0 * 1024.0));
+  float *work = new float[lwork+1];
+
+  cerr << "Calling ssyev for eigendecomp...\n";
+  ssyev_(&jobz, &uplo, &n, C.get(), &lda, W.get(), work, &lwork, &info);
+  if (info != 0) {
+      cerr << boost::format("ssyev failed with info = %d\n") % info;
+      exit(-10);
+  }
+  cerr << "Finished!\n";
+  
+  reverseColumns(C);
+  writeAsciiMatrix(prefix + "_U.asc", C, hdr);
+
+  // D = sqrt(D);  Scale eigenvectors...
+  for (uint j=0; j<W.rows(); ++j)
+    W[j] = W[j] < 0 ? 0.0 : sqrt(W[j]);
+
+  reverseRows(W);
+  writeAsciiMatrix(prefix + "_s.asc", W, hdr);
+
+  cerr << "Multiplying to get RSVs...\n";
+  RealMatrix Vt = MMMultiply(C, A, true, false);
+  RealMatrix V = transpose(Vt);
+  cerr << "Done!\n";
+  Vt.reset();
+  C.reset();
+  writeAsciiMatrix(prefix + "_V.asc", V, hdr, false);
+
+}
