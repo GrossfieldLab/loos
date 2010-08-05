f71ad5478cd94d84823f6c04df1c8fd187f6b845
Tod Romo <drtromo@gmail.com>
2010-08-05 20:33:22 +0000

    ANM class
    svn path=/software/LOOS/branches/ENM-Devel/; revision=4607

---
+++ Tools/ElasticNetworks/anm-lib.hpp
@@ -0,0 +1,46 @@
+//
+// (c) 2010 Tod D. Romo, Grossfield Lab, URMC
+//
+
+
+#include "enm-lib.hpp"
+
+
+
+
+class ANM : public ElasticNetworkModel {
+public:
+
+  void solve() {
+    buildHessian();
+
+    boost::tuple<loos::DoubleMatrix, loos::DoubleMatrix, loos::DoubleMatrix> result = svd(hessian_);
+    eigenvecs_ = boost::get<0>(result);
+    eigenvals_ = boost::get<1>(result);
+    rsv_ = boost::get<2>(result);
+
+    uint n = eigenvals_.rows();
+    loos::reverseRows(eigenvals_);
+    loos::reverseColumns(eigenvecs_);
+    loos::reverseRows(rsv_);
+  }
+
+
+  loos::DoubleMatrix inverseHessian() {
+
+    uint n = eigenvals_.rows();
+    for (uint i=6; i<n; ++i) {
+      double s = 1.0 / eigenvals_[i];
+      for (uint j=0; j<n; ++j)
+        rsv_(i, j) *= s;
+    }
+
+    loos::DoubleMatrix Hi = MMMultiply(rsv_, eigenvecs_, true, true);
+    return(Hi);
+  }
+
+
+private:
+  loos::DoubleMatrix rsv_;
+
+};
