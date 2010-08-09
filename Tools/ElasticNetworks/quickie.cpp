1ceae85de39e484fc6dfc55ef93c15a1892bb799
Tod Romo <drtromo@gmail.com>
2010-08-09 20:37:03 +0000

    Now works with arbitrary number of models for fitting...
    svn path=/software/LOOS/branches/ENM-Devel/; revision=4655

--- Tools/ElasticNetworks/quickie.cpp
+++ Tools/ElasticNetworks/quickie.cpp
@@ -9,6 +9,7 @@
 #include <Simplex.hpp>
 #include "fitter.hpp"
 #include "vsa-lib.hpp"
+#include "fit_aggregator.hpp"
 
 
 using namespace loos;
@@ -17,29 +18,45 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    cout << "Usage- quickie model subsystem environment eigvals eigvecs\n";
+    cout << "Usage- quickie tag model subsystem environment eigvals eigvecs [tag model sub env eigvals eigvecs ...]\n";
     exit(0);
   }
 
   string hdr = invocationHeader(argc, argv);
 
+
   int k=1;
-  AtomicGroup model = createSystem(argv[k++]);
-  AtomicGroup subsystem = selectAtoms(model, argv[k++]);
-  AtomicGroup environment = selectAtoms(model, argv[k++]);
-  AtomicGroup combined = subsystem + environment;
 
-  DoubleMatrix s;
-  readAsciiMatrix(argv[k++], s);
+  FitAggregator uberfit;
 
-  DoubleMatrix U;
-  readAsciiMatrix(argv[k++], U);
-
-  // Now setup blocker & springs...
-  SpringFunction *spring = new HCA;
-  SuperBlock *blocker = new SuperBlock(spring, combined);
+  while (k < argc) {
+    string tag(argv[k++]);
+    AtomicGroup model = createSystem(argv[k++]);
+    AtomicGroup subsystem = selectAtoms(model, argv[k++]);
+    AtomicGroup environment = selectAtoms(model, argv[k++]);
+    AtomicGroup combined = subsystem + environment;
+    
+    DoubleMatrix s;
+    readAsciiMatrix(argv[k++], s);
+    
+    DoubleMatrix U;
+    readAsciiMatrix(argv[k++], U);
+    
+    // Now setup blocker & springs...
+    SpringFunction *spring = new HCA;
+    SuperBlock *blocker = new SuperBlock(spring, combined);
+    
+    VSA* vsa = new VSA(blocker, subsystem.size());
   
-  VSA vsa(blocker, subsystem.size());
+    ENMFitter* fitter = new ENMFitter(vsa, s, U);
+    fitter->name(tag);
+    fitter->verbose(true);
+    fitter->normalize(true);
+
+    uberfit.push_back(fitter);
+  }
+
+
   
   Simplex<double> simp(5);
   simp.tolerance(1e-4);
@@ -56,9 +73,12 @@ int main(int argc, char *argv[]) {
     lengths.push_back(*vi/2.0);
 
   simp.seedLengths(lengths);
-  
-  ENMFitter fitter(&vsa, s, U);
-  vector<double> fit = simp.optimize(seeds, fitter);
+
+  // Do a quick check first...
+  double check = uberfit(seeds);
+  cout << "*** Initial = " << -check << " ***\n";
+
+  vector<double> fit = simp.optimize(seeds, uberfit);
 
   cout << "Final fit: ";
   cout << simp.finalValue() << "\t= ";
