9643dd2dad57c4358a645345f09612292a3552b1
Tod Romo <drtromo@gmail.com>
2010-08-09 18:04:03 +0000

    Fleshed out real implementation (I hope)
    svn path=/software/LOOS/branches/ENM-Devel/; revision=4648

--- Tools/ElasticNetworks/fitter.hpp
+++ Tools/ElasticNetworks/fitter.hpp
@@ -1,7 +1,16 @@
-//
-// Fitter for ENM parameters
-// (c) 2010 Tod D. Romo, Grossfield Lab, URMC
-//
+/*
+ Fitter for ENM parameters
+ (c) 2010 Tod D. Romo, Grossfield Lab, URMC
+
+
+ Notes:
+
+ It's assumed that we will always be comparing with PCA results and
+ the number of skipped eigenpairs (for both sides) is 6.
+
+
+*/
+
 
 
 #if !defined(FITTER_HPP)
@@ -15,22 +24,73 @@ class ENMFitter {
 public:
   ENMFitter(const ElasticNetworkModel* model, const loos::DoubleMatrix& s, const loos::DoubleMatrix& U) :
     enm_(model),
-    eigvals_(s),
-    eigvecs_(U),
     normalize_(false)
-  { }
+  {
+    uint m = U.rows();
+    uint n = s.rows();
+
+    ref_eigvals_ = submatrix(s, Range(0, n-6), Range(0, 1));
+    ref_eigvecs_ = submatrix(U, Range(0, m), Range(0, n-6));
+
+
+    // These PCA eigenpairs actually come from an SVD, so must square
+    // the svals to make eigenvalues out of them...
+
+    for (uint j=0; j<n-6; ++j)
+      ref_eigvals_[j] *= ref_eigvals_[j];
+  }
 
   void normalize(const bool b) { normalize_ = b; }
   bool normalize() const { return(normalize_); }
 
   // Set the constants required for the ENM
-  virtual void setConstants(const std::vector<double>& konst) =0;
+  void setConstants(const std::vector<double>& konst) {
+    std::vector<double> v = konst;
+    model->setConstants(v);
+  }
+
+  double operator()(const std::vector<double>& v) {
+    setConstants(v);
+    model->solve();
+    
+    uint n = (model->eigenvalues()).rows();
+    uint m = (model->eigenvectors()).rows();
+
+    loos::DoubleMatrix s = submatrix(model->eigenvalues(), Range(6, n), Range(0, 1));
+    loos::DoubleMatrix U = submatrix(model->eigenvectors(), Range(0, m), Range(6, n));
+
+    if (normalize_) {
+      double scale = normalizePower(s);
+      for (uint j=0; j<s.rows(); ++j)
+        s[j] *= scale;
+    }
+
+    return(loos::Math::covarianceOverlap(s, U, ref_eigvals_, ref_eigvecs_));
+  }
 
 
 private:
+
+  // Scale factor to make the power contained in eigenvalues s match
+  // the reference eigenvalues.  Assumes zero eigenpairs have already
+  // been trimmed off.
+
+  double normalizePower(const loos::DoubleMatrix& s) {
+    double sumA = 0.0;
+    for (uint j=0; j<s.rows(); ++j)
+      sumA += s[j];
+
+    double sumB = 0.0;
+    for (uint j=0; j<ref_eigvals_.rows(); ++j)
+      sumB += ref_eigvals_[j];
+
+    return(sumB/sumA);
+  }
+
+private:
   ElasticNetworkModel *enm_;
-  loos::DoubleMatrix eigvals_;
-  loos::DoubleMatrix eigvecs_;
+  loos::DoubleMatrix ref_eigvals_;
+  loos::DoubleMatrix ref_eigvecs_;
 
   bool normalize_;
 
