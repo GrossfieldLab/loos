35629fb8a80c5b49be12ad46c148003b5c75ff4a
Tod Romo <drtromo@gmail.com>
2010-08-11 14:33:12 +0000

    Minimal error handling on command-line args
    svn path=/software/LOOS/branches/ENM-Devel/; revision=4685

--- Tools/ElasticNetworks/vsa_fit.cpp
+++ Tools/ElasticNetworks/vsa_fit.cpp
@@ -1,7 +1,11 @@
-//
-// Quick test of fitting
-// (c) 2010 Tod D. Romo, Grossfield Lab, URMC
-//
+/*
+  vsa_fit
+
+  (c) 2010 Tod D. Romo, Grossfield Lab, URMC
+
+
+  Fits a basic VSA to a set of PCA results
+*/
 
 
 
@@ -15,9 +19,21 @@ using namespace loos;
 using namespace std;
 
 
+
+void showSprings(ostream& os) {
+  vector<string> v = springNames();
+  os << "Valid springs: ";
+  for (vector<string>::iterator i = v.begin(); i != v.end(); ++i)
+    os << *i << ( i == v.end() - 1 ? "" : ", ");
+  os << endl;
+}
+
+
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    cout << "Usage- quickie tag model subsystem environment eigvals eigvecs [tag model sub env eigvals eigvecs ...]\n";
+    cout << "Usage- quickie spring-type spring-seed [spring-seed ...] tag model subsystem environment eigvals eigvecs [tag model sub env eigvals eigvecs ...]\n";
+    showSprings(cout);
     exit(0);
   }
 
@@ -34,9 +50,16 @@ int main(int argc, char *argv[]) {
   SpringFunction *spring = springFactory(argv[k++]);
   uint nargs = spring->paramSize();
   vector<double> seeds;
-  cout << "Expecting " << nargs << " seeds.\n";
+  cout << "Expecting " << nargs << " seeds for spring " << spring->name() << endl;
   for (uint i=0; i<nargs; ++i)
     seeds.push_back(strtod(argv[k++], 0));
+
+  if ( (argc - k) % 6 != 0) {
+    cerr << "Invalid number of arguments.\n";
+    cerr << "Usage- quickie spring-type spring-seed [spring-seed ...] tag model subsystem environment eigvals eigvecs [tag model sub env eigvals eigvecs ...]\n";
+    showSprings(cerr);
+    exit(-1);
+  }
 
   while (k < argc) {
     string tag(argv[k++]);
