a37a7903a0a1a5c75e51dd24d4ff7be380458978
Tod Romo <drtromo@gmail.com>
2010-08-20 19:13:21 +0000

    Now supports config files.  Substantial rewrite to CLI...
    svn path=/software/LOOS/branches/ENM-Devel/; revision=4735

--- Tools/ElasticNetworks/vsa_fit.cpp
+++ Tools/ElasticNetworks/vsa_fit.cpp
@@ -14,10 +14,162 @@
 #include "fitter.hpp"
 #include "vsa-lib.hpp"
 
+#include <boost/program_options.hpp>
+
 
 using namespace loos;
 using namespace std;
 using namespace ENM;
+
+namespace po = boost::program_options;
+
+
+// Globals...
+
+typedef vector<string>    vString;
+
+// Track these for cleanup...
+vector<Fitter*> fits;
+vector<SuperBlock*> blocks;
+vector<VSA*> vsas;
+SpringFunction* spring;
+
+int verbosity;
+bool mass_flag;
+
+vector<double> initial_seeds, initial_lengths;
+
+
+FitAggregator* parseOptions(int argc, char *argv[]) {
+
+  FitAggregator* uberfit = new FitAggregator;
+
+  try {
+    string config_file;
+
+    vString tags;
+    vString models;
+    vString pcas;
+    vString subs;
+    vString envs;
+
+    string spring_name;
+    double seed_scale;
+
+    po::options_description visible("Allowed options", 120);
+    visible.add_options()
+      ("help", "Produce this help message")
+      ("verbosity,v", po::value<int>(&verbosity)->default_value(0), "Verbosity level")
+      ("mass,m", po::value<bool>(&mass_flag)->default_value(false), "Enable use of mass in VSA")
+      ("config,C", po::value<string>(&config_file), "Options config file");
+
+
+    po::options_description optimization("Optimization Settings");
+    optimization.add_options()
+      ("spring", po::value<string>(&spring_name), "Spring function to use")
+      ("length", po::value<double>(&seed_scale), "Scale for seed lengths")
+      ("seeds", po::value< vector<double> >(&initial_seeds), "Seed values");
+
+
+    po::options_description system("System Description");
+    system.add_options()
+      ("tag", po::value<vString>(&tags)->composing(), "Name to associate with system")
+      ("model", po::value<vString>(&models)->composing(), "Model coordinates")
+      ("sub", po::value<vString>(&subs)->composing(), "Subsystem selection")
+      ("env", po::value<vString>(&envs)->composing(), "Environment selection")
+      ("pca", po::value<vString>(&pcas)->composing(), "PCA file prefix");
+    
+    po::options_description command_line;
+    command_line.add(visible).add(system).add(optimization);
+
+    po::positional_options_description p;
+    p.add("spring", 1);
+    p.add("length", 1);
+    p.add("seeds", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+              options(command_line).positional(p).run(), vm);
+    po::notify(vm);
+
+    // Now handle config file...
+    if (!config_file.empty()) {
+      ifstream ifs(config_file.c_str());
+      if (!ifs) {
+        cerr << "Cannot open config file " << config_file << endl;
+        exit(-1);
+      }
+      store(parse_config_file(ifs, command_line), vm);
+      notify(vm);
+    }
+
+    if (vm.count("help") || !(vm.count("spring") && vm.count("length") && vm.count("seeds"))) {
+      cerr << "Usage- vsa_fit [options] <systems> spring length seeds [seeds]\n";
+      cerr << visible;
+      cerr << system;
+      cerr << optimization;
+      exit(-1);
+    }
+
+    // Set up global spring function...
+    
+    spring = springFactory(spring_name);
+    uint nargs = spring->paramSize();
+    if (initial_seeds.size() != nargs) {
+      cerr << "Error- spring wanted " << nargs << " seed values.\n";
+      exit(-2);
+    }
+
+    for (vector<double>::iterator i = initial_seeds.begin(); i != initial_seeds.end(); ++i)
+      initial_lengths.push_back(*i * seed_scale);
+
+
+
+    for (uint i=0; i<tags.size(); ++i) {
+      AtomicGroup model = createSystem(models[i]);
+      if (mass_flag)
+        massFromOccupancy(model);
+      AtomicGroup subsystem = selectAtoms(model, subs[i]);
+      AtomicGroup environment = selectAtoms(model, envs[i]);
+      AtomicGroup combined = subsystem + environment;
+    
+      DoubleMatrix s;
+      readAsciiMatrix(pcas[i] + "_s.asc", s);
+    
+      DoubleMatrix U;
+      readAsciiMatrix(pcas[i] + "_U.asc", U);
+    
+      // Now setup blocker & springs...
+      SuperBlock *blocker = new SuperBlock(spring, combined);
+    
+      VSA* vsa = new VSA(blocker, subsystem.size());
+
+      if (mass_flag) {
+        DoubleMatrix M = getMasses(combined);
+        vsa->setMasses(M);
+      }
+  
+      Fitter* fitter = new Fitter(vsa, s, U);
+      fitter->name(tags[i]);
+      fitter->verbose(true);
+      fitter->normalize(true);
+
+      uberfit->push_back(fitter);
+
+      fits.push_back(fitter);
+      blocks.push_back(blocker);
+      vsas.push_back(vsa);
+    }
+    
+  }
+  catch(exception& e) {
+    cerr << "Error - " << e.what() << endl;
+    exit(-1);
+  }
+
+  return(uberfit);
+}
+
 
 
 
@@ -32,105 +184,30 @@ void showSprings(ostream& os) {
 
 
 int main(int argc, char *argv[]) {
-  if (argc == 1) {
-    cout << "Usage- vsa_fit [mass|nomass] spring-type spring-seed [spring-seed ...] tag model subsystem environment eigvals eigvecs [tag model sub env eigvals eigvecs ...]\n";
-    showSprings(cout);
-    exit(0);
-  }
-
+  
   string hdr = invocationHeader(argc, argv);
-
-
-  int k=1;
-
-  FitAggregator uberfit;
-
-  // Track allocations for cleanup...
-  vector<Fitter*> fits;
-  vector<SuperBlock*> blocks;
-  vector<VSA*> vsas;
-
-  // See whether to include masses...
-  bool mass_flag = (string(argv[k++]) == "mass");
-  cout << "mass_flag = " << mass_flag << endl;
-
-  SpringFunction *spring = springFactory(argv[k++]);
-  uint nargs = spring->paramSize();
-  vector<double> seeds;
-  cout << "Expecting " << nargs << " seeds for spring " << spring->name() << endl;
-  for (uint i=0; i<nargs; ++i)
-    seeds.push_back(strtod(argv[k++], 0));
-
-  if ( (argc - k) % 6 != 0) {
-    cerr << "Invalid number of arguments.\n";
-    cerr << "Usage- quickie spring-type spring-seed [spring-seed ...] tag model subsystem environment eigvals eigvecs [tag model sub env eigvals eigvecs ...]\n";
-    showSprings(cerr);
-    exit(-1);
-  }
-
-  while (k < argc) {
-    string tag(argv[k++]);
-    AtomicGroup model = createSystem(argv[k++]);
-    if (mass_flag)
-      massFromOccupancy(model);
-    AtomicGroup subsystem = selectAtoms(model, argv[k++]);
-    AtomicGroup environment = selectAtoms(model, argv[k++]);
-    AtomicGroup combined = subsystem + environment;
-    
-    DoubleMatrix s;
-    readAsciiMatrix(argv[k++], s);
-    
-    DoubleMatrix U;
-    readAsciiMatrix(argv[k++], U);
-    
-    // Now setup blocker & springs...
-    SuperBlock *blocker = new SuperBlock(spring, combined);
-    
-    VSA* vsa = new VSA(blocker, subsystem.size());
-
-    if (mass_flag) {
-      DoubleMatrix M = getMasses(combined);
-      vsa->setMasses(M);
-    }
+  FitAggregator* uberfit = parseOptions(argc, argv);
   
-    Fitter* fitter = new Fitter(vsa, s, U);
-    fitter->name(tag);
-    fitter->verbose(true);
-    fitter->normalize(true);
-
-    uberfit.push_back(fitter);
-
-    fits.push_back(fitter);
-    blocks.push_back(blocker);
-    vsas.push_back(vsa);
-  }
-
-
-  
-  Simplex<double> simp(nargs);
+  Simplex<double> simp(spring->paramSize());
   simp.tolerance(1e-4);
   
-  vector<double> lengths;
-  for (vector<double>::iterator vi = seeds.begin(); vi != seeds.end(); ++vi)
-    lengths.push_back(*vi/2.0);
-
-  simp.seedLengths(lengths);
+  simp.seedLengths(initial_lengths);
 
   // Do a quick check first...
   cout << "----INITIAL----\n";
-  double check = uberfit(seeds);
+  double check = (*uberfit)(initial_seeds);
   cout << "----INITIAL----\n";
-  uberfit.resetCount();
+  uberfit->resetCount();
 
 
-  vector<double> fit = simp.optimize(seeds, uberfit);
+  vector<double> fit = simp.optimize(initial_seeds, *uberfit);
 
   cout << "----FINAL----\n";
   cout << simp.finalValue() << "\t= ";
   copy(fit.begin(), fit.end(), ostream_iterator<double>(cout, "\t"));
   cout << endl;
-  uberfit.resetCount();
-  check = uberfit(fit);
+  uberfit->resetCount();
+  check = (*uberfit)(fit);
   cout << "----FINAL----\n";
   
 
