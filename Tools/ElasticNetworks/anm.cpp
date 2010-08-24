c99fee111b058e6903e5c45d0f2e3110ba6c19d0
Tod Romo <drtromo@gmail.com>
2010-08-24 14:40:43 +0000

    Removed unnecessary ifdef for typeid issue (turned out to be a non-issue)
    svn path=/software/LOOS/branches/ENM-Devel/; revision=4746

--- Tools/ElasticNetworks/anm.cpp
+++ Tools/ElasticNetworks/anm.cpp
@@ -84,6 +84,7 @@ int verbosity;
 bool debug;
 
 string spring_desc;
+string bound_spring_desc;
 
 void fullHelp() {
 
@@ -133,6 +134,7 @@ void parseOptions(int argc, char *argv[]) {
       ("debug,d", po::value<bool>(&debug)->default_value(false), "Turn on debugging (output intermediate matrices)")
       ("selection,s", po::value<string>(&selection)->default_value("name == 'CA'"), "Which atoms to use for the network")
       ("spring,S", po::value<string>(&spring_desc)->default_value("distance"),"Spring function to use")
+      ("bound,b", po::value<string>(&bound_spring_desc)->default_value("distance"), "Bound spring")
       ("fullhelp", "More detailed help");
 
     po::options_description hidden("Hidden options");
@@ -168,6 +170,22 @@ void parseOptions(int argc, char *argv[]) {
 }
 
 
+loos::Math::Matrix<int> buildConnectivity(const AtomicGroup& model) {
+  uint n = model.size();
+  loos::Math::Matrix<int> M(n, n);
+  
+  for (uint j=0; j<n-1; ++j)
+    for (uint i=j; i<n; ++i)
+      if (i == j)
+        M(j, i) = 1;
+      else
+        M(j, i) = model[j]->isBoundTo(model[i]);
+  
+  return(M);
+}
+
+
+
 int main(int argc, char *argv[]) {
 
   string header = invocationHeader(argc, argv);
@@ -185,6 +203,19 @@ int main(int argc, char *argv[]) {
 
   SuperBlock* blocker = new SuperBlock(spring, subset);
 
+  // Handle Decoration (if necessary)
+  if (!bound_spring_desc.empty()) {
+    if (! model.hasBonds()) {
+      cerr << "Error- cannot use bound springs unless the model has connectivity\n";
+      exit(-10);
+    }
+    loos::Math::Matrix<int> M = buildConnectivity(subset);
+    SpringFunction* bound_spring = springFactory(bound_spring_desc);
+    BoundSuperBlock* decorator = new BoundSuperBlock(blocker, bound_spring, M);
+    blocker = decorator;
+  }
+
+
   ANM anm(blocker);
   anm.debugging(debug);
   anm.prefix(prefix);
@@ -200,6 +231,4 @@ int main(int argc, char *argv[]) {
 
   writeAsciiMatrix(prefix + "_Hi.asc", anm.inverseHessian(), header, false);
 
-  delete blocker;
-  delete spring;
 }
