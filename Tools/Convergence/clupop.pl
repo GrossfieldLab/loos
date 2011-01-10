426e5bace0f1f12fcf08950f73e0f89ed57c25b7
Tod Romo <drtromo@gmail.com>
2011-01-10 21:09:15 +0000

    Over-engineering, of course!
    svn path=/software/LOOS/branches/C-Devel/; revision=5322

--- Tools/Convergence/clupop.pl
+++ Tools/Convergence/clupop.pl
@@ -7,6 +7,24 @@
 #
 
 use strict;
+use Getopt::Long;
+
+
+my $subtract_average = 0;
+my $log_scale = 0;
+my $zero_value = -20;
+
+
+my $hdr = $0 . ' ' . join(' ', @ARGV);
+
+my $ok = GetOptions(
+		    'average!' => \$subtract_average,
+		    'log!' => \$log_scale,
+		    'zero=f' => \$zero_value,
+		    'help' => sub { &help; }
+		   );
+
+$ok || die "Unknown option";
 
 my @data;
 my $maxbin = 0;
@@ -23,12 +41,58 @@ while (<>) {
 ++$maxbin;
 my @bins = (0) x $maxbin;
 my $n = 0;
+my @M;
 
 for (my $i=0; $i<$#data; ++$i) {
   ++$bins[$data[$i]];
   ++$n;
+  my @row;
   for (my $j=0; $j<=$#bins; ++$j) {
-    print $bins[$j] / $n, "\t";
+    push(@row, $bins[$j] / $n);
+  }
+  push(@M, \@row);
+}
+
+if ($subtract_average) {
+  for (my $i=0; $i<=$#bins; ++$i) {
+    my $avg = 0.0;
+    for (my $j=0; $j<=$#M; ++$j) {
+      $avg += $M[$j]->[$i];
+    }
+    $avg /= ($#M + 1);
+    
+    for (my $j=0; $j<=$#M; ++$j) {
+      $M[$j]->[$i] -= $avg;
+    }
+  }
+}
+
+if ($log_scale) { 
+  for (my $j=0; $j<=$#M; ++$j) {
+    for (my $i=0; $i<=$#bins; ++$i) {
+      if ($M[$j]->[$i] == 0) {
+	$M[$j]->[$i] = $zero_value;
+      } else {
+	$M[$j]->[$i] = log(abs($M[$j]->[$i]));
+      }
+    }
+  }
+}
+
+
+print "# $hdr\n";
+for (my $j=0; $j<=$#M; ++$j) {
+  for (my $i=0; $i<=$#bins; ++$i) {
+    print $M[$j]->[$i], "\t";
   }
   print "\n";
+}
+
+
+
+sub help {
+  print <<EOF;
+Usage- clupop.pl [--average] [--log [--zero=value]] assignments >matrix.asc
+EOF
+  exit 0;
 }
