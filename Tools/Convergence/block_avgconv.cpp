36b7eadb30c4a48005d8eeea2262ac6df7628fe5
Tod Romo <drtromo@gmail.com>
2011-01-21 19:37:16 +0000

    Now CORRECTLY computes block average of avg struct rmsd...added some progress logging too...
    svn path=/software/LOOS/branches/C-Devel/; revision=5380

--- Tools/Convergence/block_avgconv.cpp
+++ Tools/Convergence/block_avgconv.cpp
@@ -38,8 +38,8 @@ AtomicGroup averageSelectedSubset(const vector<AtomicGroup>& ensemble, const vec
 
 int main(int argc, char *argv[]) {
   
-  if (argc != 5) {
-    cerr << "Usage- block_avgconv model traj sel range\n";
+  if (argc < 5 || argc > 6) {
+    cerr << "Usage- block_avgconv model traj sel range [1 = do not align trajectory]\n";
     exit(0);
   }
 
@@ -50,32 +50,52 @@ int main(int argc, char *argv[]) {
   pTraj traj = createTrajectory(argv[k++], model);
   AtomicGroup subset = selectAtoms(model, argv[k++]);
   vector<uint> sizes = parseRangeList<uint>(argv[k++]);
+  bool do_align = argc != 6;
 
   cout << "# " << hdr << endl;
   cout << "# n\tavg\tvar\tblocks\tstderr\n";
 
   vector<AtomicGroup> ensemble;
+  cerr << "Reading trajectory...\n";
   readTrajectory(ensemble, subset, traj);
 
-  boost::tuple<vector<XForm>, greal, int> result = iterativeAlignment(ensemble);
-  AtomicGroup avg = averageStructure(ensemble);
-  
-  for (vector<uint>::iterator size = sizes.begin(); size != sizes.end(); ++size) {
-    TimeSeries<double> rmsds;
+  if (do_align) {
+    cerr << "Aligning trajectory...\n";
+    boost::tuple<vector<XForm>, greal, int> result = iterativeAlignment(ensemble);
+  } else
+    cerr << "Trajectory is already aligned!\n";
 
-    for (uint i=0; i<ensemble.size() - *size; i += *size) {
-      vector<uint> indices(*size);
-      for (uint j=0; j<*size; ++j)
+  cerr << "Processing- ";
+  for (uint block = 0; block < sizes.size(); ++block) {
+    if (block % 50)
+      cerr << ".";
+
+    uint blocksize = sizes[block];
+
+    vector<AtomicGroup> averages;
+    for (uint i=0; i<ensemble.size() - blocksize; i += blocksize) {
+
+      vector<uint> indices(blocksize);
+      for (uint j=0; j<blocksize; ++j)
         indices[j] = i+j;
       
-      AtomicGroup sub_avg = averageSelectedSubset(ensemble, indices);
-      sub_avg.alignOnto(avg);
-      rmsds.push_back(avg.rmsd(sub_avg));
+      averages.push_back(averageSelectedSubset(ensemble, indices));
     }
+    
+    TimeSeries<double> rmsds;
+    for (uint j=0; j<averages.size() - 1; ++j)
+      for (uint i=j+1; i<averages.size(); ++i) {
+        AtomicGroup left = averages[j];
+        AtomicGroup right = averages[i];
+        left.alignOnto(right);
+        rmsds.push_back(left.rmsd(right));
+      }
+        
 
     double v = rmsds.variance();
-    uint n = rmsds.size();
-    cout << boost::format("%d\t%f\t%f\t%d\t%f\n") % *size % rmsds.average() % v % n % sqrt(v/n);
+    uint n = averages.size();
+    cout << boost::format("%d\t%f\t%f\t%d\t%f\n") % blocksize % rmsds.average() % v % n % sqrt(v/n);
   }
   
+  cerr << "\nDone!\n";
 }
