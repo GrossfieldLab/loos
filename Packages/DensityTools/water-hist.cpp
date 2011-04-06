ff8a49f6ae5ffdd2e18f5b72e3064b6295469e26
Tod Romo <drtromo@gmail.com>
2011-04-06 19:22:03 +0000

    First step towards adhearing to the NWO...
    svn path=/software/LOOS/branches/Reorganization-2011/; revision=5737

--- Packages/DensityTools/water-hist.cpp
+++ Packages/DensityTools/water-hist.cpp
@@ -4,206 +4,131 @@
   (c) 2009 Tod D. Romo, Grossfield Lab, URMC
 */
 
-#include <loos.hpp>
-#include <sgrid.hpp>
 #include <boost/format.hpp>
 #include <boost/program_options.hpp>
 
-#include "internal_water_filter.hpp"
-#include "water-lib.hpp"
-#include "water-hist-lib.hpp"
+#include <loos.hpp>
+#include <DensityGrid.hpp>
+#include <internal-water-filter.hpp>
+#include <OptionsFramework.hpp>
+#include <DensityOptions.hpp>
 
 using namespace std;
 using namespace loos;
-using namespace lab;
-namespace po = boost::program_options;
-namespace h2o = banal::water;
+using namespace loos::DensityTools;
 
-string model_name, traj_name, prot_sel, solv_sel;
-double gridres, gridpad;
-vector<uint> indices;
-bool estimate_bulk;
-double bulk_zclip;
-bool rescale;
-bool count_zero;
-
-GCoord clamp_min, clamp_max;
-bool clamp = false;
-
-WaterFilter::Base* the_filter;
-
-void fullHelp(void) {
-  cerr << "Full help not available at this time...\n";
-}
+namespace opts = loos::DensityTools::OptionsFramework;
 
 
-void parseArgs(int argc, char *argv[]) {
-  string mode;
-  double radius;
-  double pad;
 
-  try {
-    po::options_description generic("Allowed options");
-    generic.add_options()
-      ("help", "Produce this help message")
-      ("fullhelp", "Extended help")
-      ("mode,m", po::value<string>(&mode)->default_value("axis"), "Water filter mode:\n"
-       "Values:\n"
-       "  axis: principal axis\n"
-       "   box: protein bounding box\n")
-      ("radius,r", po::value<double>(&radius)->default_value(10.0), "Radius (for principal axis filter)")
-      ("boxpad,b", po::value<double>(&pad)->default_value(1.0), "Pad the bounding box for internal waters")
-      ("zrange,z", po::value<string>(), "Clamp the volume to integrate over in Z (min:max)")
-      ("water,w", po::value<string>(), "Add bulk water (z-slices between cutoff and bounding box) (pad,zmin:zmax)")
-      ("range,R", po::value< vector<string> >(), "Frames to operate over")
-      ("gridpad,P", po::value<double>(&gridpad)->default_value(0.0), "Pad the grid bounds by this much")
-      ("protein,p", po::value<string>(&prot_sel)->default_value("name == 'CA'"), "Protein selection")
-      ("solvent,s", po::value<string>(&solv_sel)->default_value("name == 'OH2'"), "Solvent selection")
-      ("gridres,g", po::value<double>(&gridres)->default_value(0.5), "Grid resolution")
-      ("estimate,e", po::value<bool>(&estimate_bulk)->default_value(false), "Estimate bulk density")
-      ("countzero,c", po::value<bool>(&count_zero)->default_value(false), "Count empty voxels in the density estimate")
-      ("bulkz,Z", po::value<double>(&bulk_zclip), "Only consider waters with z > this for bulk")
-      ("scale,S", po::value<bool>(&rescale)->default_value(false), "Rescale density based on bulk estimator")
+class WaterHistogramOptions : public OptionsPackage {
+public:
+  WaterHistogramOptions() :
+    grid_resolution(1.0),
+    count_empty_voxels(false),
+    rescale_density(false),
+    bulk_zclip(0.0)
+  { }
+
+  void addGeneric(po::options_descriptions& opts) {
+    opts.add_options()
+      ("gridres,G", po::value<double>(&grid_resolution)->default_value(grid_resolution), "Grid resolution")
+      ("empty", po::value<bool>(&count_empty_voxels)->default_value(count_empty_voxels), "Count empty voxels in bulk density estimate")
+      ("bulkz,Z", po::value<double>(&bulk_zclip)->default_value(bulk_zclip), "Bulk water is defined as |Z| >= k")
+      ("scale,S", po::value<bool>(&rescale_density)->default_value(rescale_density), "Scale density by bulk estimate")
       ("clamp,C", po::value<string>(), "Clamp the bounding box [(x,y,z),(x,y,z)]");
+  }
 
 
-
-    po::options_description hidden("Hidden options");
-    hidden.add_options()
-      ("model", po::value<string>(&model_name), "Model")
-      ("traj", po::value<string>(&traj_name), "Trajectory");
-    
-
-    po::options_description command_line;
-    command_line.add(generic).add(hidden);
-
-    po::positional_options_description p;
-    p.add("model", 1);
-    p.add("traj", 1);
-
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).
-              options(command_line).positional(p).run(), vm);
-    po::notify(vm);
-    
-    if (vm.count("help") || vm.count("fulhelp") || !(vm.count("model") && vm.count("traj"))) {
-      cerr << "Usage- " << argv[0] << " [options] model trajectory >output\n";
-      cerr << generic;
-      if (vm.count("fullhelp"))
-        fullHelp();
-      exit(-1);
-    }
-    
-    if (vm.count("range")) {
-      vector<string> range_spec = vm["range"].as< vector<string> >();
-      indices = parseRangeList<uint>(range_spec);
-    }
-
-    
-    // Handle modes & validation
-    if (mode == "axis")
-      the_filter = new WaterFilter::Axis(radius);
-    else if (mode == "box") {
-      the_filter = new WaterFilter::Box(pad);
-      gridpad += pad;
-    } else {
-      cerr << boost::format("ERROR - unknown mode '%s'\n") % mode;
-      exit(-1);
-    }
-
-    // Handle "decoration"
-    if (vm.count("zrange")) {
-      double zmin, zmax;
-      string s = vm["zrange"].as<string>();
-      int i = sscanf(s.c_str(), "%lf:%lf", &zmin, &zmax);
-      if (i != 2) {
-        cerr << boost::format("ERROR - unable to parse zrange '%s'\n") % s;
-        exit(-1);
-      }
-
-      the_filter = new WaterFilter::ZClipped(the_filter, zmin, zmax);
-    }
-
-
-    if (vm.count("water")) {
-      double zmin, zmax, pad;
-      string s = vm["water"].as<string>();
-      int i = sscanf(s.c_str(), "%lf,%lf:%lf", &pad, &zmin, &zmax);
-      if (i != 3) {
-        cerr << boost::format("ERROR - unable to parse bulk range '%s'\n") % s;
-        exit(-1);
-      }
-
-      the_filter = new WaterFilter::Bulked(the_filter, pad, zmin, zmax);
-    }
-
-
-    // Verify some combinations...
-    if (rescale && !estimate_bulk)
-      throw(runtime_error("You must estimate bulk density in order to rescale"));
-
-    if (vm.count("clamp")) {
-      string s = vm["clamp"].as<string>();
+  bool postConditions(po::variables_map& map) {
+    GCoord clamp_min, clamp_max;
+    if (map.count("clamp")) {
+      string s = map["clamp"].as<string>();
       stringstream ss(s);
-      if (!(ss >> clamp_min))
-        throw(runtime_error("Cannot parse lower bounds of clamp"));
-      int c = ss.get();
-      if (c != ',')
-        throw(runtime_error("Error parsing clamp"));
-      if (!(ss >> clamp_max))
-        throw(runtime_error("Cannot parse upper bounds of clamp"));
-      clamp = true;
-    }
+      if (!(ss >> clamp_min)) {
+        cerr << "Error- cannot parse lower bounds for box-clamp\n";
+        return(false);
+      }
+      clamped_box.push_back(clamp_min);
 
+      int c = ss.get();
+      if (c != ',') {
+        cerr << "Error- cannot parse box-clamp\n";
+        return(false);
+      }
+      if (!(ss >> clamp_max)) {
+        cerr << "Error- cannot parse upper bounds for box-clamp\n";
+        return(false);
+      clamped_box.push_back(clamp_max);
+    }
+    return(true);
+  }
     
-  }
-  catch(std::exception& e) {
-    cerr << "Error - " << e.what() << endl;
-    exit(-1);
-  }
-}
+
+public:
+  double grid_resolution;
+  bool count_empty_voxels;
+  bool rescale_density;
+  double bulk_zclip;
+  vector<GCoord> clamped_box;
+
+
+
+};
+
+
+
+
 
 
 
 
 int main(int argc, char *argv[]) {
   string hdr = invocationHeader(argc, argv);
-  parseArgs(argc, argv);
 
-  AtomicGroup model = createSystem(model_name);
-  pTraj traj = createTrajectory(traj_name, model);
+  opts::BasicOptions* basopts = new opts::BasicOptions;
+  opts::BasicTrajectoryOptions* trajopts = new opts::BasicTrajectoryOptions;
+  opts::BasicWaterOptions* watopts = new opts::BasicWaterOptions;
+  WaterHistogramOptions *xopts = new WaterHistogramOptions;
 
-  if (indices.empty())
-    for (uint i=0; i<traj->nframes(); ++i)
-      indices.push_back(i);
+  opts::AggregateOptions options;
+  options.add(basopts).add(trajopts).add(watopts).add(xopts);
+  if (!options.parse(argc, argv))
+    exit(-1);
 
-  AtomicGroup protein = selectAtoms(model, prot_sel);
-  AtomicGroup water = selectAtoms(model, solv_sel);
 
-  h2o::BulkEstimator* est;
-  if (estimate_bulk) {
+
+  AtomicGroup model = createSystem(trajopts->model_name);
+  pTraj traj = createTrajectory(trajopts->traj_name, model);
+  vector<uint> indices = opts::assignFrameIndices(trajm trajopts->frame_index_spec, trajopts->skip);
+
+  AtomicGroup protein = selectAtoms(model, watopts->prot_sel);
+  AtomicGroup water = selectAtoms(model, watopts->water_sel);
+
+  BulkEstimator* est;
+  if (xopts->rescale_density) {
     // Double-check the clip
     traj->readFrame(indices[0]);
     traj->updateGroupCoords(protein);
     vector<GCoord> bdd = protein.boundingBox();
-    if (bulk_zclip <= bdd[1].z())
+    if (xopts->bulk_zclip <= bdd[1].z())
       cerr << "***WARNING: the z-clip for bulk solvent overlaps the protein***\n";
 
-    h2o::ZClipEstimator* myest = new h2o::ZClipEstimator(water, traj, indices, bulk_zclip, gridres);
+    ZClipEstimator* myest = new ZClipEstimator(water, traj, indices, xopts->bulk_zclip, xopts->grid_resolution);
     myest->countZero(count_zero);
     est = myest;
   } else
-    est = new h2o::NullEstimator();
+    est = new NullEstimator();
 
   cerr << *est << endl;
 
-  h2o::WaterHistogrammer wh(protein, water, est, the_filter);
-  if (clamp) {
+  WaterHistogrammer wh(protein, water, est, watopts->filter_func);
+  if (!xopts->clamped_box.empty()) {
     clamp_min -= gridpad;
     clamp_max += gridpad;
-    wh.setGrid(clamp_min, clamp_max, gridres);
+    wh.setGrid(xopts->clamped_box[0]-watopts->pad, xopts->clamped_box[1]+watopts->pad, xopts->grid_resolution);
   } else
-    wh.setGrid(traj, indices, gridres, gridpad);
+    wh.setGrid(traj, indices, xopts->grid_resolution, watopts->pad);
 
   wh.accumulate(traj, indices);
 
@@ -211,14 +136,14 @@ int main(int argc, char *argv[]) {
   if (ob)
     cerr << "***WARNING***  There were " << ob << " out of bounds waters\n";
   
-  SGrid<double> grid = wh.grid();
+  DensityGrid<double> grid = wh.grid();
   cerr << boost::format("Grid = %s x %s @ %s\n") % grid.minCoord() % grid.maxCoord() % grid.gridDims();
 
   if (estimate_bulk) {
     double d = est->bulkDensity();
     double s = est->stdDev(d);
     cerr << boost::format("Bulk density estimate = %f, std = %f\n") % d % s;
-    if (rescale) {
+    if (xopts->rescale_density) {
       cerr << "Rescaling grid by bulk estimate...\n";
       grid.scale(1.0 / d);
       stringstream ss;
