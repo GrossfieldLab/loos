b9fc71a1c90d89ea72c11db8961f1b41cbe2b415
Tod Romo <drtromo@gmail.com>
2011-04-18 20:56:59 +0000

    Fixed periodic box units for gro files
    svn path=/software/LOOS/trunk/; revision=5792

--- gro.cpp
+++ gro.cpp
@@ -63,7 +63,7 @@ namespace loos {
     GCoord box;
     if (!(iss >> box[0] >> box[1] >> box[2]))
       throw(std::runtime_error("Cannot parse box '" + buf + "'"));
-    periodicBox(box);
+    periodicBox(box * 10.0);
 
     // Since the atomic field in .gro files is only 5-chars wide, it can
     // overflow.  if there are enough atoms to cause an overflow, manually
