17f25e9e6177c4d04eb0dd2040b3dfe41e5d7b45
Tod Romo <drtromo@gmail.com>
2011-05-19 20:03:49 +0000

    Added new utility function,        vectorAsStringWithCommas<T>(const vector<T>& v)
    svn path=/software/LOOS/branches/Reorganization-2011/; revision=6005

--- utils.hpp
+++ utils.hpp
@@ -301,6 +301,13 @@ namespace loos {
   //! Builds a list of trajectory indices (frame_index_spec supercedes skip)
   std::vector<uint> assignTrajectoryFrames(const pTraj& traj, const std::string& frame_index_spec, uint skip);
 
+  //! Convert a vector of type T to a string-list with commas
+  template<typename T> std::string vectorAsStringWithCommas(const std::vector<T>& v) {
+    std::ostringstream oss;
+    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(oss, ","));
+    return(oss.str());
+  }
+
 };
 
 #endif
