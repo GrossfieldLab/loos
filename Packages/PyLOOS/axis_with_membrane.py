29884ff0d2ce7438ab02bbb24376f73a5ab6f727
Tod Romo <drtromo@gmail.com>
2012-04-17 19:47:53 +0000

    Header commends a la Alan...now takes args from argv
    svn path=/software/LOOS/trunk/; revision=7903

--- Packages/PyLOOS/axis_with_membrane.py
+++ Packages/PyLOOS/axis_with_membrane.py
@@ -1,36 +1,51 @@
-#  This file is part of LOOS.
-#
-#  LOOS (Lightweight Object-Oriented Structure library)
-#  Copyright (c) 2012 Tod D. Romo, Grossfield Lab
-#  Department of Biochemistry and Biophysics
-#  School of Medicine & Dentistry, University of Rochester
-#
-#  This package (LOOS) is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation under version 3 of the License.
-#
-#  This package is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#!/usr/bin/env python
+"""
+  axis_with_membrane computes the cosine of the dot-product between the first
+  principal axis of a selection and the Z-axis (i.e. the putative membrane
+  normal).  This is written out for a trajectory as a time series...
+"""
+"""
+
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2012 Tod Romo, Grossfield Lab
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 
 
 # Import LOOS 
 from loos import *
 from math import *
+import sys
+
+## Command line arguments
+model_name = sys.argv[1]
+traj_name = sys.argv[2]
+selection = sys.argv[3]
 
 # Define the system first
-mol = createSystem("lfb.psf")
+mol = createSystem(model_name)
 
 # Now define the trajectory
-traj = createTrajectory("lfb.dcd", mol)
+traj = createTrajectory(traj_name, mol)
 
 # Select the backbone atoms coming from segment PE1
-backbone = selectAtoms(mol, "name =~ '^(C|O|N|CA)$' && segid == 'PE1'")
+backbone = selectAtoms(mol, selection)
 
 # Iterate over all frames in the trajectory...
 t = 0
