9b7e36913957a49c78370c33d5316cbf5251a096
Tod Romo <drtromo@gmail.com>
2012-11-16 16:25:15 +0000

    Fixed error in fullhelp (found by James)
    svn path=/software/LOOS/trunk/; revision=9011

--- Packages/ElasticNetworks/anm.cpp
+++ Packages/ElasticNetworks/anm.cpp
@@ -134,7 +134,7 @@ string fullHelpMessage() {
     "\n"
     "\n\n"
     "EXAMPLES\n\n"
-    "anm 'resid >= 10 && resid <= 50 && name == \"CA\"' 15.0 foo.pdb foo\n"
+    "anm --selection 'resid >= 10 && resid <= 50 && name == \"CA\"' 15.0 foo.pdb foo\n"
     "\tCompute the ANM for residues #10 through #50 with a 15 Angstrom cutoff\n"
     "\ti.e. construct contacts using only the CA's that are within 15 Angstroms\n"
     "\n"
