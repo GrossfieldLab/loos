d9696a0008a74bea810fa4e4a86e7d6bc52738a7
Tod Romo <drtromo@gmail.com>
2013-02-11 21:22:04 +0000

    Substantial revamp of release prep script.  Will now verify installation and generate a gzipped release tarball.  Instructions for creating a release on sourceforge are also embedded in the script.
    Requires check_loos_install.pl (in the software/scripts dir in the
    SVN) to be in your path prior to running.
    
    
    svn path=/software/LOOS/trunk/; revision=9336

--- prep_release.sh
+++ prep_release.sh
@@ -8,22 +8,65 @@
 #
 #
 
+
+export PREF=/var/tmp/loos-inst   # Where to put test install of LOOS
+export RELDIR=/var/tmp           # Where to export LOOS release for tarballing
+export PROCS=4                   # Number of procs to use in scons build
+
+export SVN=svn                   # Change this to echo for debugging (disables checkins)
+
 ### Small sanity check...
 OK=`pwd | egrep 'tags/release-[0-9.]+$'`
 if [ ! $OK ] ; then
-    echo "ERROR - you don't appear to be prepping a release in a release directory!"
-    echo "        Make sure you are in a properly tagged release from the SVN"
-    echo "        prior to running this script."
-    exit -1
+    echo "WARNING!!!"
+    echo "You don't appear to be prepping a release in a release directory!"
+    echo "Make sure you are in a properly tagged release from the SVN"
+    echo "prior to running this script."
+
+    read -p 'Do you want to proceed anyway? [y/N] ' OK
+    if [ "$OK" = "N" -o "$OK" = "n" -o \( -z "$OK" \) ] ; then
+	echo "Release aborted"
+	exit -1
+    fi
 fi
 
 
-# Build the documentation...
+echo "*** Building documentation"
+echo "+ Cleaning..."
+#rm -rf Doc                # Manually remove since scons wont
+#scons -cs caboodle        # Clean everything (to be safe)
+echo "+ Building..."
+scons -sj$PROCS docs      # Rebuild docs explicitly
 
-scons -cs caboodle
-rm -rf Docs
 
-# One more sanity check...
+
+echo "*** Checking install target"
+scons -sj$PROCS install PREFIX=$PREF
+CWD=`pwd`
+if check_loos_install.pl --nofull --prep `pwd` $PREF ; then
+    echo "Install appears ok.  Generating full report..."
+    ( echo "***INSTALL APPEARS OK***" ;\
+      echo "Please check the diff list below for any errors..." ;\
+      echo ;\
+      check_loos_install.pl --full `pwd` $PREF ) | less
+
+    read -p 'Are you sure you want to proceed? [y/N] ' OK
+    if [ "$OK" = "N" -o "$OK" = "n" -o \( -z "$OK" \) ] ; then
+	echo "Release aborted"
+	exit -1
+    fi
+
+else
+    echo "***SUSPECTED ERRORS IN INSTALLATION***"
+    echo "Aborting"
+    exit -1
+fi
+
+echo "*** Removing test install"
+rm -r $PREF
+
+
+echo "*** Checking SVN status"
 OK=`svn -q status`
 if [ -n "$OK" ] ; then
     echo "ERROR - you appear to have unchecked modifications to this release."
@@ -34,10 +77,9 @@ if [ -n "$OK" ] ; then
     exit -1
 fi
 
-scons -s docs
 
-# Massage the ChangeLog
 
+echo "*** Fixing ChangeLog"
 cp ChangeLog ChangeLog-
 sed -r 's/@[^>]+//' <ChangeLog- >ChangeLog
 rm ChangeLog-
@@ -52,16 +94,60 @@ read -p 'Shall I automatically update the SVN for you? (this will also remove th
 if [ "$OK" = "Y" -o "$OK" = "y" -o \( -z "$OK" \) ] ; then
     echo "Updating SVN for $RELEASE..."
     
-    set -o xtrace
-    svn add Docs docs.built
-    svn ci -m "Added Documentation to $RELEASE" Docs
-    svn ci -m "Added docs marker to $RELEASE" docs.built
-    svn ci -m "Sanitized ChangeLog prior to $RELEASE" ChangeLog
-    svn rm prep_release.sh
-    svn ci -m "Removed prep_release script from $RELEASE" prep_release.sh
+#    set -o xtrace
+    echo "+ Adding documentation"
+    $SVN add Docs docs.built
+    $SVN ci -m "Added Documentation to $RELEASE" Docs
+    $SVN ci -m "Added docs marker to $RELEASE" docs.built
+
+    echo "+ Checking in ChangeLog"
+    $SVN ci -m "Sanitized ChangeLog prior to $RELEASE" ChangeLog
+
+    echo "+ Removing prep_release script"
+    $SVN rm prep_release.sh
+    $SVN ci -m "Removed prep_release script from $RELEASE" prep_release.sh
+
+    echo "*** Making Release Tarball"
+    WHAT=`pwd | sed 's@^.*LOOS/@@'`
+    echo "Release base: $WHAT"
+    VERS=`echo $WHAT | sed 's/^.*-//'`
+    echo "Release version: $VERS"
+
+
+
+    pushd $RELDIR
+
+    svn export svn+ssh://membrane.urmc.rochester.edu/home/svn/subversion/software/LOOS/$WHAT loos-$VERS
+    tar cvf - loos-$VERS | gzip -cv9 >~/loos-$VERS.tar.gz
+    rm -r loos-$VERS
+    popd
+
+    cat <<EOF
+
+==== UPDATE INSTRUCTIONS ====
+
+The release is in your home directory.  Look in the SVN for the README file.
+
+
+Login on sourceforge and go into the developer's panel and select the
+file list.  Create a new dir for the release and upload the tarball to
+it.  You'll also need to create a README file and upload this
+too...this is what gets shown beneath the file as a "release notes."
+Finally, go back to the tarball file and click on the "i" information
+icon and click on "select all" for the default download.  Now, to
+update the documentation, go to the Docs directory within the tagged
+release in the SVN.  Execute the following rsync command:
+
+  rsync -avk --delete --exclude='.svn' . username,loos@web.sourceforge.net:htdocs
+
+with your Sourceforge username and password.
+   
+EOF
 
 else
     echo "***WARNING***WARNING***WARNING***"
+    echo
+    echo "Manual release is expected now"
     echo
     echo "DON'T FORGET YOU MUST UPDATE THE SVN PRIOR TO RELEASING $RELEASE"
 fi
