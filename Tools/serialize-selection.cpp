510ef3ce1903c31b1bb5900b597d6f7fe2aa2524
Tod Romo <drtromo@gmail.com>
2013-04-18 20:08:02 +0000

    Adding tool to serialize a selection into one big trajectory (assuming the selection contains multiple, identical molecules)
    svn path=/software/LOOS/trunk/; revision=9689

---
+++ Tools/serialize-selection.cpp
@@ -0,0 +1,63 @@
+// (c) 2013 Tod D. Romo, Grossfield Lab, URMC
+
+
+#include <loos.hpp>
+
+using namespace std;
+using namespace loos;
+
+
+int main(int argc, char *argv[]) 
+{
+    if (argc != 5) {
+        cerr << "Usage- serialize-selection output-prefix model trajectory selection\n";
+        exit(-1);
+    }
+
+    string hdr = invocationHeader(argc, argv);
+    
+    int k = 1;
+    string prefix(argv[k++]);
+    AtomicGroup model = createSystem(argv[k++]);
+    pTraj traj = createTrajectory(argv[k++], model);
+    AtomicGroup subset = selectAtoms(model, argv[k++]);
+
+    vector<AtomicGroup> molecules;
+    if (model.hasBonds())
+        molecules = subset.splitByMolecule();
+    else
+        molecules = subset.splitByUniqueSegid();
+
+    bool structure_written = false;
+    AtomicGroup outgroup = molecules[0].copy();
+    DCDWriter dcd(prefix + ".dcd");
+
+    PercentProgressWithTime watcher;
+    ProgressCounter<PercentTrigger, EstimatingCounter> slayer(PercentTrigger(0.1), EstimatingCounter(traj->nframes() * molecules.size()));
+    slayer.attach(&watcher);
+    slayer.start();
+    
+    while (traj->readFrame()) {
+        traj->updateGroupCoords(model);
+
+        for (uint j=0; j<molecules.size(); ++j) {
+            for (uint i=0; i<outgroup.size(); ++i)
+                outgroup[i]->coords(molecules[j][i]->coords());
+            if (!structure_written) {
+                PDB pdb = PDB::fromAtomicGroup(outgroup);
+                pdb.remarks().add(hdr);
+
+                string pdbname = prefix + ".pdb";
+                ofstream ofs(pdbname.c_str());
+                ofs << pdb;
+
+                structure_written = true;
+            }
+
+            dcd.writeFrame(outgroup);
+            slayer.update();
+        }
+    }
+
+    slayer.finish();
+}
