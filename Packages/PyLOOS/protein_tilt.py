78934d06b327a0d89e50a094bc670ef4a44a82ec
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2013-08-01 13:19:17 +0000

    New LOOS tool: protein_tilt.py computes the average tilt of a set of                transmembrane helices
    svn path=/software/LOOS/trunk/; revision=10480

---
+++ Packages/PyLOOS/protein_tilt.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import loos
+import sys
+import math
+
+if len(sys.argv) < 4 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print "Usage: ", sys.argv[0], " system trajectory selection1 [selection2...]"
+    print "       Prints the average of the orientation vectors of the "
+    print "       individual selections, assuming each individual vector "
+    print "       points in the +z direction"
+    sys.exit()
+
+print len(sys.argv)
+print "#", " ".join(sys.argv)
+system_filename = sys.argv[1]
+traj_filename = sys.argv[2]
+selections = sys.argv[3:]
+
+
+system = loos.createSystem(system_filename)
+traj = loos.createTrajectory(traj_filename, system)
+
+helices = []
+for s in selections:
+    helices.append(loos.selectAtoms(system, s))
+
+
+vec = loos.GCoord(0.,0.,0.)
+
+frame = 0
+
+print "#Frame\tAngle\tCosine"
+
+while (traj.readFrame()):
+    traj.updateGroupCoords(system)
+
+    for h in helices:
+        pca = h.principalAxes()
+        v = pca[0]
+        if v.z() < 0:
+            v *= -1.
+        vec += v
+
+    cosine = vec.z() / vec.length()
+    
+    cosine = max(-1.0, cosine)
+    cosine = min(1.0, cosine)
+    ang = math.acos(cosine) * 180./math.pi
+    
+    print frame, ang, cosine
+
+    frame += 1
+
+    
