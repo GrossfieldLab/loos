9b962f98a9bc3d088ea0eb963a30fb47ab0cb57b
Tod Romo <drtromo@gmail.com>
2013-10-07 20:22:09 +0000

    Using index()
    svn path=/software/LOOS/branches/SlowerWithMoreBugs/; revision=10865

--- amber_netcdf.cpp
+++ amber_netcdf.cpp
@@ -149,7 +149,7 @@ namespace loos {
   void AmberNetcdf::updateGroupCoords(AtomicGroup& g) {
 
     for (AtomicGroup::iterator i = g.begin(); i != g.end(); ++i) {
-      uint idx = (*i)->id() - 1;
+      uint idx = (*i)->index();
       if (idx < 0 || idx >= _natoms)
         throw(LOOSError(**i, "Atom index into trajectory frame is out of bounds"));
       idx *= 3;
