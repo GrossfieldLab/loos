37bf5c0be9175cb956858e82eea5ef8e406f8534
Tod Romo <drtromo@gmail.com>
2013-10-07 20:34:45 +0000

    Fixed index type and pointless comparison
    svn path=/software/LOOS/branches/SlowerWithMoreBugs/; revision=10870

--- dcd.cpp
+++ dcd.cpp
@@ -382,8 +382,8 @@ namespace loos {
 
   void DCD::updateGroupCoords(AtomicGroup& g) {
     for (AtomicGroup::iterator i = g.begin(); i != g.end(); ++i) {
-      int idx = (*i)->index();
-      if (idx < 0 || idx >= _natoms)
+      uint idx = (*i)->index();
+      if (idx >= _natoms)
         throw(LOOSError(**i, "Atom index into the trajectory frame is out of bounds"));
       (*i)->coords(GCoord(xcrds[idx], ycrds[idx], zcrds[idx]));
     }
