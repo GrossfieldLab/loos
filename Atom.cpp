77c1f2f234d4a7b7b2c274d6d1a075981a0ef632
Tod Romo <drtromo@gmail.com>
2013-10-08 13:55:09 +0000

    Added index to pseudo-XML output
    svn path=/software/LOOS/branches/SlowerWithMoreBugs/; revision=10892

--- Atom.cpp
+++ Atom.cpp
@@ -208,7 +208,7 @@ namespace loos {
 
 
   std::ostream& operator<<(std::ostream& os, const loos::Atom& a) {
-    os << "<ATOM ID='" << a._id << "' NAME='" << a._name << "' ";
+    os << "<ATOM INDEX='" << a._index << "' ID='" << a._id << "' NAME='" << a._name << "' ";
     os << "RESID='" << a._resid << "' RESNAME='" << a._resname << "' ";
     os << "COORDS='" << a._coords << "' ";
     os << "ALTLOC='" << a._altloc << "' CHAINID='" << a._chainid << "' ICODE='" << a._icode << "' SEGID='" << a._segid << "' ";
