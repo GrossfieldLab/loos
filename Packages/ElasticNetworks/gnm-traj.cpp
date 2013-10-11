920da6f18ff684b2424bec0b6cb4b53b2dc6fff0
Tod Romo <drtromo@gmail.com>
2013-10-11 15:14:23 +0000

    restored NWO invocation
    svn path=/software/LOOS/branches/TrajicENM/; revision=10934

--- Packages/ElasticNetworks/gnm-traj.cpp
+++ Packages/ElasticNetworks/gnm-traj.cpp
@@ -13,7 +13,7 @@
   This file is part of LOOS.
 
   LOOS (Lightweight Object-Oriented Structure library)
-  Copyright (c) 2013 Tod D. Romo
+  Copyright (c) 2008,2013 Tod D. Romo
   Department of Biochemistry and Biophysics
   School of Medicine & Dentistry, University of Rochester
 
@@ -36,6 +36,83 @@
 
 using namespace std;
 using namespace loos;
+namespace opts = loos::OptionsFramework;
+namespace po = loos::OptionsFramework::po;
+
+
+
+string fullHelpMessage() {
+  string msg = 
+    "\n"
+    "\n"
+    "SYNOPSIS\n"
+    "\n"
+    "Compute the normal modes of a guassian network model\n"
+    "\n"
+    "DESCRIPTION\n"
+    "Computes the gaussian normal mode analysis of an ENM\n"
+    "This is done by building the Kirchoff  matrix given a PDB\n"
+    "and a selection, then computing the SVD of the  matrix and\n"
+    "finally computing the pseudo-inverse.\n"
+    "See: Bahar, et al., Folding and Design 2, 173-181, (1997).\n"
+    "\n"
+    "This will create the following files:\n"
+    "\tfoo_K.asc  - Kirchoff matrix\n"
+    "\tfoo_U.asc  - Left singular vectors\n"
+    "\tfoo_s.asc  - singular values\n"
+    "\tfoo_V.asc  - Right singular vectors\n"
+    "\tfoo_Ki.asc - Pseudo-inverse of K\n"
+    "\n"
+    "Notes:\n"
+    "- The default selection (if none is specified) is to pick CA's\n"
+    "- The output is ASCII format suitable for use with Matlab/Octave/Gnuplot\n"
+    //
+    "\n"
+    "EXAMPLES\n"
+    "\n"
+    "gnm -c8.2 -s 'resid >= 10 && resid <= 50 && name == \"CA\"' model.pdb foo\n"
+    "\tCompute the GNM of model.pdb for residues #10 through #50 with\n"
+    "\tan 8.2 Angstrom cutoff i.e. construct contacts using only the CA's\n"
+    "\tthat are within 8.2 Angstroms.  Write out the files to foo_X.asc\n"
+    "\t\n"
+    "SEE ALSO\n"
+    "\n"
+    "Packages/ElasticNetworks/anm - \n"
+    "The anisotropic version of this tool.  Here eigenvectors predicting\n"
+    "the direction of movements are written out as well.\n"
+    "\t\n"
+    "Packages/ElasticNetworks/vsa - \n"
+    "This is an extension of the ANM method mentioned above that splits\n"
+    "the calculation into two parts - a subsystem and an environment.\n"
+    "These eigendecompositions of these two parts are performed separately\n"
+    "and the environment can then be 'subtracted' off the subsystem.\n"
+    "\n";
+
+  return(msg);
+  
+}
+
+
+// @cond TOOLS_INTERNAL
+class ToolOptions : public opts::OptionsPackage {
+public:
+  
+  void addGeneric(po::options_description& o) {
+    o.add_options()
+      ("cutoff", po::value<double>(&cutoff)->default_value(7.0), "Distance cutoff");
+  }
+
+  string print() const {
+    ostringstream oss;
+    oss << boost::format("cutoff='%f'") % cutoff;
+    return(oss.str());
+  }
+
+  double cutoff;
+};
+
+
+// @endcond
 
 
 // This is the Kirchoff normalization constant (see Bahar, Atilgan,
@@ -72,25 +149,30 @@ DoubleMatrix kirchoff(AtomicGroup& group, const double cutoff) {
 int main(int argc, char *argv[]) 
 {
   string hdr = invocationHeader(argc, argv);
-  int k = 1;
-  
-  if (argc != 6) {
-    cerr << "Usage- gnm-traj output-prefix model traj cutoff selection\n";
+
+  opts::BasicOptions* bopts = new opts::BasicOptions(fullHelpMessage());
+  opts::OutputPrefix* propts = new opts::OutputPrefix("gnm_traj");
+  opts::BasicSelection* sopts = new opts::BasicSelection("name == 'CA'");
+  opts::BasicTrajectory* tropts = new opts::BasicTrajectory;
+  ToolOptions* topts = new ToolOptions;
+
+  opts::AggregateOptions options;
+  options.add(bopts).add(propts).add(sopts).add(tropts).add(topts);
+  if (!options.parse(argc, argv))
     exit(-1);
-  }
-  
-  string prefix(argv[k++]);
-  AtomicGroup model = createSystem(argv[k++]);
-  pTraj traj = createTrajectory(argv[k++], model);
-  double cutoff = strtod(argv[k++], 0);
-  AtomicGroup subset = selectAtoms(model, argv[k++]);
-  
-  uint t = 0;
 
-  
+  AtomicGroup model = tropts->model;
+  AtomicGroup subset = selectAtoms(model, sopts->selection);
+  pTraj traj = tropts->trajectory;
 
+  string prefix = propts->prefix;
+
+  double cutoff = topts->cutoff;
+  
+  uint t = tropts->skip;
+  uint k = 0;
   uint n = subset.size();
-  DoubleMatrix svals(traj->nframes(), 1);
+  DoubleMatrix svals(traj->nframes(), 2);
   DoubleMatrix vecs(n, traj->nframes());
 
   cerr << "Progress- ";
@@ -105,12 +187,14 @@ int main(int argc, char *argv[])
 
     DoubleMatrix U = boost::get<0>(result);
     DoubleMatrix S = boost::get<1>(result);
-
-    svals[t] = S[n - 2];
-    for (uint i=0; i<n; ++i)
-      vecs(i, t) = U(i, n-2);
     
-    ++t;
+    svals(k, 0) = t++;
+    svals(k, 1) = S[n - 2];
+
+    for (uint i=0; i<n; ++i)
+      vecs(i, k) = U(i, n-2);
+    
+    ++k;
   }
 
   cerr << endl;
