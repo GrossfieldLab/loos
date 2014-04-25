9c8a7dddb8447b8b30783783164ec832aac72c6f
Tod Romo <drtromo@gmail.com>
2014-04-25 16:21:42 +0000

    Added minimal help and loos copyright notice
    svn path=/software/LOOS/trunk/; revision=12219

--- Packages/DensityTools/water-autocorrel.cpp
+++ Packages/DensityTools/water-autocorrel.cpp
@@ -1,5 +1,24 @@
-// (c) 2014 Tod D. Romo, Grossfield Lab, URMC
-//
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2014, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <loos.hpp>
 
@@ -10,6 +29,11 @@ using namespace loos;
 
 int main(int argc, char *argv[]) {
   string hdr = invocationHeader(argc, argv);
+
+  if (argc == 1) {
+    cerr << "Usage- water-autocorrel water_matrix [max-t] >output.asc\n";
+    exit(-1);
+  }
 
   int k = 1;
   string matname(argv[k++]);
