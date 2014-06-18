90f2adf84dd763e7ddfefd0dd28f7a7d6dc7ec67
Tod Romo <drtromo@gmail.com>
2014-06-18 14:31:31 +0000

    Beginnings of header file for xtc writer...
    svn path=/software/LOOS/branches/newxtc/; revision=12556

---
+++ xtcwriter.hpp
@@ -0,0 +1,45 @@
+// (c) 2014 Tod D. Romo, Grossfield Lab, URMC
+
+#if !defined(LOOS_XTCWRITER_HPP)
+#define LOOS_XTC_WRITER_HPP
+
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+#include <loos_defs.hpp>
+#include <AtomicGroup.hpp>
+#include <xdr.hpp>
+
+namespace loos {
+
+  class XTCWriter {
+    static const int magicints[];
+    static const int firstidx, lastidx;
+    
+
+  public:
+
+
+  private:
+    int sizeofints(const int num_of_bits, const unsigned int sizes[]) const;
+    void encodebits(int* buf, const int num_of_bits, const int num) const;
+    void encodeints(int* buf, const int num_of_ints, const int num_of_bits,
+		    const unsigned int* sizes, const unsigned int* nums) const;
+    int writeCompressedCoordsFloat(float* ptr, int size, float precision);
+    int writeCompressedCoordsDouble(double* ptr, int size, float precision);
+    
+
+  private:
+    uint buf1size, buf2size;
+    float* buf1;
+    float* buf2;
+  };
+
+
+};
+
+
+
+#endif
