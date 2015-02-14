4dfe50efa78de1493cf7e3c7b9ba75fd4eee4adb
Tod Romo <drtromo@gmail.com>
2015-02-14 13:25:06 +0000

    Install into single dir
    svn path=/software/LOOS/trunk/; revision=14003

--- make_macos_tarball.sh
+++ make_macos_tarball.sh
@@ -9,13 +9,16 @@
 MAKE="make -j4"
 SCONS="scons -j4"
 CHECK=""
+PACKAGES="/Volumes/Doubler/Packages"
 
 PREFIX=${1:-/Volumes/Doubler}
-PACKAGES=${2:-/Volumes/Doubler/Packages}
-CLEAN=${3:-}
+CLEAN="$2"
+
+
 
 TOPDIR=`pwd`
 
+# Getting the basename is primarily in case we want to install into package-specific dirs
 BOOSTDIR=`ls -lUd $PACKAGES/boost* | head -1 | awk '{print $9}'`
 BOOST=`basename $BOOSTDIR`
 echo "Using BOOST $BOOST"
@@ -39,7 +42,7 @@ if [ -d $INSTALL_DIR ] ; then
     echo "***WARNING***"
     echo "Install location already exists"
     echo "Moving it out of the way..."
-    rm -f $INSTALL_DIR.bak
+    rm -rf $INSTALL_DIR.bak
     mv $INSTALL_DIR $INSTALL_DIR.bak
 fi
 mkdir $INSTALL_DIR
@@ -49,15 +52,17 @@ mkdir $INSTALL_DIR
 echo '***MARKER: BOOST'
 pushd $BOOSTDIR
 # Really need a distclean here...
-./bootstrap.sh --prefix=$INSTALL_DIR/$BOOST || exit -1
-./b2 install
+BOOSTOPTS=""
+if [ -n "$CLEAN" ] ; then BOOSTOPTS="-a" ; fi
+./bootstrap.sh --prefix=$INSTALL_DIR || exit -1
+./b2 $BOOSTOPTS install
 popd
 
 # Now, deal with HDF5
 echo '***MARKER: HDF5'
 pushd $HDF5DIR
 if [ -n "$CLEAN" ] ; then make distclean  ; fi
-./configure --prefix=$INSTALL_DIR/$HDF5 || exit -1
+./configure --prefix=$INSTALL_DIR || exit -1
 $MAKE $CHECK || exit -1
 $MAKE install || exit -1
 popd
@@ -66,15 +71,15 @@ popd
 echo '***MARKER: NETCDF'
 pushd $NETCDFDIR
 if [ -n "$CLEAN" ] ; then make distclean  ; fi
-CPPFLAGS="-I$INSTALL_DIR/$HDF5/include" LDFLAGS="-L$INSTALL_DIR/$HDF5/lib" ./configure --prefix=$INSTALL_DIR/$NETCDF || exit -1
+CPPFLAGS="-I$INSTALL_DIR/include" LDFLAGS="-L$INSTALL_DIR/lib" ./configure --prefix=$INSTALL_DIR || exit -1
 $MAKE $CHECK install || exit -1
 popd
 
 echo '***MARKER: LOOS'
 # Now, build LOOS
 if [ -n "$CLEAN" ] ; then scons -c ; scons -c config ; fi
-$SCONS PREFIX=$INSTALL_DIR BOOST=$INSTALL_DIR/$BOOST NETCDF=$INSTALL_DIR/$NETCDF docs || exit -1
-$SCONS PREFIX=$INSTALL_DIR BOOST=$INSTALL_DIR/$BOOST NETCDF=$INSTALL_DIR/$NETCDF install || exit -1
+$SCONS PREFIX=$INSTALL_DIR BOOST=$INSTALL_DIR NETCDF=$INSTALL_DIR docs || exit -1
+$SCONS PREFIX=$INSTALL_DIR BOOST=$INSTALL_DIR NETCDF=$INSTALL_DIR install || exit -1
 
 cd $PREFIX
 tar cvf - loos-$RELEASE_VERSION | bzip2 -cv9 >$HOME/loos-$RELEASE_VERSION-macos.tar.bz2
