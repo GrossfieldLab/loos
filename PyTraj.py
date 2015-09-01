c7aec52395f9aa7df578d683e1b4c8cd754e0015
Tod Romo <drtromo@gmail.com>
2015-09-01 17:09:19 +0000

    Unwinding changes to PyTraj for compat reasons...
    svn path=/software/LOOS/branches/PyAligned/; revision=14854

--- PyTraj.py
+++ PyTraj.py
@@ -10,36 +10,38 @@ class PyTraj:
     the one bound into the Iterator will be updated at each
     iteration.
 
+    Basic usage:
+      model = loos.createSystem(model_name)
+      calphas = selectAtoms(model, 'name == "CA"')
+
+      # Take every tenth frame...
+      itraj = PyTraj(traj, model, stride = 10)
+      for frame in itraj:
+         computeSomething(frame)
+         computeSomethingElse(calphas)
+
+      # Take every other frame, skipping the first 100
+      itraj = PyTraj(traj, model, stride = 2, skip = 100)
+      for frame in itraj:
+         computeSomething(frame)
+         computeSomethingElse(calphas)
 
     """
+    def __init__(self, traj, frame, skip = 0, stride = 1, iterator = None):
+        self.frame = frame
+        self.traj = traj
 
-    def __init__(self, fname, model, **dict):
-
-        skip = 0
-        stride = 1
-        iterator = None
-        
-        if 'skip' in dict:
-            skip = dict['skip']
-        if 'stride' in dict:
-            stride = dict['stride']
-        if 'iterator' in dict:
-            iterator = dict['iterator']
-        
-        self.frame = model
-        self.fname = fname
-        self.traj = loos.createTrajectory(fname, model)
-
-        self.framelist = []
-        if iterator is None:
-            it = range(skip, self.traj.nframes(), stride)
+        if (iterator is None):
+            it = iter(range(skip, traj.nframes(), stride))
         else:
             it = iter(iterator)
-        
+
+        self.framelist = loos.UIntVector()
         for i in it:
-            self.framelist.append(i)
+            self.framelist.push_back(i)
 
         self.index = 0
+ 
 
 
     def __iter__(self):
@@ -59,25 +61,11 @@ class PyTraj:
         self.index += 1
         return(self.frame)
 
-
-    def readFrame(self, i):
-        if (i < 0 or i >= len(self.framelist)):
-            raise IndexError
-        self.traj.readFrame(i)
-        return(self.frame)
-
-    def currentFrame(self):
-        return(self.frame)
-    
-    def currentIndexInTrajectory(self):
+    def currentIndex(self):
         return(self.framelist[self.index-1])
 
-    def currentIndexInFramelist(self):
-        return(self.index-1)
-
     def averageStructure(self):
-        flist = loos.UIntVector(self.framelist)
-        return(loos.averageStructure(self.frame, self.traj, flist))
+        return(loos.averageStructure(self.frame, self.traj, self.framelist))
 
 
 
@@ -189,67 +177,3 @@ class PyAlignedTraj(PyTraj):
 
 
 
-class VirtualTrajectory:
-
-    def __init__(self, *trajs, **dict):
-        self.skip = 0
-        self.stride = 1
-        self.nframes = 0
-        self.iterator = None
-        self.trajectories = list(trajs)
-
-        self.index = 0
-        self.framelist = []
-        self.trajlist = [] 
-        self.stale = 1
-
-        if 'skip' in dict:
-            self.skip = dict['skip']
-        if 'stride' in dict:
-            self.stride = dict['stride']
-        if 'iterator' in dict:
-            self.iterator = dict['iterator']
-        
-    def addTrajectory(self, traj):
-        self.trajectories.append(traj)
-        self.stale = 1
-
-    def countTrajectoryFrames(self):
-        n = 0
-        for t in self.trajectories:
-            n += len(t)
-        return(n)
-
-    def verifyModels(self):
-        if not self.trajectories:
-            return
-        n = 0
-        for t in self.trajectories:
-            if n == 0:
-                n = len(t.frame)
-            elif n != len(t.frame):
-                raise RuntimeError('Inconsistant models or subsets inside a virtual trajectory')
-
-    def initFrameList(self):
-        n = self.countTrajectoryFrames()
-        if (self.iterator is None):
-            it = iter(range(self.skip, n, self.stride))
-        else:
-            it = iter(iterator)
-
-        frames = []
-        trajs = []
-        for t in self.trajectories:
-            for i in range(len(t)):
-                frames.append(i)
-                trajs.append(t)
-
-        self.framelist = frames
-        self.trajlist = trajs
-            
-    def __len__(self):
-        if self.stale:
-            self.initFrameList()
-        return(len(self.framelist))
-
-                
