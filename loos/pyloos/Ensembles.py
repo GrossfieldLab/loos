27d3630f8209b07c960adf81bbdd484d6b66a67e
Tod Romo <drtromo@gmail.com>
2015-09-10 20:21:29 +0000

    Summary: Now with SVD!
    svn path=/software/LOOS/branches/PyAligned/; revision=14922

--- loos/pyloos/Ensembles.py
+++ loos/pyloos/Ensembles.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy.linalg
 
 def averageStructure(traj):
     avg = numpy.zeros((len(traj.currentFrame()), 3))
@@ -7,3 +8,24 @@ def averageStructure(traj):
         avg += coords
     avg /= len(traj)
     return(avg)
+
+
+def extractCoords(traj):
+    m = len(traj.currentFrame()) * 3
+    n = len(traj)
+
+    A = numpy.zeros((m, n))
+    for i in range(n):
+        coords = traj[i].getCoords()
+        A[:,i] = numpy.reshape(coords, (1,m))
+    return(A)
+
+
+
+def svd(traj):
+    A = extractCoords(traj)
+    avg = numpy.average(A, 1)
+    avg = avg[:,numpy.newaxis]
+    A -= avg
+    (U,s,V) = numpy.linalg.svd(A)
+    return(U,s,V,avg)
