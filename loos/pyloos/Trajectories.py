40c1da38ea2e9831b5064e3819a43111b161f7c6
Tod Romo <drtromo@gmail.com>
2015-09-14 14:10:04 +0000

    Summary: Migrating to python naming convention for instance vars
    svn path=/software/LOOS/branches/PyAligned/; revision=14936

--- loos/pyloos/Trajectories.py
+++ loos/pyloos/Trajectories.py
@@ -58,90 +58,90 @@ class Trajectory(object):
         if 'iterator' in kwargs:
             iterator = kwargs['iterator']
         if 'subset' in kwargs:
-            self.subset = loos.selectAtoms(model, kwargs['subset'])
+            self._subset = loos.selectAtoms(model, kwargs['subset'])
         else:
-            self.subset = model
+            self._subset = model
             
-        self.model = model
-        self.fname = fname
-        self.traj = loos.createTrajectory(fname, model)
+        self._model = model
+        self._fname = fname
+        self._traj = loos.createTrajectory(fname, model)
 
-        self.framelist = []
+        self._framelist = []
         if iterator is None:
-            it = range(skip, self.traj.nframes(), stride)
+            it = range(skip, self._traj.nframes(), stride)
         else:
             it = iter(iterator)
         
         for i in it:
-            self.framelist.append(i)
+            self._framelist.append(i)
 
-        self.index = 0
+        self._index = 0
 
     def fileName(self):
         """
         Return the filename that this Trajectory represents
         """
-        return(self.fname)
+        return(self._fname)
         
     def setSubset(self, selection):
         """
         Set the subset used when iterating over a trajectory.
         The selection is a LOOS selection string.
         """
-        self.subset = loos.selectAtoms(self.model, selection)
+        self._subset = loos.selectAtoms(self._model, selection)
 
         
     def __iter__(self):
-        self.index = 0
+        self._index = 0
         return(self)
 
     def __len__(self):
         """
         Number of frames in the trajectory
         """
-        return(len(self.framelist))
+        return(len(self._framelist))
 
 
     def reset(self):
         """Reset the iterator"""
-        self.index = 0
+        self._index = 0
 
     def next(self):
-        if (self.index >= len(self.framelist)):
+        if (self._index >= len(self._framelist)):
             raise StopIteration
-        frame = self.__getitem__(self.index)
+        frame = self.__getitem__(self._index)
 
-        self.index += 1
+        self._index += 1
         return(frame)
 
 
     def trajectory(self):
         """Access the wrapped loos.Trajectory"""
-        return(self.traj)
+        return(self._traj)
 
     def model(self):
         """Return the current model"""
-        return(self.model)
+        return(self._model)
     
     def readFrame(self, i):
         """Read a frame and update the model"""
-        if (i < 0 or i >= len(self.framelist)):
+        if (i < 0 or i >= len(self._framelist)):
             raise IndexError
-        self.traj.readFrame(self.framelist[i])
-        self.traj.updateGroupCoords(self.model)
-        return(self.subset)
+        self._traj.readFrame(self._framelist[i])
+        self._traj.updateGroupCoords(self._model)
+        return(self._subset)
 
     def currentFrame(self):
         """Return the current frame (subset)"""
-        return(self.subset)
+        return(self._subset)
 
     def currentRealIndex(self):
         """The 'real' frame in the trajectory for this index"""
-        return(self.framelist[self.index-1])
+        return(self._framelist[self._index-1])
 
     def currentIndex(self):
         """The state of the iterator"""
-        return(self.index-1)
+        return(self._index-1)
 
 
     def frameNumber(self, i):
@@ -157,11 +157,11 @@ class Trajectory(object):
         """
         if type(i) is int:
             if (i < 0):
-                i += len(self.framelist)
-            return(self.framelist[i])
+                i += len(self._framelist)
+            return(self._framelist[i])
         
-        indices = [x if x >=0 else len(self.framelist)+x for x in i]
-        framenos = [self.framelist[x] for x in indices]
+        indices = [x if x >=0 else len(self._framelist)+x for x in i]
+        framenos = [self._framelist[x] for x in indices]
         return(framenos)
 
 
@@ -169,9 +169,9 @@ class Trajectory(object):
         indices = list(range(*s.indices(self.__len__())))
         ensemble = []
         for i in indices:
-            self.traj.readFrame(self.framelist[i])
-            self.traj.updateGroupCoords(self.model)
-            dup = self.subset.copy()
+            self._traj.readFrame(self._framelist[i])
+            self._traj.updateGroupCoords(self._model)
+            dup = self._subset.copy()
             ensemble.append(dup)
         return(ensemble)
 
@@ -183,12 +183,12 @@ class Trajectory(object):
             return(self.getSlice(i))
 
         if (i < 0):
-            i += len(self.framelist)
-        if (i >= len(self.framelist) or i < 0):
+            i += len(self._framelist)
+        if (i >= len(self._framelist) or i < 0):
             raise IndexError
-        self.traj.readFrame(self.framelist[i])
-        self.traj.updateGroupCoords(self.model)
-        return(self.subset)
+        self._traj.readFrame(self._framelist[i])
+        self._traj.updateGroupCoords(self._model)
+        return(self._subset)
 
 
 
@@ -244,23 +244,23 @@ class VirtualTrajectory(object):
 
         """
 
-        self.skip = 0
-        self.stride = 1
-        self.nframes = 0
-        self.iterator = None
-        self.trajectories = list(trajs)
+        self._skip = 0
+        self._stride = 1
+        self._nframes = 0
+        self._iterator = None
+        self._trajectories = list(trajs)
 
-        self.index = 0
-        self.framelist = []
-        self.trajlist = [] 
-        self.stale = 1
+        self._index = 0
+        self._framelist = []
+        self._trajlist = [] 
+        self._stale = 1
 
         if 'skip' in kwargs:
-            self.skip = kwargs['skip']
+            self._skip = kwargs['skip']
         if 'stride' in kwargs:
-            self.stride = kwargs['stride']
+            self._stride = kwargs['stride']
         if 'iterator' in kwargs:
-            self.iterator = kwargs['iterator']
+            self._iterator = kwargs['iterator']
         if 'subset' in kwargs:
             self.setSubset(kwargs['subset'])
 
@@ -270,15 +270,15 @@ class VirtualTrajectory(object):
         Add a trajectory to the end of the virtual trajectory.  Resets
         the iterator state
         """
-        self.trajectories.extend(traj)
-        self.stale = 1
+        self._trajectories.extend(traj)
+        self._stale = 1
 
 
     def setSubset(self, selection):
         """
         Set the subset selection for all managed trajectories
         """
-        for t in self.trajectories:
+        for t in self._trajectories:
             t.setSubset(selection)
 
     def currentFrame(self):
@@ -286,21 +286,21 @@ class VirtualTrajectory(object):
         Return the current frame/model.  If the iterator is past the
         end of the trajectory list, return the last valid frame.
         """
-        if self.stale:
+        if self._stale:
             self.initFrameList()
 
-        if self.index >= len(self.framelist):
-            i = len(self.framelist) - 1
+        if self._index >= len(self._framelist):
+            i = len(self._framelist) - 1
         else:
-            i = self.index
+            i = self._index
             
-        return(self.trajectories[self.trajlist[i]].currentFrame())
+        return(self._trajectories[self._trajlist[i]].currentFrame())
 
     def currentIndex(self):
         """
         Return index into composite trajectory for current frame
         """
-        return(self.index-1)
+        return(self._index-1)
     
 
     def currentTrajectoryIndex(self):
@@ -308,14 +308,14 @@ class VirtualTrajectory(object):
         Returns the index into the list of trajectories that the
         current frame is from
         """
-        return(self.trajlist[self.index-1])
+        return(self._trajlist[self._index-1])
 
     def currentTrajectory(self):
         """
         Returns the loos.pyloos.Trajectory object that the current
         frame is from
         """
-        return(self.trajectories[self.trajlist[self.index-1]])
+        return(self._trajectories[self._trajlist[self._index-1]])
 
     def frameLocation(self, i):
         """
@@ -345,45 +345,45 @@ class VirtualTrajectory(object):
         and vt.frameLocation(25) will return (25, 1, t2, 0)
         
         """
-        if (self.stale):
+        if (self._stale):
             self.initFrameList()
             
         if (i < 0):
-            i += len(self.framelist)
+            i += len(self._framelist)
 
-        t = self.trajectories[self.trajlist[i]]
-        return( self.framelist[i], self.trajlist[i], t, t.frameNumber(self.framelist[i]))
+        t = self._trajectories[self._trajlist[i]]
+        return( self._framelist[i], self._trajlist[i], t, t.frameNumber(self._framelist[i]))
     
     def initFrameList(self):
         frames = []
         trajs = []
-        for j in range(len(self.trajectories)):
-            t = self.trajectories[j]
+        for j in range(len(self._trajectories)):
+            t = self._trajectories[j]
             for i in range(len(t)):
                 frames.append(i)
                 trajs.append(j)
 
-        self.framelist = []
-        self.trajlist = []
+        self._framelist = []
+        self._trajlist = []
         n = len(frames)
-        if (self.iterator is None):
-            it = iter(range(self.skip, n, self.stride))
+        if (self._iterator is None):
+            it = iter(range(self._skip, n, self._stride))
         else:
             it = iter(iterator)
         for i in it:
-            self.framelist.append(frames[i])
-            self.trajlist.append(trajs[i])
+            self._framelist.append(frames[i])
+            self._trajlist.append(trajs[i])
 
-        self.index = 0
-        self.stale = 0
+        self._index = 0
+        self._stale = 0
             
     def __len__(self):
         """
         Total number of frames
         """
-        if self.stale:
+        if self._stale:
             self.initFrameList()
-        return(len(self.framelist))
+        return(len(self._framelist))
 
                 
     def __getitem__(self, i):
@@ -392,7 +392,7 @@ class VirtualTrajectory(object):
         Python slicing.  Negative indices are relative to the end of
         the composite trajectory.
         """
-        if self.stale:
+        if self._stale:
             self.initFrameList()
 
         if isinstance(i, slice):
@@ -403,32 +403,32 @@ class VirtualTrajectory(object):
         if (i >= len(self)):
             raise IndexError
 
-        return(self.trajectories[self.trajlist[i]][self.framelist[i]])
+        return(self._trajectories[self._trajlist[i]][self._framelist[i]])
 
 
     def __iter__(self):
-        if self.stale:
+        if self._stale:
             self.initFrameList()
-        self.index = 0
+        self._index = 0
         return(self)
 
     def reset(self):
-        self.index = 0
+        self._index = 0
 
     def next(self):
-        if self.stale:
+        if self._stale:
             self.initFrameList()
-        if (self.index >= len(self.framelist)):
+        if (self._index >= len(self._framelist)):
             raise StopIteration
-        frame = self.__getitem__(self.index)
-        self.index += 1
+        frame = self.__getitem__(self._index)
+        self._index += 1
         return(frame)
 
     def getSlice(self, s):
         indices = list(range(*s.indices(self.__len__())))
         ensemble = []
         for i in indices:
-            frame = self.trajectories[self.trajlist[i]][self.framelist[i]].copy()
+            frame = self._trajectories[self._trajlist[i]][self._framelist[i]].copy()
             ensemble.append(frame)
         return(ensemble)
 
@@ -483,46 +483,46 @@ class AlignedVirtualTrajectory(VirtualTrajectory):
 
     def __init__(self, *trajs, **kwargs):
         super(AlignedVirtualTrajectory, self).__init__(*trajs, **kwargs)
-        self.aligned = False
-        self.xformlist = []
-        self.rmsd = 0
-        self.iters = 0
+        self._aligned = False
+        self._xformlist = []
+        self._rmsd = 0
+        self._iters = 0
         if 'alignwith' in kwargs:
-            self.alignwith = kwargs['alignwith']
+            self._alignwith = kwargs['alignwith']
         else:
-            self.alignwith = 'name == "CA"'
+            self._alignwith = 'name == "CA"'
 
         if 'reference' in kwargs:
-            self.reference = copy.deepcopy(kwargs['reference'])
+            self._reference = copy.deepcopy(kwargs['reference'])
         else:
-            self.reference = None
+            self._reference = None
 
 
     def append(self, *traj):
         """
         Add another trajectory at the end.  Requires re-aligning
         """
-        self.trajectories.extend(traj)
-        self.stale = True
-        self.aligned = False
+        self._trajectories.extend(traj)
+        self._stale = True
+        self._aligned = False
 
     def alignWith(self, selection):
         """
         Change the selection used to align with.  Requires re-aligning
         """
-        self.alignwith = selection
-        self.aligned = False
+        self._alignwith = selection
+        self._aligned = False
 
 
     def __iter__(self):
         self.align()
-        self.index = 0
+        self._index = 0
         return(self)
 
 
     def setReference(self, reference):
-        self.reference = copy.deepcopy(reference)
-        self.aligned = False
+        self._reference = copy.deepcopy(reference)
+        self._aligned = False
     
     def align(self):
         """
@@ -532,46 +532,46 @@ class AlignedVirtualTrajectory(VirtualTrajectory):
         current_subset = None
         ensemble = []
 
-        if self.stale:
+        if self._stale:
             self.initFrameList()
 
-        if self.reference:
-            self.xformlist = []
-            for i in range(len(self.framelist)):
-                t = self.trajectories[self.trajlist[i]]
+        if self._reference:
+            self._xformlist = []
+            for i in range(len(self._framelist)):
+                t = self._trajectories[self._trajlist[i]]
                 if t != current_traj:
                     current_traj = t
-                    current_subset = loos.selectAtoms(t.currentModel(), self.alignwith)
-                t.readFrame(self.framelist[i])
-                m = current_subset.superposition(self.reference)
+                    current_subset = loos.selectAtoms(t.model(), self._alignwith)
+                t.readFrame(self._framelist[i])
+                m = current_subset.superposition(self._reference)
                 x = loos.XForm()
                 x.load(m)
-                self.xformlist.append(x)
+                self._xformlist.append(x)
 
-            self.rmsd = 0.0
-            self.iters = 0
+            self._rmsd = 0.0
+            self._iters = 0
 
         else:
             
-            for i in range(len(self.framelist)):
-                t = self.trajectories[self.trajlist[i]]
+            for i in range(len(self._framelist)):
+                t = self._trajectories[self._trajlist[i]]
                 if t != current_traj:
                     current_traj = t
-                    current_subset = loos.selectAtoms(t.currentModel(), self.alignwith)
-                t.readFrame(self.framelist[i])
+                    current_subset = loos.selectAtoms(t.model(), self._alignwith)
+                t.readFrame(self._framelist[i])
                 ensemble.append(current_subset.copy())
 
-            (self.xformlist, self.rmsd, self.iters) = loos.iterativeAlignEnsemble(ensemble)
+            (self._xformlist, self._rmsd, self._iters) = loos.iterativeAlignEnsemble(ensemble)
 
-        self.aligned = True
+        self._aligned = True
 
         
     def getSlice(self, s):
         indices = list(range(*s.indices(self.__len__())))
         ensemble = []
         for i in indices:
-            frame = self.trajectories[self.trajlist[i]][self.framelist[i]].copy()
-            frame.applyTransform(self.xformlist[i])
+            frame = self._trajectories[self._trajlist[i]][self._framelist[i]].copy()
+            frame.applyTransform(self._xformlist[i])
             ensemble.append(frame)
         return(ensemble)
 
@@ -581,17 +581,17 @@ class AlignedVirtualTrajectory(VirtualTrajectory):
         Returns the ith frame aligned.  Supports Python slices.  Negative indices are relative
         to the end of the composite trajectory.
         """
-        if not self.aligned:
+        if not self._aligned:
             self.align()
 
         if isinstance(i, slice):
             return(self.getSlice(i))
         
         if (i < 0):
-            i += len(self.framelist)
-        if (i >= len(self.framelist)):
+            i += len(self._framelist)
+        if (i >= len(self._framelist)):
             raise IndexError
 
-        frame = self.trajectories[self.trajlist[i]][self.framelist[i]]
-        frame.applyTransform(self.xformlist[i])
+        frame = self._trajectories[self._trajlist[i]][self._framelist[i]]
+        frame.applyTransform(self._xformlist[i])
         return(frame)
