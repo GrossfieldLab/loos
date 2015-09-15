9896ffdc9567e0a2bbd1c4b758cd829fecaf237b
Tod Romo <drtromo@gmail.com>
2015-09-15 15:04:27 +0000

    Summary: Hybrid docs
    svn path=/software/LOOS/trunk/; revision=14957

--- loos/pyloos/Ensembles.py
+++ loos/pyloos/Ensembles.py
@@ -1,4 +1,7 @@
+"""
+PyLOOS functions applicable to ensembles (trajectories or trajectory-like collections)
 
+"""
 import loos
 import numpy
 import numpy.linalg
@@ -7,9 +10,10 @@ import numpy.linalg
 ## Python version of averageStructure (using loos.pyloos.Trajectory-like objects)
 # The subset defined in the trajectory controls what is averaged over.  The trajectory
 # may actually be a VirtualTrajectory or an AlignedVirtualTrajectory.
-#
+
 def averageStructure(traj):
     """Returns the average structure for a trajectory.
+    >>> avg = loos.pyloos.averageStructure(traj)
     """
     avg = numpy.zeros((len(traj.frame()), 3))
     for frame in traj:
@@ -31,6 +35,7 @@ def averageStructure(traj):
 def extractCoords(traj):
     """
     Extracts coords from a trajectory as a NumPy matrix
+    >>> A = loos.pyloos.extractCoords(traj)
     """
     m = len(traj.frame()) * 3
     n = len(traj)
@@ -43,14 +48,25 @@ def extractCoords(traj):
 
 
 
+## Returns a tuple containing the SVD result for a trajectory, along with the average structure.
+# The tuple is,
+# <tt>(left-singular-vectors, singular-values, right-singular-vectors, average)</tt>
+#
+# The subset set in the trajectory controls what is used for the SVD.  The
+# trajectory may be a VirtualTrajectory or an AlignedVirtualTrajectory.
+# The following example computes an SVD using backbone atoms for a trajectory
+# that has been iteratively aligned using C-alphas
+# \code
+#traj = loos.pyloos.Trajectory('foo.dcd', model, subset = 'backbone')
+#aligned = loos.pyloos.AlignedVirtualTrajectory(traj, alignwith = 'name == "CA"')
+#(L,S,V,avg) = loos.pyloos.svd(aligned)
+# \endcode
+
+
 def svd(traj):
     """
-    Returns a tuple containing the SVD result for a trajectory, along with
-    the average structure.  The tuple is,
-      (left-singular-vectors, singular-values, right-singular-vectors, average)
-
-    The subset set in the trajectory controls what is used for the SVD.  The
-    trajectory may be a VirtualTrajectory or an AlignedVirtualTrajectory.
+    Returns a tuple containing SVD results along with the average structure for a trajectory
+    >>> (L,S,V,avg) = loos.pyloos.svd(traj)
     """
     A = extractCoords(traj)
     avg = numpy.average(A, 1)
