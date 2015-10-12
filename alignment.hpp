29dda060ffef970a76e2156fad3cd4e0f36f38ab
Tod Romo <drtromo@gmail.com>
2015-10-12 18:39:33 +0000

    Summary: Comments and copyright
    svn path=/software/LOOS/branches/SuperRedux/; revision=15117

--- alignment.hpp
+++ alignment.hpp
@@ -1,3 +1,26 @@
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2015, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
 #if !defined(ALIGNMENT_HPP)
 #define ALIGNMENT_HPP
 
@@ -12,7 +35,8 @@
 
 
 namespace loos {
-    
+
+        // Lower-level routines for optimizing alignment performance.
         namespace alignment {
         
                 typedef std::vector<double>    vecDouble;
