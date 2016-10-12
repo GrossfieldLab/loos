7623f6157455b53ebed70ff789a1e15c1b06f683
Tod D. Romo <drtromo@gmail.com>
2016-10-12 16:47:38 -0400

    Added comments

--- src/MultiTraj.hpp
+++ src/MultiTraj.hpp
@@ -32,13 +32,28 @@
 
 namespace loos {
 
+  //! Combine multiple trajectories (pTraj's) into one large virtual trajectory
+  /**
+   * This class can be used just about anywhere a regular Trajectory/pTraj can be used.
+   * Note that the skip and stride settings are applied to each sub-trajectory (as opposed
+   * to the composite trajectory).  They are also set ONLY at instantiation.
+   *
+   */
   class MultiTrajectory : public Trajectory {
   public:
 
+
+    //! instantiate a new empty MultiTrajectory
     MultiTrajectory(const AtomicGroup& model)
       : _nframes(0), _skip(0), _stride(1), _curtraj(0), _curframe(0), _model(model)
     { }
 
+    MultiTrajectory(const AtomicGroup& model, const uint skip, const uint stride)
+      : _nframes(0), _skip(skip), _stride(stride), _curtraj(0), _curframe(0), _model(model)
+    { }
+
+
+    //! Instantiate a new MultiTrajectory using the passed filenames
     MultiTrajectory(const std::vector<std::string>& filenames,
 		     const AtomicGroup& model)
       : _nframes(0), _skip(0), _stride(1), _curtraj(0), _curframe(0), _model(model)
@@ -57,41 +72,54 @@ namespace loos {
     }
 
 
+    //! Add a trajectory (by filename)
     void addTrajectory(const std::string& filename) {
       pTraj traj = createTrajectory(filename, _model);
       _trajectories.push_back(traj);
       _nframes += (traj->nframes() - _skip) / _stride;
     }
 
+    
     virtual std::string description() const { return("virtual-trajectory"); }
 
     virtual uint natoms() const { return(_model.size()); }
-    
+
+    //! Total number of frames in composite trajectory
     virtual uint nframes() const { return(_nframes); }
 
+    //! Number of frames in the ith trajectory
     uint nframes(const uint i) const { return( (_trajectories[i]->nframes() - _skip + _stride - 1) / _stride ); }
 
+    //! Number of trajectories contained
     uint size() const { return(_trajectories.size()); }
+
+    //! Access the individual trajectories
     pTraj operator[](const uint i) const { return(_trajectories[i]); }
     
-    // Ignore timesteps (for now)
+    //! Ignore timesteps (for now)
     virtual float timestep() const { return(0.0); }
 
+    //! Whether or not the current sub-trajectory has a periodic box
     virtual bool hasPeriodicBox() const {
       return(_trajectories[_curtraj]->hasPeriodicBox());
     }
 
+    //! The periodic box of the current sub-trajectory
     virtual GCoord periodicBox() const {
       return(_trajectories[_curtraj]->periodicBox());
     }
 
+    //! Coordinates from the most recently read frame
     virtual std::vector<GCoord> coords() {
       return(_trajectories[_curtraj]->coords());
     }
 
     
 
+    //! Index into the trajectory list for the trajectory currently used
     uint currentTrajectoryIndex() const { return(_curtraj); }
+
+    //! Raw index into the current trajectory for the current frame (i.e. with skip & stride applied)
     uint currentFrameIndex() const { return(_curframe); }
     
     
