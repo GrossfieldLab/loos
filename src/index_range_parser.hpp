8bee774f40ae9e215012116bd79d8476a13e3662
Tod D. Romo <drtromo@gmail.com>
2016-11-21 16:29:33 -0500

    Added doxygen comments
    [ci skip]

--- src/index_range_parser.hpp
+++ src/index_range_parser.hpp
@@ -32,6 +32,14 @@
 
 namespace loos {
 
+  //! Use boost::Spirit to parse a new-style range list
+  /**
+   * Similar to parseRangeList(), but uses boost::spirit can requires a
+   * maximum index size.  This allows you to specify ranges without knowing
+   * the endpoint, e.g. 10:2:, means start at 10, skipping 2, until the value
+   * of maxsize.  This is useful for trajectories, otherwise you would have to
+   * know how big they are before using a range to specify a skip and stride.
+   */
   std::vector<uint> parseIndexRange(const std::string& input, const uint maxsize);
 
 }
