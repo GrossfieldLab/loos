23226246cfc285bf27844fc17bd7b0b14092ebcd
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2017-01-20 16:49:05 -0500

    converting to argparse == NOT TESTED AT ALL

--- Packages/PyLOOS/all_contacts.py
+++ Packages/PyLOOS/all_contacts.py
@@ -5,9 +5,10 @@ import loos.pyloos
 import sys
 import numpy
 from os.path import basename, splitext
+import argparse
 
-if len(sys.argv) < 5 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print "Usage: ", sys.argv[0], "system selection out_file trajectory_file [trajectory2 ...]"
+#if len(sys.argv) < 5 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+def fullhelp():
     print """
         Description: all_contacts.py: tracks contacts between pairs of side
                      chains in a protein or nucleic acid.  Returns a
@@ -21,19 +22,54 @@ if len(sys.argv) < 5 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
                      RESPECTED, since we're assuming they're both in the same
                      molecule.
           """
+class FullHelp(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        kwargs['nargs']=0
+        super(FullHelp, self).__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, values, option_string = None):
+        fullhelp()
+        parser.print_help()
+        setattr(namespace, self.dest, True)
+        parser.exit()
 
-system_file = sys.argv[1]
-selection = sys.argv[2]
-out_file = sys.argv[3]
-traj_files = sys.argv[4:]
+parser = argparse.ArgumentParser(description="Track residue-residue contacts")
+# Positional arguments
+parser.add_argument('system_file', help="System file, e.g. psf, pdb")
+parser.add_argument('selection_string', help="Selection string for analysis")
+parser.add_argument('out_file', help="Matrix of total contacts")
+parser.add_argument('--skip_hydrogen',
+                    help="Don't use hydrogens for contact calculation",
+                    action='store_true')
+parser.add_argument('--skip_backbone',
+                    help="Ignore backbone atoms",
+                    action='store_true')
+parser.add_argument('--fullhelp',
+                    help="Print detailed description of all options",
+                    action=FullHelp)
+parser.add_argument('--cutoff',
+                    help="Cutoff distance",
+                    default=4.0,
+                    type=float)
+parser.add_argument('--threshold',
+                    help="Number of atom-atom contacts to say residues touch",
+                    default=1,
+                    type=int)
 
+
+# This must be the last argument on the command line
+parser.add_argument('--traj_files', help="One of more trajectory files",
+                    nargs= argparse.REMAINDER,
+                    required=True)
+
+args = parser.parse_args()
 header = " ".join(sys.argv) + "\n"
+print "#", header
 
 
-system = loos.createSystem(system_file)
+system = loos.createSystem(args.system_file)
 all_trajs = []
 out_names = []
-num_trajs = len(traj_files)
+num_trajs = len(args.traj_files)
 for t in traj_files:
     traj = loos.pyloos.Trajectory(t, system)
     all_trajs.append(traj)
@@ -41,17 +77,17 @@ for t in traj_files:
     core, ext = splitext(t_base)
     out_names.append(core + ".dat")
 
-# Used to remove hydrogens, but that messes up glycines
-#no_hydrogens = loos.selectAtoms(system, "!hydrogen")
-#target = loos.selectAtoms(no_hydrogens, selection)
+if args.skip_hydrogen:
+    no_hydrogens = loos.selectAtoms(system, "!hydrogen")
+    target = loos.selectAtoms(no_hydrogens, selection)
 
 target = loos.selectAtoms(system, selection)
 
 
 residues = target.splitByResidue()
-# now remove the backbone -- doing before the split loses the glycines
-# Woohoo, look at me, I used a lambda!
-residues = list(map(lambda r: loos.selectAtoms(r, "!backbone"), residues))
+
+if args.skip_backbone:
+    residues = list(map(lambda r: loos.selectAtoms(r, "!backbone"), residues))
 
 frac_contacts = numpy.zeros([len(residues), len(residues), num_trajs],
                             numpy.float)
@@ -62,11 +98,11 @@ for traj_id in range(num_trajs):
     for frame in traj:
         for i in range(len(residues)):
             for j in range(i+1, len(residues)):
-                if residues[i].contactWith(4.0, residues[j]):
+                if residues[i].contactWith(args.cutoff, residues[j], args.threshold):
                     frac_contacts[i, j, traj_id] += 1.0
                     frac_contacts[j, i, traj_id] += 1.0
     frac_contacts[:, :, traj_id] /= len(traj)
-    numpy.savetxt(out_names[traj_id], frac_contacts[:,:,traj_id],
+    numpy.savetxt(out_names[traj_id], frac_contacts[:, :, traj_id],
                   header=header)
 
 average = numpy.add.reduce(frac_contacts, axis=2)
