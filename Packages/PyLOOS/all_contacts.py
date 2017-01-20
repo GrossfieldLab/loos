789c1823771e86a9a4c4e605ed5be26dbf545205
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2017-01-20 14:49:30 -0500

    formatting changes requested by flakes

--- Packages/PyLOOS/all_contacts.py
+++ Packages/PyLOOS/all_contacts.py
@@ -10,15 +10,16 @@ if len(sys.argv) < 5 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
     print "Usage: ", sys.argv[0], "system selection out_file trajectory_file [trajectory2 ...]"
     print """
         Description: all_contacts.py: tracks contacts between pairs of side
-                     chains in a protein or nucleic acid.  Returns a matlab-format
-                     matrix containing the fraction of frames containing each
-                     residue-residue contact, as well as a matrix for each
-                     trajectory individually.
+                     chains in a protein or nucleic acid.  Returns a
+                     matlab-format matrix containing the fraction of frames
+                     containing each residue-residue contact, as well as a
+                     matrix for each trajectory individually.
 
                      The threshold for deciding if 2 residues are in contact is
                      whether any atoms from one residue are within 4.0 ang of
-                     any atom from the other.  NOTE: PERIODICITY IS NOT RESPECTED,
-                     since we're assuming they're both in the same molecule.
+                     any atom from the other.  NOTE: PERIODICITY IS NOT
+                     RESPECTED, since we're assuming they're both in the same
+                     molecule.
           """
 
 system_file = sys.argv[1]
@@ -26,7 +27,7 @@ selection = sys.argv[2]
 out_file = sys.argv[3]
 traj_files = sys.argv[4:]
 
-header =  " ".join(sys.argv) + "\n"
+header = " ".join(sys.argv) + "\n"
 
 
 system = loos.createSystem(system_file)
@@ -37,7 +38,7 @@ for t in traj_files:
     traj = loos.pyloos.Trajectory(t, system)
     all_trajs.append(traj)
     t_base = basename(t)
-    core,ext = splitext(t_base)
+    core, ext = splitext(t_base)
     out_names.append(core + ".dat")
 
 # Used to remove hydrogens, but that messes up glycines
@@ -50,7 +51,7 @@ target = loos.selectAtoms(system, selection)
 residues = target.splitByResidue()
 # now remove the backbone -- doing before the split loses the glycines
 # Woohoo, look at me, I used a lambda!
-residues = list(map(lambda r:loos.selectAtoms(r, "!backbone"), residues))
+residues = list(map(lambda r: loos.selectAtoms(r, "!backbone"), residues))
 
 frac_contacts = numpy.zeros([len(residues), len(residues), num_trajs],
                             numpy.float)
@@ -62,16 +63,14 @@ for traj_id in range(num_trajs):
         for i in range(len(residues)):
             for j in range(i+1, len(residues)):
                 if residues[i].contactWith(4.0, residues[j]):
-                    frac_contacts[i,j,traj_id] += 1.0
-                    frac_contacts[j,i,traj_id] += 1.0
-    frac_contacts[:,:,traj_id] /= len(traj)
+                    frac_contacts[i, j, traj_id] += 1.0
+                    frac_contacts[j, i, traj_id] += 1.0
+    frac_contacts[:, :, traj_id] /= len(traj)
     numpy.savetxt(out_names[traj_id], frac_contacts[:,:,traj_id],
                   header=header)
-
-
 
 average = numpy.add.reduce(frac_contacts, axis=2)
 average /= len(traj_files)
 
 
-numpy.savetxt(out_file, average, header = header)
+numpy.savetxt(out_file, average, header=header)
