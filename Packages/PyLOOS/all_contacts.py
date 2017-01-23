2594b2f13e90b3103c30267c71a50b47cceb9a52
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2017-01-23 10:43:25 -0500

    Added LOOS header

--- Packages/PyLOOS/all_contacts.py
+++ Packages/PyLOOS/all_contacts.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
+"""
+    all_contacts.py : compute average residue-residue contact probabilities
+"""
+"""
 
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2012 Tod Romo, Grossfield Lab
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import loos
 import loos.pyloos
 import sys
