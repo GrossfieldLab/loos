4e42d4421241215b9d4a3a4e6f09c8ed2f338784
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2017-09-20 16:14:38 -0400

    preliminary commit, untested -- took working code from the stim1 project and converted to use argparse

--- Packages/PyLOOS/all_contacts.py
+++ Packages/PyLOOS/all_contacts.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 """
-    all_contacts.py : compute average residue-residue contact probabilities
+Track a set of contacts through a trajectory.  Intended for use with a protein
+or RNA, to track all residue-residue contacts within the trajectory.
 """
+
 """
 
   This file is part of LOOS.
 
   LOOS (Lightweight Object-Oriented Structure library)
-  Copyright (c) 2017, Alan Grossfield Grossfield Lab
+  Copyright (c) 2013 Tod Romo, Grossfield Lab
   Department of Biochemistry and Biophysics
   School of Medicine & Dentistry, University of Rochester
 
@@ -22,7 +24,10 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
+
+
 import loos
 import loos.pyloos
 import sys
@@ -31,36 +36,9 @@ from os.path import basename, splitext
 import argparse
 
 def fullhelp():
-    print """
-        Description: all_contacts.py: tracks contacts between pairs of side
-                     chains in a protein or nucleic acid.  Returns a
-                     matlab-format matrix containing the fraction of frames
-                     containing each residue-residue contact, as well as a
-                     matrix for each trajectory individually.
-
-        Optional flags
-        --skip_hydrogen:  don't use hydrogens when computing residue-residue
-                          contacts
-        --skip_backbone:  exclude backbone atoms from contact calculation.
-                          If you don't use this flag, consecutive residues
-                          will always be in contact.
-        --cutoff:         set the distance at which two atoms are considered
-                          to be in contact.  Default = 4 ang
-        --threshold       set the number of atom-atom pairs that must be in
-                          contact for the two residues to be considered to
-                          be in contact.  Default = 1 pair
-
-        File-naming
-            In addition to the average over all frames in all trajectories,
-            the program will write a separate file for each trajectory.  The
-            name of the output file is based on the name of the trajectory,
-            built by removing the directory portion and replacing the
-            extension (if any) will ".dat".  Note that it doesn't check for
-            duplicate filenames, so if you git it a bunch of trajectories with
-            the same name but in different directories, it'll overwrite the
-            output files.  This seemed like a rare enough case to not worry
-            about it -- if it's a problem, please contact loos-maintainer@gmail.
-          """
+  print """
+  Insert fullhelp here
+  """
 
 class FullHelp(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
@@ -72,38 +50,27 @@ class FullHelp(argparse.Action):
         setattr(namespace, self.dest, True)
         parser.exit()
 
+##############################################################################
+
+cmd_args = " ".join(sys.argv)
 parser = argparse.ArgumentParser(description="Track residue-residue contacts")
-# Positional arguments
-parser.add_argument('system_file', help="System file, e.g. psf, pdb")
-parser.add_argument('selection_string', help="Selection string for analysis")
-parser.add_argument('out_file', help="Matrix of total contacts")
-parser.add_argument('--skip_hydrogen',
-                    help="Don't use hydrogens for contact calculation",
-                    action='store_true')
-parser.add_argument('--skip_backbone',
-                    help="Ignore backbone atoms",
-                    action='store_true')
-parser.add_argument('--fullhelp',
-                    help="Print detailed description of all options",
+parser.add_argument('system_file', help="File describing the system")
+parser.add_argument('selection', help="Selection string describing which residues to use")
+parser.add_argument('out_file', help="File with the average contact occupancies")
+parser.add_argument('traj_files', type=argparse.FileType('r'), nargs='+')
+
+
+parser.add_argument('--cutoff', help="Cutoff distance for contact", default=4.0)
+# TODO: add a number of contacts option
+parser.add_argument('--no_hydrogens', nargs=0, help="Don't include hydrogens")
+parser.add_argument('--no_backbone', nargs=0, help="Don't include the backbone")
+parser.add_argument('--fullhelp', help="Print detailed description of all options",
                     action=FullHelp)
-parser.add_argument('--cutoff',
-                    help="Cutoff distance",
-                    default=4.0,
-                    type=float)
-parser.add_argument('--threshold',
-                    help="Number of atom-atom contacts to say residues touch",
-                    default=1,
-                    type=int)
-
-
-# This must be the last argument on the command line
-parser.add_argument('--traj_files', help="One of more trajectory files",
-                    nargs= argparse.REMAINDER,
-                    required=True)
-
 args = parser.parse_args()
-header = " ".join(sys.argv) + "\n"
-print "#", header
+
+
+header =  " ".join(sys.argv) + "\n"
+
 
 system = loos.createSystem(args.system_file)
 all_trajs = []
@@ -112,27 +79,25 @@ num_trajs = len(args.traj_files)
 for t in args.traj_files:
     traj = loos.pyloos.Trajectory(t, system)
     all_trajs.append(traj)
-    t_base = basename(t)
-    core, ext = splitext(t_base)
-    out_names.append(core + ".dat")
+    if num_trajs > 1:
+        t_base = basename(t)
+        core,ext = splitext(t_base)
+        out_names.append(core + ".dat")
 
-# Apply the selection
-target = loos.selectAtoms(system, args.selection_string)
+if args.no_hydrogens:
+    no_hydrogens = loos.selectAtoms(system, "!hydrogen")
+    target = loos.selectAtoms(no_hydrogens, args.selection)
+else:
+    target = loos.selectAtoms(system, selection)
 
-# Additionally remove all hydrogens if requested
-if args.skip_hydrogen:
-    target = loos.selectAtoms(target, "!hydrogen")
 
-# Split by residue, then optionally remove backbone
-#   Performing this in the reverse order will exclude
-#   glycine from the search if skip_backbone and
-#   skip_hydrogen were both chosen.  Doing it this way
-#   will include a space for the glycine, although there
-#   will be no atoms.
+
+
 residues = target.splitByResidue()
-
-if args.skip_backbone:
-    residues = list(map(lambda r: loos.selectAtoms(r, "!backbone"), residues))
+# now remove the backbone -- doing before the split loses the glycines
+# Woohoo, look at me, I used a lambda!
+if args.no_backbone:
+    residues = list(map(lambda r:loos.selectAtoms(r, "!backbone"), residues))
 
 frac_contacts = numpy.zeros([len(residues), len(residues), num_trajs],
                             numpy.float)
@@ -143,15 +108,18 @@ for traj_id in range(num_trajs):
     for frame in traj:
         for i in range(len(residues)):
             for j in range(i+1, len(residues)):
-                if residues[i].contactWith(args.cutoff, residues[j], args.threshold):
-                    frac_contacts[i, j, traj_id] += 1.0
-                    frac_contacts[j, i, traj_id] += 1.0
-    frac_contacts[:, :, traj_id] /= len(traj)
-    numpy.savetxt(out_names[traj_id], frac_contacts[:, :, traj_id],
-                  header=header)
+                if residues[i].contactWith(args.cutoff, residues[j]):
+                    frac_contacts[i,j,traj_id] += 1.0
+                    frac_contacts[j,i,traj_id] += 1.0
+    frac_contacts[:,:,traj_id] /= len(traj)
+    if num_trajs > 1:
+        numpy.savetxt(out_names[traj_id], frac_contacts[:,:,traj_id],
+                      header=header)
+
+
 
 average = numpy.add.reduce(frac_contacts, axis=2)
 average /= len(args.traj_files)
 
 
-numpy.savetxt(args.out_file, average, header=header)
+numpy.savetxt(args.out_file, average, header = header)
