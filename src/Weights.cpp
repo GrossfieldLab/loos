0a43cb851919fb1f6a210c89367f4eb9e0836122
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2019-01-05 17:07:31 -0500

    initial commit, doesn't do anything yet

---
+++ src/Weights.cpp
@@ -0,0 +1,57 @@
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2008, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Weights.hpp>
+
+namespace loos {
+    //! Weights class to handle reweighting values computed from a trajectory
+    uint Weights::read_weights(const std::string& filename)
+    {
+        std::ifstream ifs(filename.c_str());
+        if (!ifs) {
+            throw(FileOpenError(filename));
+        }
+
+        std::string input;
+        while (getline(ifs, input)) {
+            // skip blank lines and comments beginning with "#"
+            if ( (input.length() == 0) || (input[0] == '#' ) ) {
+                // do nothing
+            }
+            else {
+                double value = parseStringAs<double>(input);
+                _weights.push_back(value);
+            }
+        }
+        return _weights.size();
+    }
+
+    void Weights::normalize() {
+        double sum = 0.0;
+        for (uint i=0; i<_weights.size(); ++i) {
+            sum += _weights[i];
+        }
+        // TODO : Really should check for underflow to prevent div by 0
+        for (uint i=0; i<_weights.size(); ++i) {
+            _weights[i] /= sum;
+        }
+    }
+}
---
+++ src/Weights.hpp
@@ -0,0 +1,69 @@
+/*
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2008, Tod D. Romo, Alan Grossfield
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if !defined(LOOS_WEIGHTS_HPP)
+#define LOOS_WEIGHTS_HPP
+
+#include <loos_defs.hpp>
+#include <Trajectory.hpp>
+#include <exceptions.hpp>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+namespace loos {
+
+    class Weights {
+    public:
+        Weights(const std::string &filename, const Trajectory *traj):
+                                        _traj(traj),
+                                        current_frame(0)
+                                       {
+            uint num_weights = read_weights(filename);
+            // # of weights must match number of frames in the associated traj
+            if (num_weights != _traj->nframes()) {
+                throw(LOOSError(std::string("Number of weights must match the length of the trajectory")));
+
+            normalize();
+            }
+
+        };
+
+        ~Weights() { };
+
+        double get();
+        double get(uint index);
+        uint current_frame;
+
+        void normalize();
+
+
+
+
+    private:
+        uint read_weights(const std::string &filename);
+        const Trajectory *_traj;
+        std::vector<double> _weights;
+    };
+
+}
+
+#endif
