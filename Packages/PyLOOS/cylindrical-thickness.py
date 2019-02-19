c1801e0a2d508dd56bc249bed70880aa32c1676d
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2019-02-19 18:22:11 -0500

    minor linter change

--- Packages/PyLOOS/cylindrical-thickness.py
+++ Packages/PyLOOS/cylindrical-thickness.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 """
    cylindrical-thickness.py : compute the thickness of the membrane around
-                            a centering selection, output the average as 
+                            a centering selection, output the average as
                             a function of lateral distance from origin
    The expected use-case for this is looking at lipid packing around a membrane
-   protein, when for whatever region you want to average out the "polar" 
-   degrees of freedom.  
+   protein, when for whatever region you want to average out the "polar"
+   degrees of freedom.
 
    For example, a command line could look like:
 
-   cylindrical-thickness.py sim.psf sim.dcd 'segid == "PROT"' 'segid =~ "PE" && name == "P"' 10 30 20 
+   cylindrical-thickness.py sim.psf sim.dcd 'segid == "PROT"' 'segid =~ "PE" && name == "P"' 10 30 20
 
    This would read the system info from sim.psf, and use the trajectory sim.dcd.
    The system would be translated such that "PROT" is at the origin, and then
-   the heights of phosphate with segment names containing "PE" would be 
-   computed. 
+   the heights of phosphate with segment names containing "PE" would be
+   computed.
 
-   NOTE: This code assumes the membrane is centered at z = 0. Atoms with 
-   z>0 are assigned to the upper leaflet, z<0 the lower leaflet, and the 
+   NOTE: This code assumes the membrane is centered at z = 0. Atoms with
+   z>0 are assigned to the upper leaflet, z<0 the lower leaflet, and the
    difference between the averages is computed as a function of r.
 
-   
+
    Alan Grossfield
 """
 """
@@ -94,8 +94,8 @@ for frame in traj:
 
         if (rmin2 < r2 < rmax2):
             r = math.sqrt(r2)
-    
-            rbin = int((r - rmin)/ rbin_width)
+
+            rbin = int((r - rmin)/rbin_width)
             if z > 0:
                 upper_sum[rbin] += z
                 upper_count[rbin] += 1
@@ -108,8 +108,8 @@ print("#r   Thick   UpperHeight LowerHeight")
 for i in range(rnum_bins):
     r = rmin + (i+0.5)*rbin_width
     upper = lower = 0.0
-    if upper_count[rbin] > 0:
+    if upper_count[i] > 0:
         upper = upper_sum[i] / upper_count[i]
-    if lower_count[rbin] > 0:
+    if lower_count[i] > 0:
         lower = lower_sum[i] / lower_count[i]
     print(r, upper-lower, upper, lower)
