1c0b0ee740911592be31bd24d8b0b5ceefd6a3e8
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2019-05-05 10:06:59 -0400

    updated comments

--- Packages/OptimalMembraneGenerator/make_library.py
+++ Packages/OptimalMembraneGenerator/make_library.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+"""
+Build a lipid library that can be used for an OMG run
+
+Alan Grossfield,  University of Rochester Medical Center, 2019
+"""
+
+"""
+
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2019 Tod Romo, Grossfield Lab
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 
 import sys
 import os
