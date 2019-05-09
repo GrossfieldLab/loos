beee00c55f2d2316ca1f635da7cd4ca26c204cd0
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2019-05-09 15:54:50 -0400

    fullhelp, formatting output

--- Packages/PyLOOS/packing_score_per_res.py
+++ Packages/PyLOOS/packing_score_per_res.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+"""
+Compute packing score between each residue of a protein and a number of
+small molecules in solution.
+
+Alan Grossfield, University of Rochester Medical Center
+"""
+
+"""
+
+  This file is part of LOOS.
+
+  LOOS (Lightweight Object-Oriented Structure library)
+  Copyright (c) 2019 Tod Romo, Grossfield Lab
+  Department of Biochemistry and Biophysics
+  School of Medicine & Dentistry, University of Rochester
+
+  This package (LOOS) is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation under version 3 of the License.
+
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 
 import sys
 import loos
@@ -6,6 +36,68 @@ import loos.pyloos
 import numpy
 import argparse
 from sklearn import decomposition
+
+
+def fullhelp():
+    print("""
+    Compute packing score between each residue of a protein and a number of
+    small molecules in solution.
+
+    Summary of arguments
+    system_file: file describing the full system contents. For this program
+                 to work correctly, it must contain connectivity information, so either a PSF or a PDB with CONECT records is best
+    protein_selection: selection string for the protein
+    probe_selection: selection string for the small molecules (will be
+                     split into individual molecules)
+    output_core: root name of the output files
+    --traj: one or more trajectory files
+
+    Options
+    --skip value : number of frames to skip at the front of each trajectory
+    --stride value: how to step through the trajectory (default=1)
+    --no_h: remove hydrogens from the selection (recommended)
+    --pca: perform principal component analysis on the time series
+
+    Packing score is a measure of packing between two groups or moieties
+    inspired by the Lennard-Jones function.  It is the sum over all
+    inter-moiety pairs of atoms of 1/r^6.  To my knowledge, this term
+    was first introduced in Grossfield et al, Proc. Nat. Acad. Sci.
+    USA, 2006, 103, 4888-4893.
+
+    Output:
+    If output_core is set to "foo", then the following files will be
+    produced:
+
+    foo_raw_score.dat: matrix of packing scores.  Each row is the packing
+                       score between 1 specific probe molecule against each
+                       protein residue from 1 frame.
+    foo_ave.dat: column of per-residue summed packing scores
+
+    If --pca is specified, principal component analysis is performed on the
+    matrix from foo_raw_score, producing 3 additional files:
+    
+      foo_var.dat: the fraction of the signal for each eigenmode
+                   (essentially the eigenvalues)
+      foo_var_cum.dat: the cumulative variance, so you can assess how many
+                       eigenmodes you should analyze)
+      foo_comp.dat: each column is an eigenvector,
+                    representing an intensity for each residue.  This is
+                    useful for identifying residues that simultaneously
+                    interact with an individual probe molecule, and thus are
+                    candidate binding sites.
+
+    """)
+
+class FullHelp(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        kwargs['nargs']=0
+        super(FullHelp, self).__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, values, option_string = None):
+        fullhelp()
+        parser.print_help()
+        setattr(namespace, self.dest, True)
+        parser.exit()
+
 
 
 header = "#" + " ".join(sys.argv)
@@ -39,6 +131,8 @@ parser.add_argument('--no_h',
 parser.add_argument('--pca',
                     help="Perform PCA on the per-residue profiles",
                     action='store_true')
+parser.add_argument('--fullhelp', help="Print detailed description of all options", action=FullHelp)
+
 args = parser.parse_args()
 
 
@@ -79,17 +173,25 @@ for frame in vtraj:
 print("finished calculating packing scores")
 
 scores = scores.reshape(len(residues), len(probes)*len(vtraj)).transpose()
-numpy.savetxt(args.output_core + "_raw_score.dat", scores, header=header)
+numpy.savetxt(args.output_core + "_raw_score.dat",
+              scores,
+              header=header,
+              fmt='%.6e')
 
 ave = numpy.add.reduce(scores, axis=0)
-numpy.savetxt(args.output_core + "_ave.dat", ave)
+numpy.savetxt(args.output_core + "_ave.dat",
+              ave,
+              fmt='%.6e')
 
 if args.pca:
     pca = decomposition.PCA()
     pca.fit(scores)
     numpy.savetxt(args.output_core + "_var.dat",
-                  pca.explained_variance_ratio_)
+                  pca.explained_variance_ratio_,
+                  fmt='%.6e')
     numpy.savetxt(args.output_core + "_var_cum.dat",
-                  numpy.add.accumulate(pca.explained_variance_ratio_))
+                  numpy.add.accumulate(pca.explained_variance_ratio_),
+                  fmt='%.6e')
     numpy.savetxt(args.output_core + "_comp.dat",
-                  numpy.transpose(pca.components_))
+                  numpy.transpose(pca.components_),
+                  fmt='%.6e')
