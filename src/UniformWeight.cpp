9a871de82c887c3126126faf8f251d10e409f73e
louis <louis_smith@urmc.rochester.edu>
2019-12-03 18:41:57 -0500

    Fixed some issues with delimiters and definitions. Introduced _frameWeight ILO the pervasive 1.0s.

--- src/UniformWeight.cpp
+++ src/UniformWeight.cpp
@@ -1,38 +1,57 @@
 #include "UniformWeight.hpp"
 
-namespace loos {
-    // return a vector with the number of weights in it
-    std::vector<double> UniformWeight::weights() {
-        std::vector<double> uniform(1.0, _num_weights);
-        _weights = uniform;
-        return _weights;
-    }
+namespace loos
+{
 
-    // return the weighting of the current frame
-    const double UniformWeight::get() {
-        current_frame = _traj->currentFrame();
-        return 1.0;
-    }
+// Accumulate the 'weight' used so far
 
-    // return the weight of the current index
-    const double UniformWeight::get() {
-        return 1.0;
+// Normalize weights using length of traj
+void UniformWeight::normalize()
+{
+    _frameWeight /= _traj->nframes();
+    if (!_weights.empty())
+    {
+        for (uint i = 0; i < _weights.size(); i++)
+            _weights[i] = _frameWeight;
     }
-
-    // calling nomenclature wraps get
-    const double Weights::operator()(){
-        return get();
-    }
-
-    // calling nomenclature wraps get
-    const double Weights::operator(const uint index)(){
-        return get(index);
-    }
-
-    // give number of frames, which is equivalent to _num_weights, as size
-    uint UniformWeights::size() {
-        return _num_weights;
-    }
-
-    // Return the total weight used so far (from accumulate) 
 }
+
+// return a vector with the number of weights in it
+std::vector<double> UniformWeight::weights()
+{
+    std::vector<double> uniform(_frameWeight, _traj->nframes());
+    _weights = uniform;
+    return _weights;
+}
+
+// return the weighting of the current frame
+const double UniformWeight::get()
+{
+    current_frame = _traj->currentFrame();
+    return _frameWeight;
+}
+
+// return the weight of the current index
+const double UniformWeight::get(const uint index)
+{
+    return _frameWeight;
+}
+
+// calling nomenclature wraps get
+const double UniformWeight::operator()()
+{
+    return get();
+}
+
+// calling nomenclature wraps get
+const double UniformWeight::operator()(const uint index)
+{
+    return get(index);
+}
+
+// give number of frames, which is equivalent to _num_weights, as size
+uint UniformWeight::size()
+{
+    return _traj->nframes();
+}
+} // namespace loos
--- src/UniformWeight.hpp
+++ src/UniformWeight.hpp
@@ -1,44 +1,59 @@
 #if !defined(LOOS_UNIFORM_WEIGHTS_HPP)
 #define LOOS_UNIFORM_WEIGHTS_HPP
 #include "Weights.hpp"
+#include <loos_defs.hpp>
+#include <Trajectory.hpp>
 
-namespace loos {
-    class UniformWeight : public Weights {
-    public:
-        uint current_frame;
+namespace loos
+{
+class UniformWeight : public Weights
+{
+public:
+    uint current_frame;
 
-        const double get();
-        const double get(const uint index);
-        uint size();
+    const double get();
+    const double get(const uint index);
+    uint size();
 
-        void normalize();
-        void accumulate();
-        void accumulate(const uint index);
-        const double totalWeight();
-        const double trajWeight();
-        void add_traj(ptraj& traj);
-        std::vector<double> weights();
-    };
-    private:
-        double _total;
-        std::string _filename;
-        std::vector<double> _weights;
-        pTraj _traj;
-        bool _has_list;
-    
-    public:
+    void normalize();
+    void accumulate();
+    void accumulate(const uint index);
+    const double totalWeight();
+    const double trajWeight();
+    void add_traj(pTraj &traj);
+    const double operator()();
+    const double operator()(const uint index);
+    // this will be built upon request.
+    // will be a trajlength vector of 1.0s,
+    // so don't ask if you don't need.
+    std::vector<double> weights();
+
+private:
+    double _frameWeight;
+    double _total;
+    double _totalTraj;
+    std::string _filename;
+    std::vector<double> _weights;
+    pTraj _traj;
+    bool _has_list;
+
+public:
     UniformWeight() : current_frame(0),
+                      _frameWeight(1.0),
                       _total(0.0),
                       _has_list(false),
-                      _filename("") {};
+                      _filename(""){};
 
-    UniformWeight(&pTraj traj) : current_frame(0),
+    UniformWeight(pTraj &traj) : current_frame(0),
+                                 _frameWeight(1.0),
                                  _total(0.0),
-                                 _filename(filename),
+                                 _filename(""),
                                  _has_list(false)
-                                 {
-                                    add_traj(traj);
+    {
+        add_traj(traj);
     };
 
-    ~UniformWeight() { };
-}
+    ~UniformWeight(){};
+};
+} // namespace loos
+#endif
