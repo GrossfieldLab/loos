f5775db691ced26be3e869b859b0222c37be5053
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2019-12-19 08:52:49 -0500

    bumped version to 3.1

--- loos_build_config.py
+++ loos_build_config.py
@@ -19,7 +19,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-loos_version = '3.0'
+loos_version = '3.1'
 min_boost_version = '1_36'
 required_boost_libraries = ('regex', 'system', 'program_options', 'thread', 'filesystem')
 
