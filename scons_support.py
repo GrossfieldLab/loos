2eb9addb1e3ffccee6e320fe71c534b5e577fe95
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2020-03-10 16:53:02 -0400

    hack for Louis building eigen

--- scons_support.py
+++ scons_support.py
@@ -491,6 +491,12 @@ def SetupNetCDFPaths(env):
     if netcdf_include:
         env.Prepend(CPPPATH=[netcdf_include])
 
+def SetupEigen(env):
+    if env.USING_CONDA:
+        eigen_include_path = env['CONDA_PREFIX'] + "/include/eigen"
+    else:
+        eigen_include_path = "/usr/include/eigen"
+    env.Prepend(CPPPATH=[eigen_include_path])
 
 def AutoConfigSystemBoost(conf):
     boost_libs = []
@@ -772,6 +778,8 @@ def AutoConfiguration(env):
             boost_libs = AutoConfigUserBoost(conf)
 
         env.Append(LIBS=boost_libs)
+
+        SetupEigen(env)
 
         # --- Check for ATLAS/LAPACK and how to build
 
