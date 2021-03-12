611e4fe3b3b51da89038f8f27c3acdf21a21f5b9
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2021-03-12 16:48:43 -0500

    program to test the stacking (also does packing score as a control)

---
+++ Packages/PyLOOS/try_stacking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import loos
+import loos.pyloos
+import sys
+
+print("#", " ".join(sys.argv))
+
+system = loos.createSystem(sys.argv[1])
+traj = loos.pyloos.Trajectory(sys.argv[2], system)
+
+res1 = loos.selectAtoms(system, sys.argv[3])
+res2 = loos.selectAtoms(system, sys.argv[4])
+
+for frame in traj:
+    box = system.periodicBox()
+    stacking = res1.stacking(res2, box)
+    packing = res1.packingScore(res2, box, norm=True)
+    print(traj.index(), stacking, packing)
