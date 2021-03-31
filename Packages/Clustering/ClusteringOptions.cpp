cb181cab7e3d5632c5ce52ef9277f778e1f7115d
lgsmith <louis.smith88@gmail.com>
2021-03-31 10:41:50 -0600

    smoothed sharper edges of cluster-kgs (#41)
    * Added squareness check in clusteringutils for matrix reader function. Added verbiage to fullhelp cautioning against using matricies that are not symmetric but are square.
    
    * Fixed capitalization error in fullhelp.

--- Packages/Clustering/ClusteringOptions.cpp
+++ Packages/Clustering/ClusteringOptions.cpp
@@ -12,16 +12,29 @@ namespace Clustering{
 
   bool ClusteringOptions::postConditions(po::variables_map& vm) {
     if (stream_mode && !similarity_filename.empty()){
-      std::cerr << "Usage Error: \nYou've asked that the tool both reads a file: \n  \"" << similarity_filename << "\"\nand that it read from stdin. \n";
+      std::cerr << "Usage Error:\nYou've asked that the tool both reads a file: \n  \"" << similarity_filename << "\"\nand that it read from stdin. \n";
       return false;
     }
       
-  	if (stream_mode)
-  		similarityScores = readMatrixFromStream<dtype>(std::cin);
+  	if (stream_mode) {
+      try {
+        similarityScores = readMatrixFromStream<dtype>(std::cin);
+      }
+      catch(const loos::LOOSError &e) {
+        std::cerr << e.what() << '\n';
+        return false;
+      }
+    }
     else if (!similarity_filename.empty()){
       std::ifstream stream;
       stream.open(similarity_filename);
-      similarityScores = readMatrixFromStream<dtype>(stream);
+      try{
+        similarityScores = readMatrixFromStream<dtype>(stream);
+      }
+      catch(const loos::LOOSError &e) {
+        std::cerr << e.what() << '\n';
+        return false;
+      }
     }
     else // false causes briefhelp print and exit
       return false;
