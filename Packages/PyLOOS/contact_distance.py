e38770a56916f8b595cfb55829e72db066a3cf0d
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2021-04-23 13:10:53 -0400

    changed --sel to --selection

--- Packages/PyLOOS/contact_distance.py
+++ Packages/PyLOOS/contact_distance.py
@@ -31,7 +31,7 @@ import loos
 import loos.pyloos
 import loos.pyloos.options as options
 import numpy
-import math
+from scipy.spatial.distance import pdist
 
 fullhelp = """
   Insert something useful here
@@ -51,12 +51,15 @@ lo.parser.add_argument('--radius',
                        default=5.0,
                        type=float,
                        help="midpoint distance for the logistic contact")
+# fix this so you don't need to give a value
 lo.parser.add_argument('--skip_backbone',
-                      default=False,
-                      help="Consider only sidechains")
+                       default=False,
+                       help="Consider only sidechains")
 lo.parser.add_argument('--include_h',
-                      default=False,
-                      help="Include hydrogens")
+                       default=False,
+                       help="Include hydrogens")
+lo.parser.add_argument('--outfile',
+                       help="Name of outputted file ")
 
 args = lo.parse_args()
 
@@ -69,9 +72,10 @@ if not args.include_h:
     chain = loos.selectAtoms(chain, '!hydrogen')
 
 residues = chain.splitByResidue()
+num_residues = len(residues)
 
 # if there's 1 trajectory, args.traj will be a string, otherwise
-#
+# it's a list
 if type(args.traj) == str:
     t = loos.pyloos.Trajectory(args.traj, system,
                                skip=args.skip, stride=args.stride)
@@ -90,6 +94,7 @@ contacts = numpy.zeros((len(residues) * (len(residues)-1) / 2, len(traj)),
                        type=float)
 
 default_box = loos.GCoord(10000., 10000., 10000.)
+frame_number = 0
 for frame in traj:
     # checking each frame is wasteful, but shouldn't have a measurable
     # effect on performance
@@ -99,5 +104,26 @@ for frame in traj:
         box = default_box
 
     # compute residue-residue contacts and store
+    for i in range(num_residues - 1):
+        r1 = residues[i]
+        for j in range(i+1, num_residues):
+            contact = r1.logisticContact(r2, args.radius, args.sigma, box)
+            index = (i*num_residues) + j
+            contacts[frame_number, index] = contact
 
 print("Finished computing contacts, starting distance calculation")
+
+dists = pdist(contacts, 'euclidean')
+square_dists = np.zeros([num_residues, num_residues], np.float)
+for i in range(num_residues - 1):
+    for j in range(i+1, num_residues):
+        index = i*num_residues + j
+        square_dists[i, j] = dists[index]
+        square_dists[j, i] = dists[index]
+
+if args.outfile:
+    outfile = open(args.outfile, "w")
+else:
+    outfile = sys.stdout
+
+square_dists.savetxt(outfile, header=lo.header())
