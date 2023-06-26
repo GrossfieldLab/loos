753e03ba58a067c0aeb72e2a07ea85742c767ade
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2023-06-26 18:02:40 -0400

    parsing works, converting to string sorta

--- Packages/User/test_h5.cpp
+++ Packages/User/test_h5.cpp
@@ -32,17 +32,88 @@
 */
 #include <loos.hpp>
 #include <H5Cpp.h>
+#include <boost/json.hpp>
+//using namespace boost::json;
 
+std::string getTopology(H5::H5File &file) {
+  H5::DataSet dataset = file.openDataSet("topology");
+  H5::DataSpace dataspace = dataset.getSpace();
+  H5::DataType datatype = dataset.getDataType();
+  H5std_string topology;
+  dataset.read(topology, datatype, dataspace, dataspace);
+  return std::string(topology);
+}
 
 int main(int argc, char *argv[]) {
 
     std::string filename = argv[1];
 
+    // Turn off the auto-printing when failure occurs so that we can
+  	// handle the errors appropriately
+    //H5:Exception::dontPrint();
+
     H5::H5File file(filename, H5F_ACC_RDONLY);
+#if 0
     H5::DataSet dataset = file.openDataSet("topology");
 
     H5::DataSpace dataspace = dataset.getSpace();
     int rank = dataspace.getSimpleExtentNdims();
     std::cout << rank << std::endl;    
-    //int length = dataspace.getSimpleExtentNpoints();
+    int length = dataspace.getSimpleExtentNpoints();
+    std::cout << length << std::endl;    
+    const int ndims = dataspace.getSimpleExtentNdims();
+    std::cout << ndims << std::endl;
+    hsize_t * dims = new hsize_t[1]; 
+    dataspace.getSimpleExtentDims(dims, NULL);
+    std::cout << dims[0] << std::endl;    
+
+    H5::DataType datatype = dataset.getDataType();
+    H5std_string topology;
+    dataset.read(topology, datatype, dataspace, dataspace);
+    std::cout << topology << std::endl;
+
+
+    delete[] dims;
+#endif
+
+    std::string topology_json = getTopology(file);
+    //std::cout << topology << std::endl;
+
+    // Need to put this into try catch
+    boost::json::value topology = boost::json::parse(topology_json);
+
+    loos::AtomicGroup ag;
+    uint index = 0;
+
+    boost::json::array chains = topology.at("chains").as_array();
+    for (auto chain: chains) {
+      boost::json::array residues = chain.at("residues").as_array();
+      for (auto residue: residues) {
+        uint resnum = residue.at("resSeq").as_uint64();
+        std::string resname = std::string_view(residue.at("name").as_string());
+
+        boost::json::array atoms = residue.at("atoms").as_array();
+        for (auto atom: atoms) {
+          std::stringview atom_name = boost::json::value_to<std::string>(atom.at("name").as_string());
+          uint id = atom.at("index").as_uint64();
+          std::stringview element = boost::json::value_to<std::string>(atom.at("element").as_string());
+
+          loos::pAtom pa(new loos::Atom);
+          pa->name(atom_name);
+          pa->id(id);
+          pa->index(index);
+          pa->resid(resnum);
+          pa->resname(resname);
+          pa->PDBelement(element);
+
+          //ag._atomid_to_patom[pa->id()] = pa;
+          ag.append(pa);
+
+          index++;
+        }
+      }
+    }
+
+  
+    return 0;
 }
