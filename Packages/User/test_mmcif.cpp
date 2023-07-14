e949e4d4c54af5400880279762e715ccdc078923
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2023-07-14 16:20:59 -0400

    all atom properties except bonds work

--- Packages/User/test_mmcif.cpp
+++ Packages/User/test_mmcif.cpp
@@ -11,7 +11,13 @@ namespace cif = gemmi::cif;
 int main(int argc, char *argv[]) {
     std::string filename = std::string(argv[1]);
     
+    loos::AtomicGroup ag;
+    
     auto structure = gemmi::read_structure_file(filename, gemmi::CoorFormat::Mmcif);
+    auto unit_cell = structure.cell;
+    auto box = loos::GCoord(unit_cell.a, unit_cell.b, unit_cell.c);
+    ag.periodicBox(box);
+
     // TODO: hard-wired to read first model, but there should probably be a way to read others
     auto model = structure.first_model();
     int atom_index = 0;
@@ -20,23 +26,42 @@ int main(int argc, char *argv[]) {
         std::string chain_name = chain.name;
         for (auto residue:chain.residues) {
             std::string residue_name = residue.name;
-            std::string label_seq = residue.label_seq.str();
+            auto label_seq = residue.label_seq;
             std::string res_entity_id = residue.entity_id;
             std::cout << "Residue: "
                       << residue_number << "\t"
-                      << label_seq << "\t"
-                      << res_entity_id << std::endl;
+                      << label_seq.str() << "\t"
+                      << res_entity_id << "\t" 
+                      << residue.name << "\t"
+                      << residue.seqid.num.value << "\t"
+                      << std::endl;
             for (auto atom:residue.atoms) {
+                loos::pAtom pa(new loos::Atom);
+                pa->index(atom.serial);
+                pa->id(atom_index);
+                pa->name(atom.name);
+                pa->PDBelement(atom.element.name());
+                pa->coords().x(atom.pos.x);
+                pa->coords().y(atom.pos.y);
+                pa->coords().z(atom.pos.z);
+                pa->resid(residue.seqid.num.value);
+                pa->chainId(chain_name);
+                // might as well fill in segid, even though it's not official
+                pa->segid(chain_name);
+                pa->resname(residue.name);
                 std::cout << atom.name << "\t" 
                           << atom.element.name() << "\t" 
                           << atom.pos.x << "\t" 
-                          << atom.pos.y << "\t" << atom.pos.z 
-                          << std::endl;
-            
+                          << atom.pos.y << "\t" 
+                          << atom.pos.z << std::endl;
+                // TODO: charge is a char*, looks like it's usually "?" in actual
+                //       mmCIF files. Perhaps a try/catch block to convert to float?
+                ag.append(pa); 
 
             atom_index++;
             }
         residue_number++;
         }
     }
+    std::cout << ag << std::endl;
 }
